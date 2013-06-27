c3509b830d3cff1499f1a1f72dd8ec8cb87610c7
theim <timon.heim@cern.ch>
2013-06-27 17:47:59 +0200

    Added Gennum Register Map, probably the ugly way.

---
+++ src/libSpec/include/GennumRegMap.h
@@ -0,0 +1,61 @@
+// #################################
+// # Author: Timon Heim
+// # Email: timon.heim at cern.ch
+// # Project: Yarr
+// # Description: SPEC cpp library
+// # Comment: Original driver taken from Marcus Guillermo
+// #          Modified for SPEC card
+// ################################
+
+enum {
+	/* page 106 */
+	GNPPCI_MSI_CONTROL	= 0x48,		/* actually, 3 smaller regs */
+	GNPPCI_MSI_ADDRESS_LOW	= 0x4c,
+	GNPPCI_MSI_ADDRESS_HIGH	= 0x50,
+	GNPPCI_MSI_DATA		= 0x54,
+
+	GNPCI_SYS_CFG_SYSTEM	= 0x800,
+
+	/* page 130 ff */
+    GNCLK_CSR       = 0x808,
+	GNINT_CTRL		= 0x810,
+	GNINT_STAT		= 0x814,
+	GNINT_CFG_0		= 0x820,
+	GNINT_CFG_1		= 0x824,
+	GNINT_CFG_2		= 0x828,
+	GNINT_CFG_3		= 0x82c,
+	GNINT_CFG_4		= 0x830,
+	GNINT_CFG_5		= 0x834,
+	GNINT_CFG_6		= 0x838,
+	GNINT_CFG_7		= 0x83c,
+#define GNINT_CFG(x) (GNINT_CFG_0 + 4 * (x))
+
+	/* page 146 ff */
+	GNGPIO_BASE = 0xA00,
+	GNGPIO_BYPASS_MODE	= GNGPIO_BASE,
+	GNGPIO_DIRECTION_MODE	= GNGPIO_BASE + 0x04, /* 0 == output */
+	GNGPIO_OUTPUT_ENABLE	= GNGPIO_BASE + 0x08,
+	GNGPIO_OUTPUT_VALUE	= GNGPIO_BASE + 0x0C,
+	GNGPIO_INPUT_VALUE	= GNGPIO_BASE + 0x10,
+	GNGPIO_INT_MASK		= GNGPIO_BASE + 0x14, /* 1 == disabled */
+	GNGPIO_INT_MASK_CLR	= GNGPIO_BASE + 0x18, /* irq enable */
+	GNGPIO_INT_MASK_SET	= GNGPIO_BASE + 0x1C, /* irq disable */
+	GNGPIO_INT_STATUS	= GNGPIO_BASE + 0x20,
+	GNGPIO_INT_TYPE		= GNGPIO_BASE + 0x24, /* 1 == level */
+	GNGPIO_INT_VALUE	= GNGPIO_BASE + 0x28, /* 1 == high/rise */
+	GNGPIO_INT_ON_ANY	= GNGPIO_BASE + 0x2C, /* both edges */
+
+	/* page 158 ff */
+	FCL_BASE		= 0xB00,
+	FCL_CTRL		= FCL_BASE,
+	FCL_STATUS		= FCL_BASE + 0x04,
+	FCL_IODATA_IN		= FCL_BASE + 0x08,
+	FCL_IODATA_OUT		= FCL_BASE + 0x0C,
+	FCL_EN			= FCL_BASE + 0x10,
+	FCL_TIMER_0		= FCL_BASE + 0x14,
+	FCL_TIMER_1		= FCL_BASE + 0x18,
+	FCL_CLK_DIV		= FCL_BASE + 0x1C,
+	FCL_IRQ			= FCL_BASE + 0x20,
+	FCL_TIMER_CTRL		= FCL_BASE + 0x24,
+	FCL_IM			= FCL_BASE + 0x28,
+};
