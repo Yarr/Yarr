8e9d1d6b882be074dd69080882c36bf2ceacabad
theim <timon.heim@cern.ch>
2013-07-04 20:48:33 +0200

    Small program that writes and reads a sample via DMA and then check it for errors.

---
+++ src/tools/errorCheck.cpp
@@ -0,0 +1,56 @@
+#include <SpecController.h>
+
+#include <iostream>
+#include <stdint.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+uint32_t rand32() {
+    uint32_t number = 0;
+    number += rand() % 256;
+    number += (rand() % 256) << 8;
+    number += (rand() % 256) << 16;
+    number += (rand() % 256) << 24;
+    return number;
+}
+
+
+int main (void) {
+    SpecController mySpec(0);
+
+    int maxLoops = 10;
+    
+    const size_t size = 1024/4; // 1kB
+    
+    srand(time(NULL));
+
+    for (int loop = 0; loop<maxLoops; loop++) {
+        std::cout << std::endl << "==================================" << std::endl;
+        
+        std::cout << "Creating Sample of size " << size*4/1024 << "kB ... ";
+        uint32_t sample[size];
+        for(unsigned int i = 0; i<size; i++)
+            sample[i] = rand32();
+
+        std::cout << "Writing Sample ... ";
+        mySpec.writeDma(0x0, sample, size);
+         
+        std::cout << "Reading Sample!" << std::endl;
+        uint32_t readBack[size];
+        mySpec.readDma(0x0, readBack, size);
+
+        int counter = 0;
+        std::cout << "Sample\tReadback" << std::endl;
+        for(unsigned int i = 0; i<size; i++) {
+            if(sample[i] != readBack[i]) {
+                counter ++;
+                std::cout << "[" << i << "] " << std::hex << sample[i] << "\t" << readBack[i] << std::dec << std::endl;
+            }
+        }
+        std::cout << "Found #" << counter << " errors!" << std::endl;
+        std::cout << "==================================" << std::endl;
+    }
+
+    return 0;
+}
