2c4dd13f26b92671c5ca8e89bab6d593fff2a193
theim <timon.heim@cern.ch>
2013-10-04 16:04:42 +0200

    Looking for minimum of files to compile firmware.

--- hdl/Manifest.py
+++ hdl/Manifest.py
@@ -1,8 +1,8 @@
 files = ["ip_cores/clk_gen.vhd",
-         "ip_cores/ila.vhd",
-         "ip_cores/ila_icon.vhd",
-         "ip_cores/fifo_64x512.vhd",
-         "ip_cores/fifo_32x512.vhd"]
+         "ip_cores/ila.ngc",
+         "ip_cores/ila_icon.ngc",
+         "ip_cores/fifo_64x512.ngc",
+         "ip_cores/fifo_32x512.ngc"]
 
 modules = { "local" : ["common",
             "gn4124-core",
