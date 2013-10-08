637febe3a1cbe8f5d51ba942e258bcb62325b2f1
theim <timon.heim@cern.ch>
2013-10-08 09:35:10 +0200

    Made a a package for the common stuff aka async_fifo. Not nice because depends on gn4124_core_pkg.

--- hdl/common/Manifest.py
+++ hdl/common/Manifest.py
@@ -1,4 +1,6 @@
 files = ["dummy_ctrl_regs.vhd",
          "dummy_stat_regs.vhd",
          "wb_addr_decoder.vhd",
-         "generic_async_fifo_wrapper.vhd"]
+         "generic_async_fifo_wrapper.vhd",
+         "common_pkg.vhd"
+         ]
