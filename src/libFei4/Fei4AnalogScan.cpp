b8699c1289fafeae5203f9673adf09704463d77d
Timon Heim <timon.heim@cern.ch>
2014-10-13 23:32:19 +0200

    Forgot to add AnalogScan

---
+++ src/libFei4/Fei4AnalogScan.cpp
@@ -0,0 +1,65 @@
+// #################################
+// # Author: Timon Heim
+// # Email: timon.heim at cern.ch
+// # Project: Yarr
+// # Description: Fei4 Analog Scan
+// # Comment: Nothing fancy
+// ################################
+
+#include "Fei4AnalogScan.h"
+
+Fei4AnalogScan::Fei4AnalogScan(Fei4 *fe, TxCore *tx, RxCore *rx, ClipBoard<RawData> *data) : ScanBase(fe, tx, rx, data) {
+    mask = MASK_32;
+    dcMode = QUAD_DC;
+    numOfTriggers = 100;
+    triggerFrequency = 20e3;
+    triggerDelay = 50;
+    verbose = false;
+}
+
+// Initialize Loops
+void Fei4AnalogScan::init() {
+    // Loop 1: Mask Staging
+    std::shared_ptr<Fei4MaskLoop> maskStaging(new Fei4MaskLoop);
+    maskStaging->setVerbose(verbose);
+    maskStaging->setMaskStage(mask);
+    maskStaging->setScap(true);
+    maskStaging->setLcap(true);
+    // Loop 2: Double Columns
+    std::shared_ptr<Fei4DcLoop> dcLoop(new Fei4DcLoop);
+    dcLoop->setVerbose(verbose);
+    dcLoop->setMode(dcMode);
+
+    // Loop 3: Trigger
+    std::shared_ptr<Fei4TriggerLoop> triggerLoop(new Fei4TriggerLoop);
+    triggerLoop->setVerbose(verbose);
+    triggerLoop->setTrigCnt(numOfTriggers);
+    triggerLoop->setTrigFreq(triggerFrequency);
+    triggerLoop->setTrigDelay(triggerDelay);
+
+    // Loop 4: Data gatherer
+    std::shared_ptr<StdDataLoop> dataLoop(new StdDataLoop);
+    dataLoop->setVerbose(verbose);
+    dataLoop->connect(g_data);
+
+    loops.push_back(maskStaging);
+    loops.push_back(dcLoop);
+    loops.push_back(triggerLoop);
+    loops.push_back(dataLoop);
+
+    engine.addAction(maskStaging);
+    engine.addAction(dcLoop);
+    engine.addAction(triggerLoop);
+    engine.addAction(dataLoop);
+
+    engine.init();
+}
+
+// Do necessary pre-scan configuration
+void Fei4AnalogScan::preScan() {
+    g_fe->writeRegister(&Fei4::Trig_Count, 15);
+    g_fe->writeRegister(&Fei4::Trig_Lat, (255-triggerDelay)-5);
+    g_fe->writeRegister(&Fei4::PlsrDAC, 300);
+    g_fe->writeRegister(&Fei4::CalPulseWidth, 20); // Longer than max ToT 
+    while(!g_tx->isCmdEmpty());
+}
