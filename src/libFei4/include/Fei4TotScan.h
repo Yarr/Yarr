6eed5184821ee2f0b6ae1ef3fff9bfc369b00a3e
Timon Heim <timon.heim@cern.ch>
2014-11-27 15:58:14 +0100

    Adding ToT scan at one target

---
+++ src/libFei4/include/Fei4TotScan.h
@@ -0,0 +1,38 @@
+#ifndef FEI4TOTSCAN_H
+#define FEI4TOTSCAN_H
+
+// #################################
+// # Author: Timon Heim
+// # Email: timon.heim at cern.ch
+// # Project: Yarr
+// # Description: Fei4 Tot Scan
+// # Comment: Nothing fancy
+// ################################
+
+#include "ScanBase.h"
+
+#include "AllStdActions.h"
+#include "AllFei4Actions.h"
+
+class Fei4TotScan : public ScanBase {
+    public:
+        Fei4TotScan(Fei4 *fe, TxCore *tx, RxCore *rx, ClipBoard<RawData> *data);
+        
+        void init();
+        void preScan();
+        void postScan() {}
+
+    private:
+        enum MASK_STAGE mask;
+        enum DC_MODE dcMode;
+        unsigned numOfTriggers;
+        double triggerFrequency;
+        unsigned triggerDelay;
+        bool useScap;
+        bool useLcap;
+        double target;
+        
+        bool verbose;
+};
+
+#endif
