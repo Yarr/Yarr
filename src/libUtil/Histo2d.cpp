75cf5aadadbbfde91dc64c27bac904ff8cb69f44
Timon Heim <timon.heim@cern.ch>
2014-11-27 17:06:18 +0100

    Don't divide by Zero!

--- src/libUtil/Histo2d.cpp
+++ src/libUtil/Histo2d.cpp
@@ -127,7 +127,11 @@ void Histo2d::divide(const Histo2d &h) {
     if (this->size() != h.size())
         return;
     for (unsigned int i=0; i<(xbins*ybins); i++) {
-        data[i] = data[i]/h.getBin(i);
+        if (h.getBin(i) == 0) {
+            data[i] = 0;
+        } else {
+            data[i] = data[i]/h.getBin(i);
+        }
     }
     entries += h.numOfEntries();
 }
