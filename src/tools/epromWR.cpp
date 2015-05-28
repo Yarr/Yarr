db2247d2a1862acc0e587970b3abb7fa5dafdbcd
Jonathan Debus <etphysik.jd@gmail.com>
2015-05-28 21:43:36 +0200

    EEPROM Write should work - doulbe check because errors might be highly invasive - plus minor bug fixes in the EEPROM read

---
+++ src/tools/epromWR.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <stdint.h>
+
+#include <SpecController.h>
+
+
+int main() {
+
+    // Open device
+    std::cout << "Opening Spec device." << std::endl; //debug
+    SpecController mySpec(0);
+    std::cout << "Opened SpecController.\n"; //debug
+
+    uint32_t offs = 90;
+    uint32_t len = 6;
+    uint8_t * buffer = new uint8_t[len];
+    *(buffer) = 0xfc;
+    *(buffer+1) = 0x0f;
+    *(buffer+2) = 0x77;
+    *(buffer+3) = 0x07;
+    *(buffer+4) = 0x0;
+    *(buffer+6) = 0x0;
+
+    mySpec.writeEeprom(buffer, len, offs);
+
+    delete buffer;
+
+    return 0;
+
+}
