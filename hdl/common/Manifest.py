b3f1392cc9d136f062217c846666b1c26a596791
Timon Heim <timon.heim@cern.ch>
2015-06-15 16:48:41 +0200

    Adding makefile and disabling 16x multiplication of module data

--- hdl/common/Manifest.py
+++ hdl/common/Manifest.py
@@ -2,5 +2,6 @@ files = ["dummy_ctrl_regs.vhd",
          "dummy_stat_regs.vhd",
          "wb_addr_decoder.vhd",
          "generic_async_fifo_wrapper.vhd",
-         "common_pkg.vhd"
+         "common_pkg.vhd",
+         "rr_arbiter.vhd"
          ]
