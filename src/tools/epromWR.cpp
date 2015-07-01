bbaf6a15585116081ff335cf97a098f942af749b
Jonathan Debus <etphysik.jd@gmail.com>
2015-07-01 17:45:58 +0200

    Added minimal file sanity check for EEPROM write functionality

--- src/tools/epromWR.cpp
+++ src/tools/epromWR.cpp
@@ -25,6 +25,13 @@ int main(int argc, char * argv[]) {
     {
         std::string tmpStr;
         std::getline(iF, tmpStr); //Headline - add sanity check if desired
+        if(tmpStr != "     addr  msk        data") {
+            std::cout << "Invalid headline. \n "
+                      << "Note that it is encouraged to automatically create an sbe file \n"
+                      << "using the EEPROM read functionality, copy the file, do desired \n"
+                      << "changes by hand and use this file to write to the EEPROM. \n";
+            return -31;
+        }
     }
 
     {
@@ -35,28 +42,40 @@ int main(int argc, char * argv[]) {
         while(iF >> tmpInt) {
             switch(i%3) {
             case 0:
+                if(tmpInt>0xFFF) {
+                    std::cout << "Invalid address size in line " << i/3 + 1 << ". Aborting... \n";
+                    return -32
+                }
                 *(buffer+i) = (tmpInt & 0xFF);
                 i++;
                 *(buffer+i) = ((tmpInt >> 8) & 0xF);
                 break;
             case 1:
+                if(tmpInt>0xF) {
+                    std::cout << "Invalid mask size in line " << i/3 + 1 << ". Aborting... \n";
+                    return -33;
+                }
                 *(buffer+i) |= ((tmpInt & 0xF) << 4);
                 i++;
                 break;
             case 2:
+                if(tmpInt>0xFFFFFFFF) {
+                    std::cout << "Invalid data size in line " << i/3 + 1 << ". Aborting... \n";
+                    return -34;
+                }
                 *(buffer+i)   = (tmpInt & 0xFF);
                 *(buffer+i+1) = ((tmpInt >>  8) & 0xFF);
                 *(buffer+i+2) = ((tmpInt >> 16) & 0xFF);
                 *(buffer+i+3) = ((tmpInt >> 24) & 0xFF);
-                i+=4; //equivalent i->i+3+1, system invariant under i->i+3
+                i+=4; //equivalent i->i+3+1, case structure invariant under i->i+3
                 break;
             }
         }
         iF >> std::dec;
         iF >> std::noshowbase;
         if(i!=252) {
-            std::cout << "Input file incomplete. Aborting... \n";
-            exit(-20);
+            std::cout << "Input file incomplete or too big. Aborting... \n";
+            return -20;
         }
     }
 
