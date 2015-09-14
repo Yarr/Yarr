8eacc1cfc921ecde57294075223924cef3a7441f
Jonathan Debus <etphysik.jd@gmail.com>
2015-09-14 16:20:35 +0200

    Added global config file support - yet to be tested

--- gui/YarrGui/yarrgui.cpp
+++ gui/YarrGui/yarrgui.cpp
@@ -348,47 +348,128 @@ void YarrGui::on_SBEReadButton_clicked()
 void YarrGui::on_addFeButton_clicked()
 {
 
-    unsigned chipIdAdded = (ui->chipIdEdit->text()).toUInt();
-    unsigned txChannelAdded = (ui->txChannelEdit->text()).toUInt();
-    unsigned rxChannelAdded = (ui->rxChannelEdit->text()).toUInt();
+    if(!(ui->global_cfg_checkBox->isChecked())){
+        unsigned chipIdAdded = (ui->chipIdEdit->text()).toUInt();
+        unsigned txChannelAdded = (ui->txChannelEdit->text()).toUInt();
+        unsigned rxChannelAdded = (ui->rxChannelEdit->text()).toUInt();
 
-    bk->addFe(chipIdAdded, txChannelAdded, rxChannelAdded);
-    bk->getLastFe()->fromFileBinary((ui->configfileName->text()).toStdString());
-    tx->setCmdEnable(0x1 << bk->getLastFe()->getTxChannel());
-    bk->getLastFe()->configure();
-    bk->getLastFe()->configurePixels();
-    while(!(tx->isCmdEmpty())) {
-        ;
+        if(bk->getFe(rxChannelAdded) != NULL) {
+            std::cout << "ERROR - rx channel already used. Aborting... \n";
+            return;
+        }
+        bk->addFe(chipIdAdded, txChannelAdded, rxChannelAdded);
+        bk->getLastFe()->fromFileBinary((ui->configfileName->text()).toStdString());
+        tx->setCmdEnable(0x1 << bk->getLastFe()->getTxChannel());
+        bk->getLastFe()->configure();
+        bk->getLastFe()->configurePixels();
+        while(!(tx->isCmdEmpty())) {
+            ;
+        }
+        std::this_thread::sleep_for(std::chrono::microseconds(1000));
+        tx->setCmdEnable(bk->getTxMask());
+        rx->setRxEnable(bk->getRxMask());
+
+        QTreeWidgetItem * feTreeItem = new QTreeWidgetItem(ui->feTree);
+        QTreeWidgetItem * feTreeItemId = new QTreeWidgetItem();
+        QTreeWidgetItem * feTreeItemTx = new QTreeWidgetItem();
+        QTreeWidgetItem * feTreeItemRx = new QTreeWidgetItem();
+        QTreeWidgetItem * feTreeItemCf = new QTreeWidgetItem();
+        QTreeWidgetItem * feTreeItemCk = new QTreeWidgetItem();
+
+        feTreeItem->setText(0, "FE " + QString::number(ui->feTree->topLevelItemCount()));
+        feTreeItemId->setText(0, "Chip ID");
+        feTreeItemId->setText(1, QString::number(chipIdAdded));
+        feTreeItemTx->setText(0, "TX Channel");
+        feTreeItemTx->setText(1, QString::number(txChannelAdded));
+        feTreeItemRx->setText(0, "RX Channel");
+        feTreeItemRx->setText(1, QString::number(rxChannelAdded));
+        feTreeItemCf->setText(0, "Config file");
+        feTreeItemCf->setText(1, ui->configfileName->text());
+        feTreeItemCk->setText(0, "Active");
+        feTreeItemCk->setFlags(feTreeItemCk->flags() | Qt::ItemIsUserCheckable | Qt::ItemIsSelectable);
+        feTreeItemCk->setCheckState(1, Qt::Unchecked);
+
+        feTreeItem->addChild(feTreeItemId);
+        feTreeItem->addChild(feTreeItemTx);
+        feTreeItem->addChild(feTreeItemRx);
+        feTreeItem->addChild(feTreeItemCf);
+        feTreeItem->addChild(feTreeItemCk);
+    } else {
+        std::string gCfgFilename((ui->configfileName_2->text()).toStdString());
+        std::ifstream gCfg(gCfgFilename);
+        if(!gCfg) {
+            std::cout << "ERROR - could not open global config file. Aborting... \n";
+        }
+        std::string chipNameTmp;
+        unsigned chipIdTmp;
+        unsigned txChannelTmp;
+        unsigned rxChannelTmp;
+        std::string chipCfgFilenameTmp;
+
+        std::vector<std::string> chipNamesAdded;
+        std::vector<unsigned> chipIdsAdded;
+        std::vector<unsigned> txChannelsAdded;
+        std::vector<unsigned> rxChannelsAdded;
+        std::vector<std::string> chipCfgFilenamesAdded;
+        char peeek;
+        while(!(gCfg.eof()) && gCfg){
+            peeek = gCfg.peek();
+            if(peeek == '#' || peeek == '\n') {
+                char tmpStr[2048];
+                gCfg.getline(tmpStr, 2048);
+                continue;
+            }
+            gCfg >> chipNameTmp >> chipIdTmp >> txChannelTmp >> rxChannelTmp >> chipCfgFilenameTmp;
+            chipNamesAdded.push_back(chipNameTmp);
+            chipIdsAdded.push_back(chipIdTmp);
+            txChannelsAdded.push_back(txChannelTmp);
+            rxChannelsAdded.push_back(rxChannelTmp);
+            chipCfgFilenamesAdded.push_back(chipCfgFilenameTmp);
+        }
+        for(unsigned i = 0; i<chipIdsAdded.size(); i++) {
+            if(bk->getFe(rxChannelsAdded.at(i)) != NULL) {
+                std::cout << "ERROR - rx channel already used. Aborting... \n";
+                return;
+            }
+            bk->addFe(chipIdsAdded.at(i), txChannelsAdded.at(i), rxChannelsAdded.at(i));
+            bk->getLastFe()->fromFileBinary(chipCfgFilenamesAdded.at(i));
+            tx->setCmdEnable(0x1 << bk->getLastFe()->getTxChannel());
+            bk->getLastFe()->configure();
+            bk->getLastFe()->configurePixels();
+            while(!(tx->isCmdEmpty())) {
+                ;
+            }
+            std::this_thread::sleep_for(std::chrono::microseconds(1000));
+            tx->setCmdEnable(bk->getTxMask());
+            rx->setRxEnable(bk->getRxMask());
+
+            QTreeWidgetItem * feTreeItem = new QTreeWidgetItem(ui->feTree);
+            QTreeWidgetItem * feTreeItemId = new QTreeWidgetItem();
+            QTreeWidgetItem * feTreeItemTx = new QTreeWidgetItem();
+            QTreeWidgetItem * feTreeItemRx = new QTreeWidgetItem();
+            QTreeWidgetItem * feTreeItemCf = new QTreeWidgetItem();
+            QTreeWidgetItem * feTreeItemCk = new QTreeWidgetItem();
+
+            feTreeItem->setText(0, QString::fromStdString(chipNamesAdded.at(i)));
+            feTreeItemId->setText(0, "Chip ID");
+            feTreeItemId->setText(1, QString::number(chipIdsAdded.at(i)));
+            feTreeItemTx->setText(0, "TX Channel");
+            feTreeItemTx->setText(1, QString::number(txChannelsAdded.at(i)));
+            feTreeItemRx->setText(0, "RX Channel");
+            feTreeItemRx->setText(1, QString::number(rxChannelsAdded.at(i)));
+            feTreeItemCf->setText(0, "Config file");
+            feTreeItemCf->setText(1, QString::fromStdString(chipCfgFilenamesAdded.at(i)));
+            feTreeItemCk->setText(0, "Active");
+            feTreeItemCk->setFlags(feTreeItemCk->flags() | Qt::ItemIsUserCheckable | Qt::ItemIsSelectable);
+            feTreeItemCk->setCheckState(1, Qt::Unchecked);
+
+            feTreeItem->addChild(feTreeItemId);
+            feTreeItem->addChild(feTreeItemTx);
+            feTreeItem->addChild(feTreeItemRx);
+            feTreeItem->addChild(feTreeItemCf);
+            feTreeItem->addChild(feTreeItemCk);
+        }
     }
-    std::this_thread::sleep_for(std::chrono::microseconds(1000));
-    tx->setCmdEnable(bk->getTxMask());
-    rx->setRxEnable(bk->getRxMask());
-
-    QTreeWidgetItem * feTreeItem = new QTreeWidgetItem(ui->feTree);
-    QTreeWidgetItem * feTreeItemId = new QTreeWidgetItem();
-    QTreeWidgetItem * feTreeItemTx = new QTreeWidgetItem();
-    QTreeWidgetItem * feTreeItemRx = new QTreeWidgetItem();
-    QTreeWidgetItem * feTreeItemCf = new QTreeWidgetItem();
-    QTreeWidgetItem * feTreeItemCk = new QTreeWidgetItem();
-
-    feTreeItem->setText(0, "FE " + QString::number(ui->feTree->topLevelItemCount()));
-    feTreeItemId->setText(0, "Chip ID");
-    feTreeItemId->setText(1, QString::number(chipIdAdded));
-    feTreeItemTx->setText(0, "TX Channel");
-    feTreeItemTx->setText(1, QString::number(txChannelAdded));
-    feTreeItemRx->setText(0, "RX Channel");
-    feTreeItemRx->setText(1, QString::number(rxChannelAdded));
-    feTreeItemCf->setText(0, "Config file");
-    feTreeItemCf->setText(1, ui->configfileName->text());
-    feTreeItemCk->setText(0, "Active");
-    feTreeItemCk->setFlags(feTreeItemCk->flags() | Qt::ItemIsUserCheckable | Qt::ItemIsSelectable);
-    feTreeItemCk->setCheckState(1, Qt::Unchecked);
-
-    feTreeItem->addChild(feTreeItemId);
-    feTreeItem->addChild(feTreeItemTx);
-    feTreeItem->addChild(feTreeItemRx);
-    feTreeItem->addChild(feTreeItemCf);
-    feTreeItem->addChild(feTreeItemCk);
 
 }
 
