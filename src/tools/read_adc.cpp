3067792ca7657a73463c44b28aab6e6b46a968ad
Timon Heim <timon.heim@cern.ch>
2015-12-07 11:41:41 -0800

    Update with more functionality

--- src/tools/read_adc.cpp
+++ src/tools/read_adc.cpp
@@ -3,20 +3,31 @@
 #include <iostream>
 #include <unistd.h>
 #include <chrono>
+#include <fstream>
+#include <iomanip>
 
-int main(void) {
+int main(int argc, char* argv[]) {
     SpecController mySpec(0);
 
     std::cout << "Init" << std::endl;
     AD7995 adc(&mySpec);
     std::cout << "Set channels" << std::endl;
     adc.setActiveChannels(1, 0, 0, 0);
+    
+    std::fstream ofile(argv[1], std::ios::out);
+    if (!ofile) {
+        std::cout << " Coudln't open file: " << argv[1] << std::endl;
+        return 1;
+    }
+
     std::cout << "Reading" << std::endl;
-    std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+
+    long double timestamp = 0;
     while(1) {
         double value = adc.read();
-        std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::seconds>(now-start).count() << " " << value << std::endl;
+        timestamp = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count()/1.0e6;
+        std::cout << std::setprecision(15) << timestamp << " " << value << std::endl;
+        ofile << std::setprecision(15) << timestamp << " " << value << std::endl;
         sleep(1);
     }
     return 0;
