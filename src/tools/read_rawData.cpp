2f555118d1b4f7aa4dedfd2261030fb15c493a03
Timon Heim <pixel@riverhorse.dhcp.lbl.gov>
2016-08-23 23:37:08 -0700

    Made output better

--- src/tools/read_rawData.cpp
+++ src/tools/read_rawData.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 
 #include "Fei4EventData.h"
+#include "Histo1d.h"
+#include "Histo2d.h"
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -12,22 +14,26 @@ int main(int argc, char* argv[]) {
     for (int i=1; i<argc; i++) {
         std::cout << "Opening file: " << argv[i] << std::endl;
         std::fstream file(argv[i], std::fstream::in | std::fstream::binary);
-        
+
         file.seekg(0, std::ios_base::end);
         int size = file.tellg();
         file.seekg(0, std::ios_base::beg);
-        std::cout << "Size: " << size/1024.0/1024.0 << " MB" << std::endl;
+        std::cout << "Size: " << size/1024.0/1024.0 << " MB" << std::endl;        
 
+        int count = 0;
         while (file) {
             Fei4Event event;
             event.fromFileBinary(file);
             if (!file)
                 break;
-            std::cout << event.l1id << " " << event.bcid << " " << event.nHits << std::endl;
-            for (unsigned i=0; i<event.nHits; i++) {
-                //std::cout << event.hits[i].col << " " << event.hits[i].row << " " << event.hits[i].tot << std::endl;
+            if(event.nHits > 0) {
+                std::cout << "~~ Event #" << count << " ~~" << std::endl;
+                std::cout << event.l1id << " " << event.bcid << " " << event.nHits << std::endl;
+                for (unsigned i=0; i<event.nHits; i++) {
+                    std::cout << event.hits[i].col << " " << event.hits[i].row << " " << event.hits[i].tot << std::endl;
+                }
             }
-            std::cout << "Now: " << file.tellg() << std::endl;
+            count ++;
         }
 
         file.close();
