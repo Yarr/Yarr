af8ab199739cd9ed80e6e42ad28a82cdccc97509
Karolos Potamianos <karolos.potamianos@cern.ch>
2017-02-02 15:48:32 -0800

    Adjusting runMe.sh

--- src/runMe.sh
+++ src/runMe.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-./bin/scanConsole -c configs/default_fei4b.json -s digitalscan -p  -n 30 >& scan.out &
+scanType=${1}
+[ -z ${scanType} ] && echo -e "Usage: ${0} scanType" && exit
+
+echo Starting scanConsole ...
+./bin/scanConsole -c configs/default_fei4b.json -s ${scanType} -p  -n 30 >& scan.out &
 SCAN_PID=$!
 echo Scan PID: $SCAN_PID
 sleep 2
@@ -9,10 +13,21 @@ echo Starting emulator ...
 EMU_PID=$!
 echo Emulator PID: $EMU_PID
 wait $SCAN_PID
+echo "Scan completed."
 
 echo -n "Killing emulator..."
 kill -9 $EMU_PID
 echo "Done"
 
+scanNo=$(cat ~/.yarr/runCounter)
+scanDataFolder=data/$(printf "%06d" ${scanNo})_${scanType}
+
 echo "Available plots:"
-ls data/0*$(cat ~/.yarr/runCounter)*/*png
+ls ${scanDataFolder}/*png
+
+mv scan.out emu.out ${scanDataFolder}
+
+echo "Available logs:"
+ls ${scanDataFolder}/*out
+
+echo "Note: the killing of PID ${EMU_PID} (bin/emulator) is expected"
