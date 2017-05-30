291f5011c2835584df3f7911d5888e15cf64ab33
cretingame <arnaud.sautaux@gmail.com>
2017-05-30 15:19:28 -0700

    cleaner

--- syn/xpressk7/bram_revA/bitstream.tcl
+++
@@ -1,4 +0,0 @@
-open_project yarr.xpr
-launch_runs impl_1 -to_step write_bitstream
-wait_on_run impl_1
-exit
--- syn/xpressk7/bram_revA/project.tcl
+++
@@ -1,8 +0,0 @@
-create_project yarr ./
-set_property part xc7k160tfbg676-2 [current_project]
-set_property target_language VHDL [current_project]
-set_property top top_level [get_property srcset [current_run]]
-source files.tcl
-update_compile_order -fileset sources_1
-update_compile_order -fileset sim_1
-exit
