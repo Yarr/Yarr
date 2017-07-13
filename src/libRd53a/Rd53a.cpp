5bc0fcab201274946843e08c9f09a24ee755f9c3
Timon Heim <timon.heim@cern.ch>
2017-07-13 13:48:09 -0700

    added proper constructor

--- src/libRd53a/Rd53a.cpp
+++ src/libRd53a/Rd53a.cpp
@@ -8,3 +8,28 @@
 // ################################
 
 #include "Rd53a.h"
+
+Rd53a::Rd53a(TxCore *core) : FrontEnd(), Rd53aCfg(), Rd53aCmd(core) {
+    txChannel = 99;
+    rxChannel = 99;
+	histogrammer = NULL;
+	ana = NULL;
+	active = true;
+}
+
+Rd53a::Rd53a(TxCore *core, unsigned arg_channel) : FrontEnd(), Rd53aCfg(), Rd53aCmd(core) {
+	txChannel = arg_channel;
+	rxChannel = arg_channel;
+	histogrammer = NULL;
+	ana = NULL;
+	active = true;
+}
+
+Rd53a::Rd53a(TxCore *core, unsigned arg_txChannel, unsigned arg_rxChannel) : FrontEnd(), Rd53aCfg(), Rd53aCmd(core) {
+	txChannel = arg_txChannel;
+	rxChannel = arg_rxChannel;
+	histogrammer = NULL;
+	ana = NULL;
+	active = true;
+}
+
