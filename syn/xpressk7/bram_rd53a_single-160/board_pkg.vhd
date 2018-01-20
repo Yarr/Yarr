4d47a94c9aee61f336f581feae5405f4ea511552
Timon Heim <timon.heim@cern.ch>
2018-01-20 14:08:08 -0800

    Addition to board pkg, need to propagate to other leafs

--- syn/xpressk7/bram_rd53a_single-160/board_pkg.vhd
+++ syn/xpressk7/bram_rd53a_single-160/board_pkg.vhd
@@ -9,5 +9,7 @@ package board_pkg is
     constant c_FE_TYPE : string := "RD53";
     constant c_RX_NUM_LANES : integer := 4;
     constant c_TX_IDLE_WORD : std_logic_vector(31 downto 0) := x"69696969";
+    constant c_TX_SYNC_WORD : std_logic_vector(31 downto 0) := x"817e6969";
+    constant c_TX_SYNC_INTERVAL : unsigned(7 downto 0) := to_unsigned(31,8);
 end board_pkg;
 
