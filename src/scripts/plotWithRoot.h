a86765864a439ae8c7adbdb2678166d8b9ddf42d
clabitan <chariloulabitan@gmail.com>
2018-06-08 16:39:50 -0700

    Nicer Occupancy plot. Includes .h file.

---
+++ src/scripts/plotWithRoot.h
@@ -0,0 +1,79 @@
+//Header file for plotWithRoot
+
+#include <TCanvas.h>
+#include <TH2F.h>
+#include <TPaveStats.h>
+#include <THStack.h>
+#include <TLegend.h>
+#include <TLatex.h>
+
+#include <RD53Style.C>
+
+//Style for TH1
+void style_TH1(TH1* hist_TH1, const char* Xtitle, const char* Ytitle ){
+	hist_TH1->GetXaxis()->SetTitle(Xtitle);
+	hist_TH1->GetYaxis()->SetTitle(Ytitle);
+	hist_TH1->GetXaxis()->SetTitleSize(0.045);
+	hist_TH1->GetXaxis()->SetTitleOffset(1.25);
+	hist_TH1->GetYaxis()->SetTitleSize(0.05);
+	hist_TH1->GetYaxis()->SetTitleOffset(1.5);
+	hist_TH1->SetStats(0);
+}
+
+//Style for THStack (TH1)
+void style_THStack(THStack* hist_Stack, const char* Xtitle, const char*Ytitle){
+	hist_Stack->GetXaxis()->SetTitle(Xtitle);
+	hist_Stack->GetYaxis()->SetTitle(Ytitle);
+	hist_Stack->GetXaxis()->SetTitleSize(0.045);
+	hist_Stack->GetXaxis()->SetTitleOffset(1.25);
+	hist_Stack->GetYaxis()->SetTitleSize(0.05);
+	hist_Stack->GetYaxis()->SetTitleOffset(1.5);
+}
+
+//Style for TH1 Canvas
+void style_TH1canvas(TCanvas *c){
+	c->SetLeftMargin(0.15);
+	c->SetRightMargin(0.05);
+	c->SetBottomMargin(0.1225);
+
+}
+
+//Style for TLatex --> RD53A and Chip Id labels.
+void latex_Chip(TLatex* name){
+	name->SetNDC();
+	name->SetTextAlign(22);
+	name->SetTextFont(73);
+	name->SetTextSizePixels(30);
+
+}
+
+//Function for if pixel was in certain FE
+int whichFE(int col) {
+	//const char fe_type[3] = ["syn", "lin", "diff"];
+	int division1 = 128, division2 = 264;
+	int which_fe = 10; //add if 10, give error in .cxx file
+	if (col<division1){ //Syn FE
+		which_fe = 0;
+	} else if (col>=division1 && col<division2){ //Lin FE
+		which_fe = 1;
+	} else if (col>=division2) { //Diff FE
+		which_fe = 2;
+	}
+	return which_fe;
+}
+
+//Write a function where you give it occupancy and it returns a bin number
+int whichBin(double value, double occnum) {
+	int hist_bin;
+	double zero = 0.0;
+	double ninety8 = value * 0.98;
+	double hundred02 = value * 1.02;
+	if (occnum == zero) hist_bin = 1;
+	else if (occnum > zero && occnum < ninety8) hist_bin = 2;
+	else if (occnum >= ninety8 && occnum < value) hist_bin = 3;
+	else if (occnum == value) hist_bin = 4;
+	else if (occnum > value && occnum <= hundred02) hist_bin = 5;
+	else if (occnum > hundred02) hist_bin = 6;
+	return hist_bin;
+}
+
--- src/scripts/plotWithRoot_Occupancy.cxx
+++ src/scripts/plotWithRoot_Occupancy.cxx
@@ -4,14 +4,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#include <TStyle.h>
-#include <TCanvas.h>
-#include <TH2F.h>
-#include <TPaveStats.h>
-#include <THStack.h>
-#include <TLegend.h>
-#include <TLatex.h>
-#include <RD53Style.h>
+#include <plotWithRoot.h>
+
 
 int main(int argc, char *argv[]) { //./plotWithRoot_Occupancydir Directory_name
 	if (argc < 2) {
@@ -45,9 +39,9 @@ int main(int argc, char *argv[]) { //./plotWithRoot_Occupancydir Directory_name
 
 		if ( strstr( file_path, "OccupancyMap") != NULL) { //if filename contains string declared in argument.
 			if (strstr(file_path, ".dat") != NULL) {
-				
+
 				chipnum = file_name.substr(0, file_name.find(delimiter)); //get chip # from file name
-				
+
 				std::cout << "Opening file: " << filepath.c_str() << std::endl;
 				std::string filename = filepath.c_str();
 				std::fstream infile(filepath.c_str(), std::ios::in);
@@ -68,11 +62,11 @@ int main(int argc, char *argv[]) { //./plotWithRoot_Occupancydir Directory_name
 
 				std::getline(infile, type);
 				std::getline(infile, name);
-				std::getline(infile, line); //skip line
-				std::getline(infile, line);
-				std::getline(infile, line);
-				std::getline(infile, line);
-				std::getline(infile, line); 
+				std::getline(infile, line); //skip "Column"
+				std::getline(infile, line); //skip "Rows"
+				std::getline(infile, line); //skip "Hits"
+				std::getline(infile, line); //skip x range
+				std::getline(infile, line); //skip y range
 
 				rowno = 192;
 				colno = 400;
@@ -97,22 +91,17 @@ int main(int argc, char *argv[]) { //./plotWithRoot_Occupancydir Directory_name
 
 				TH1 *h_Syn = NULL;
 				h_Syn = (TH1*) new TH1F("h_Syn", "", xbins, xlow, xhigh);
-				h_Syn->GetXaxis()->SetTitle(xaxistitle.c_str());
-				h_Syn->GetYaxis()->SetTitle(yaxistitle.c_str());
 
 				TH1 *h_Lin = NULL;
 				h_Lin = (TH1*) new TH1F("h_Lin","", xbins, xlow, xhigh);
-				h_Lin->GetXaxis()->SetTitle(xaxistitle.c_str());
-				h_Lin->GetYaxis()->SetTitle(yaxistitle.c_str());
 
 				TH1 *h_Diff = NULL;
 				h_Diff = (TH1*) new TH1F("h_Diff","", xbins, xlow, xhigh);
-				h_Diff->GetXaxis()->SetTitle(xaxistitle.c_str());
-				h_Diff->GetYaxis()->SetTitle(yaxistitle.c_str());
 
-				int zero_S=0, less98_S=0, less100_S=0, hundred_S=0, less102_S=0, more102_S=0;
-				int zero_L=0, less98_L=0, less100_L=0, hundred_L=0, less102_L=0, more102_L=0;
-				int zero_D=0, less98_D=0, less100_D=0, hundred_D=0, less102_D=0, more102_D=0;
+				THStack *hs = new THStack("hs","");
+
+				TH1* fe_hist[3] = {h_Syn, h_Lin, h_Diff};
+				const char *LabelName[6] = {"0%", " 0-98%", " 98-100%", "100%", "100-102%", " >102%"};
 
 				for (int i=0; i<rowno; i++) {
 					for (int j=0; j<colno; j++) {
@@ -121,101 +110,31 @@ int main(int argc, char *argv[]) { //./plotWithRoot_Occupancydir Directory_name
 						infile >> tmp;
 						//std::cout << i*j << " " << tmp << std::endl;
 
-						if (tmp == 0 ){
-							if (j<128) {
-								zero_S++;
-								h_Syn->SetBinContent(1, zero_S);
-							} else if (j>=128 && j<264) {
-								zero_L++;	
-								h_Lin->SetBinContent(1, zero_L);
-							} else if (j>=264) {
-								zero_D++;
-								h_Diff->SetBinContent(1, zero_D);
-							}	
-						} else if (tmp > 0  && tmp < 98.0) {
-							if (j<128) {
-								less98_S++;
-								h_Syn->SetBinContent(2, less98_S);
-							} else if (j>=128 && j<264) {
-								less98_L++;	
-								h_Lin->SetBinContent(2, less98_L);
-							} else if (j>=264) {
-								less98_D++;
-								h_Diff->SetBinContent(2, less98_D);
-							}	
-						} else if (tmp >= 98.0 && tmp < 100) {
-							if (j<128) {
-								less100_S++;
-								h_Syn->SetBinContent(3, less100_S);
-							} else if (j>=128 && j<264) {
-								less100_L++;	
-								h_Lin->SetBinContent(3, less100_L);
-							} else if (j>=264) {
-								less100_D++;
-								h_Diff->SetBinContent(3, less100_D);
-							}	
-						} else if (tmp == 100) {
-							if (j<128) {
-								hundred_S++;
-								h_Syn->SetBinContent(4, hundred_S);
-							} else if (j>=128 && j<264) {
-								hundred_L++;	
-								h_Lin->SetBinContent(4, hundred_L);
-							} else if (j>=264) {
-								hundred_D++;
-								h_Diff->SetBinContent(4, hundred_D);
-							}	
-						} else if (tmp > 100 && tmp <= 102.0) {
-							if (j<128) {
-								less102_S++;
-								h_Syn->SetBinContent(5, less102_S);
-							} else if (j>=128 && j<264) {
-								less102_L++;	
-								h_Lin->SetBinContent(5, less102_L);
-							} else if (j>=264) {
-								less102_D++;
-								h_Diff->SetBinContent(5, less102_D);
-							}	
-						} else if (tmp >= 102.0) {
-							if (j<128) {
-								more102_S++;
-								h_Syn->SetBinContent(6, more102_S);
-							} else if (j>=128 && j<264) {
-								more102_L++;	
-								h_Lin->SetBinContent(6, more102_L);
-							} else if (j>=264) {
-								more102_D++;
-								h_Diff->SetBinContent(6, more102_D);
-							}	
-						}
+						double inj_value = 100;
+						int bin_num = whichBin(inj_value, tmp);
+						fe_hist[whichFE(j)]->AddBinContent(bin_num);
 					}
 				}
 
-				const char *LabelName[6] = {"0%", " 0-98%", " 98-100%", "100%", "100-102%", " >102%"};
-				for (int i=1; i<=6; i++) h_Syn->GetXaxis()->SetBinLabel(i,LabelName[i-1]);	
+				for(int i=0; i<3; i++){
+					style_TH1(fe_hist[i], xaxistitle.c_str(), yaxistitle.c_str());
+					for (int j=1; j<=xbins; j++) fe_hist[i]->GetXaxis()->SetBinLabel(j,LabelName[j-1]);
+					fe_hist[i]->GetXaxis()->LabelsOption("h");
+					fe_hist[i]->GetXaxis()->SetLabelSize(0.065);
+					fe_hist[i]->GetYaxis()->SetLabelSize(0.045);
+
+				}
+
 				//Synchronous FE Plot	
-				h_Syn->GetXaxis()->LabelsOption("h");
-				h_Syn->GetXaxis()->SetLabelSize(0.065);
-				h_Syn->GetYaxis()->SetLabelSize(0.045);
 				h_Syn->SetFillColor(kRed);
 				h_Syn->SetLineColor(kRed);
-				h_Syn->SetStats(0);
-				h_Syn->GetXaxis()->SetTitleSize(0.045);
-				h_Syn->GetXaxis()->SetTitleOffset(1.25);
-				h_Syn->GetYaxis()->SetTitleSize(0.05);
-				h_Syn->GetYaxis()->SetTitleOffset(1.5);
 				TCanvas *c_Syn = new TCanvas("c_Syn", "c_Syn", 800, 600);
-				c_Syn->SetLeftMargin(0.15);
-				c_Syn->SetRightMargin(0.05);
-				c_Syn->SetBottomMargin(0.1225);
+				style_TH1canvas(c_Syn);
 				h_Syn->Draw();
 				h_Syn->SetMarkerSize(1.8);
 				h_Syn->Draw("TEXT0 SAME");
 				TLatex *tname= new TLatex();
-				tname->SetNDC();
-				tname->SetTextAlign(22);
-				tname->SetTextFont(73);
-				tname->SetTextSizePixels(30);
+				latex_Chip(tname);
 				tname->DrawLatex(0.21,0.93,"RD53A");
 				tname->DrawLatex(0.88, 0.93, chipnum.c_str());
 				TLegend *syn_legend = new TLegend(0.7,0.77,0.88,0.88);
@@ -227,21 +146,10 @@ int main(int argc, char *argv[]) { //./plotWithRoot_Occupancydir Directory_name
 				c_Syn->Print(filename1.c_str());
 
 				//Linear FE Plot
-				for (int i=1; i<=6; i++) h_Lin->GetXaxis()->SetBinLabel(i,LabelName[i-1]);
-				h_Lin->GetXaxis()->LabelsOption("h");
-				h_Lin->GetXaxis()->SetLabelSize(0.065);
-				h_Lin->GetYaxis()->SetLabelSize(0.045);
 				h_Lin->SetFillColor(kGreen+2);
 				h_Lin->SetLineColor(kGreen+2);
-				h_Lin->SetStats(0);
-				h_Lin->GetXaxis()->SetTitleSize(0.045);
-				h_Lin->GetXaxis()->SetTitleOffset(1.25);
-				h_Lin->GetYaxis()->SetTitleSize(0.05);
-				h_Lin->GetYaxis()->SetTitleOffset(1.5);
 				TCanvas *c_Lin = new TCanvas("c_Lin", "c_Lin", 800, 600);
-				c_Lin->SetLeftMargin(0.15);
-				c_Lin->SetRightMargin(0.05);
-				c_Lin->SetBottomMargin(0.1225);
+				style_TH1canvas(c_Lin);
 				h_Lin->Draw();
 				h_Lin->SetMarkerSize(1.8);
 				h_Lin->Draw("TEXT0 SAME");
@@ -256,21 +164,10 @@ int main(int argc, char *argv[]) { //./plotWithRoot_Occupancydir Directory_name
 				c_Lin->Print(filename2.c_str());
 
 				//Diff FE Plot
-				for (int i=1; i<=6; i++) h_Diff->GetXaxis()->SetBinLabel(i,LabelName[i-1]);
-				h_Diff->GetXaxis()->LabelsOption("h");
-				h_Diff->GetXaxis()->SetLabelSize(0.065);
-				h_Diff->GetYaxis()->SetLabelSize(0.045);
 				h_Diff->SetFillColor(kBlue);
 				h_Diff->SetLineColor(kBlue);
-				h_Diff->SetStats(0);
-				h_Diff->GetXaxis()->SetTitleSize(0.045);
-				h_Diff->GetXaxis()->SetTitleOffset(1.25);
-				h_Diff->GetYaxis()->SetTitleSize(0.05);
-				h_Diff->GetYaxis()->SetTitleOffset(1.5);
 				TCanvas *c_Diff = new TCanvas("c_Diff", "c_Diff", 800, 600);
-				c_Diff->SetLeftMargin(0.15);
-				c_Diff->SetRightMargin(0.05);
-				c_Diff->SetBottomMargin(0.1225);
+				style_TH1canvas(c_Diff);
 				h_Diff->Draw();
 				h_Diff->SetMarkerSize(1.8);
 				h_Diff->Draw("TEXT0 SAME");
@@ -285,7 +182,6 @@ int main(int argc, char *argv[]) { //./plotWithRoot_Occupancydir Directory_name
 				c_Diff->Print(filename3.c_str());
 
 				//Stack Plot for all 3 FEs
-				THStack *hs = new THStack("hs","");
 				hs->Add(h_Syn);
 				hs->Add(h_Lin);
 				hs->Add(h_Diff);
@@ -296,14 +192,7 @@ int main(int argc, char *argv[]) { //./plotWithRoot_Occupancydir Directory_name
 				hs->Draw(); 
 				//hs->Draw("TEXt0 SAME"); //Works, but the text layers on top of each other if the values are too low
 				//Setting the title for THStack plots has to be after Draw(), and needs canvas->Modified() after
-				hs->GetXaxis()->SetTitle(xaxistitle.c_str());
-				hs->GetYaxis()->SetTitle(yaxistitle.c_str());
-				hs->GetXaxis()->SetLabelSize(0.065);
-				hs->GetYaxis()->SetLabelSize(0.045);
-				hs->GetXaxis()->SetTitleSize(0.045);
-				hs->GetXaxis()->SetTitleOffset(1.25);
-				hs->GetYaxis()->SetTitleSize(0.05);
-				hs->GetYaxis()->SetTitleOffset(1.5);
+				style_THStack(hs, xaxistitle.c_str(), yaxistitle.c_str());	
 				c_Stack->Modified();
 				gStyle->SetOptStat(0);
 				TLegend *stack_legend = new TLegend(0.7,0.65,0.88,0.88);
