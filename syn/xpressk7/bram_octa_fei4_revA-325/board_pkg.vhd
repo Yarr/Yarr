04c2509a460570392edf147b2d7a512523f0d796
Timon Heim <timon.heim@cern.ch>
2018-07-27 17:15:26 +0200

    Adding correct constants

--- syn/xpressk7/bram_octa_fei4_revA-325/board_pkg.vhd
+++ syn/xpressk7/bram_octa_fei4_revA-325/board_pkg.vhd
@@ -11,5 +11,7 @@ package board_pkg is
     constant c_TX_IDLE_WORD : std_logic_vector(31 downto 0) := x"00000000";
     constant c_TX_SYNC_WORD : std_logic_vector(31 downto 0) := x"00000000";
     constant c_TX_SYNC_INTERVAL : unsigned(7 downto 0) := to_unsigned(31,8);
+    constant c_tx_az_word : std_logic_vector(31 downto 0) := x"0";
+    constant c_tx_az_interval : unsigned(15 downto 0) := to_unsigned(500,16);
 end board_pkg;
 
