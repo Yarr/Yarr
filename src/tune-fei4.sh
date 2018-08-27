68a5a688e4334a8a2a08b6663d37bb4328167a13
Timon Heim <timon.heim@cern.ch>
2018-08-27 14:10:35 -0700

    Fixed switched parameters

--- src/tune-fei4.sh
+++ src/tune-fei4.sh
@@ -7,12 +7,12 @@ fi
 bin/scanConsole -s digitalscan -r $4 -c ${@:5} -p -m 1
 bin/scanConsole -s analogscan -r $4 -c ${@:5} -p
 bin/scanConsole -s tune_globalthreshold -t $1 -r $4 -c ${@:5} -p
-bin/scanConsole -s tune_globalpreamp -t $2 $3 -r $4 -c ${@:5} -p
+bin/scanConsole -s tune_globalpreamp -t $3 $2 -r $4 -c ${@:5} -p
 bin/scanConsole -s tune_globalthreshold -t $1 -r $4 -c ${@:5} -p
 bin/scanConsole -s tune_pixelthreshold -t $1 -r $4 -c ${@:5} -p
-bin/scanConsole -s tune_globalpreamp -t $2 $3 -r $4 -c ${@:5} -p
-bin/scanConsole -s tune_pixelpreamp -t $2 $3 -r $4 -c ${@:5} -p
+bin/scanConsole -s tune_globalpreamp -t $3 $2 -r $4 -c ${@:5} -p
+bin/scanConsole -s tune_pixelpreamp -t $3 $2 -r $4 -c ${@:5} -p
 bin/scanConsole -s tune_pixelthreshold -t $1 -r $4 -c ${@:5} -p
 bin/scanConsole -s noisescan -r $4 -c ${@:5} -p
-bin/scanConsole -s totscan -t $2 $3 -r $4 -c ${@:5} -p
+bin/scanConsole -s totscan -t $3 $2 -r $4 -c ${@:5} -p
 bin/scanConsole -s thresholdscan -r $4 -c ${@:5} -p
