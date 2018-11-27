4e5b6f7a5fe58af140b0a44b2604bc42a20e4105
Aleksandra Dimitrievska <adimitri@CERN.CH>
2018-11-27 23:49:51 +0100

    adding script for measurig current

---
+++ src/measure_Current.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Measuring currnent for a specific loaded configuration"
+echo "Need to have labRemote, change the path to the repository"
+
+cd ~/labRemote/build
+
+#Analog
+AnalogI=`./bin/agilent_measure -c 1 -p /dev/ttyUSB0 -g 3 get-current`
+#AnalogI=`./bin/rigol_measure -c 1 -p /dev/usbtmc0 get-current`
+#Digital
+DigitalI=`./bin/agilent_measure -c 2 -p /dev/ttyUSB0 -g 3 get-current`
+#DigitalI=`./bin/rigol_measure -c 2 -p /dev/usbtmc0 get-current`
+
+echo "Analog current is ${AnalogI}"
+echo "Digital current is ${DigitalI}"
+
+
+
+exit 0
+
