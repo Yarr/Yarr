88d5c38004452d6baf4c78c0e625399e5a655bc4
Lingxin Meng <lmeng@cern.ch>
2018-11-30 13:17:42 -0800

    Typo

--- src/generateConfig.sh
+++ src/generateConfig.sh
@@ -29,7 +29,7 @@ cp configs/connectivity/example_rd53a_setup.json configs/connectivity/example_rd
 cp configs/connectivity/example_rd53a_setup.json configs/connectivity/example_rd53a_IL55.json
 
 value=`grep configs configs/connectivity/example_rd53a_setup.json | grep -o "configs/.*.json"`
-sed -i "s:${value}:\"configs/rd53a_${5}_OL30.json\":g" configs/connectivity/example_rd53a_OL30.json
+sed -i "s:\${value}:\"configs/rd53a_${5}_OL30.json\":g" configs/connectivity/example_rd53a_OL30.json
 sed -i "s:\${value}:\"configs/rd53a_${5}_OL35.json\":g" configs/connectivity/example_rd53a_OL35.json
 sed -i "s:\${value}:\"configs/rd53a_${5}_OL40.json\":g" configs/connectivity/example_rd53a_OL40.json
 sed -i "s:\${value}:\"configs/rd53a_${5}_IL45.json\":g" configs/connectivity/example_rd53a_IL45.json
