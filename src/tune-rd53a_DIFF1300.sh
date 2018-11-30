e289ef6744cea69e3debd269eb5d29446725d293
Lingxin Meng <lmeng@cern.ch>
2018-11-30 12:36:11 -0800

    Add missing output path

--- src/tune-rd53a_DIFF1300.sh
+++ src/tune-rd53a_DIFF1300.sh
@@ -20,4 +20,4 @@ scripts/plotWithRoot_ThresholdTDAC ${3}/last_scan pdf 1
 scripts/plotWithRoot_NoiseMap ${3}/last_scan pdf 1
 ./bin/scanConsole -r $1 -c $2 -s configs/scans/rd53a/std_totscan.json -p -m 0 -t 6000 -o $3
 scripts/plotWithRoot_ToT ${3}/last_scan pdf 1
-./bin/scanConsole -r $1 -c $2 -s configs/scans/rd53a/std_noisescan.json -p
+./bin/scanConsole -r $1 -c $2 -s configs/scans/rd53a/std_noisescan.json -p -o $3
