e6728cb804e0601abdff5f47e33dc937096b3164
Aleksandra Dimitrievska <adimitri@CERN.CH>
2018-11-30 09:18:28 -0800

    fixing path for ploting

--- src/tune-rd53a_LIN1000.sh
+++ src/tune-rd53a_LIN1000.sh
@@ -13,10 +13,13 @@
 ./bin/scanConsole -r $1 -c $2 -s configs/scans/rd53a/lin_retune_pixelthreshold.json -t $4 -p -m 0 -o $3
 
 ./bin/scanConsole -r $1 -c $2 -s configs/scans/rd53a/lin_thresholdscan.json -p -m 0 -o $3
-scripts/plotWithRoot_Threshold data/last_scan pdf 1
-scripts/plotWithRoot_ThresholdTDAC data/last_scan pdf 1
+scripts/plotWithRoot_Threshold ${3}/last_scan pdf 1
+scripts/plotWithRoot_ThresholdTDAC ${3}/last_scan pdf 1
+scripts/plotWithRoot_NoiseMap ${3}/last_scan pdf 1
 ./bin/scanConsole -r $1 -c $2 -s configs/scans/rd53a/lin_intimethresholdscan.json -p -m 0 -o $3
-scripts/plotWithRoot_Threshold data/last_scan pdf 1
-scripts/plotWithRoot_ThresholdTDAC data/last_scan pdf 1
+scripts/plotWithRoot_Threshold ${3}/last_scan pdf 1
+scripts/plotWithRoot_ThresholdTDAC ${3}/last_scan pdf 1
+scripts/plotWithRoot_NoiseMap ${3}/last_scan pdf 1
 ./bin/scanConsole -r $1 -c $2 -s configs/scans/rd53a/std_totscan.json -p -m 0 -t 6000 -o $3
+scripts/plotWithRoot_ToT ${3}/last_scan pdf 1
 ./bin/scanConsole -r $1 -c $2 -s configs/scans/rd53a/std_noisescan.json -p -o $3
