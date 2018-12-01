26af88bb0fb436ee6804d804b3199c2e9b65dabe
Timon Heim <timon.heim@cern.ch>
2018-11-30 17:29:18 -0800

    Adding overdrive

--- src/run_AFE-measurement.sh
+++ src/run_AFE-measurement.sh
@@ -12,41 +12,104 @@ fi
 TargetTh_IL=1000
 TargetTh_OL=1300
 
+OvrDrvSyn30OL=0
+OvrDrvSyn35OL=0
+OvrDrvSyn40OL=0
+OvrDrvSyn40IL=0
+OvrDrvSyn50IL=0
+OvrDrvSyn55IL=0
+
+OvrDrvLin30OL=300
+OvrDrvLin35OL=300
+OvrDrvLin40OL=300
+OvrDrvLin40IL=200
+OvrDrvLin50IL=200
+OvrDrvLin55IL=200
+
+OvrDrvDiff30OL=0
+OvrDrvDiff35OL=0
+OvrDrvDiff40OL=0
+OvrDrvDiff40IL=0
+OvrDrvDiff50IL=0
+OvrDrvDiff55IL=0
+
 if [ ${2} == "Sensor50" ]
 then
 TargetTh_IL=1200
 TargetTh_OL=1500
+
+OvrDrvSyn30OL=350
+OvrDrvSyn35OL=230
+OvrDrvSyn40OL=100
+OvrDrvSyn40IL=0
+OvrDrvSyn50IL=0
+OvrDrvSyn55IL=0
+
+OvrDrvLin30OL=600
+OvrDrvLin35OL=600
+OvrDrvLin40OL=500
+OvrDrvLin40IL=400
+OvrDrvLin50IL=350
+OvrDrvLin55IL=300
+
+OvrDrvDiff30OL=100
+OvrDrvDiff35OL=70
+OvrDrvDiff40OL=70
+OvrDrvDiff40IL=0
+OvrDrvDiff50IL=0
+OvrDrvDiff55IL=0
 fi
 
 if [ ${2} == "Sensor25" ]
 then
 TargetTh_IL=1200
 TargetTh_OL=1500
+
+OvrDrvSyn30OL=350
+OvrDrvSyn35OL=230
+OvrDrvSyn40OL=100
+OvrDrvSyn40IL=0
+OvrDrvSyn50IL=0
+OvrDrvSyn55IL=0
+
+OvrDrvLin30OL=600
+OvrDrvLin35OL=600
+OvrDrvLin40OL=500
+OvrDrvLin40IL=400
+OvrDrvLin50IL=350
+OvrDrvLin55IL=300
+
+OvrDrvDiff30OL=100
+OvrDrvDiff35OL=70
+OvrDrvDiff40OL=70
+OvrDrvDiff40IL=0
+OvrDrvDiff50IL=0
+OvrDrvDiff55IL=0
 fi
 
 echo "Target Threshold for inner layers is ${TargetTh_IL} and for outer layers ${TargetTh_OL}"
 
 echo "Tune DIFF FE, Vddd trimed at 1.2 V"
 
-./tune-rd53a_DIFF1000.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_IL45.json $1 $[${TargetTh_IL}+50]
-./measure_Current.sh
-./tune-rd53a_DIFF1000.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_IL50.json $1 $[${TargetTh_IL}+50]
-./measure_Current.sh
-./tune-rd53a_DIFF1300.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_OL30.json $1 $[${TargetTh_OL}+50]
-./measure_Current.sh
-./tune-rd53a_DIFF1300.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_OL35.json $1 $[${TargetTh_OL}+50]
-./measure_Current.sh
+./tune-rd53a_DIFF1000.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_IL45.json $1 $[${TargetTh_IL}-OvrDrvDiff40IL]
+#./measure_Current.sh
+./tune-rd53a_DIFF1000.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_IL50.json $1 $[${TargetTh_IL}-OvrDrvDiff50IL]
+#./measure_Current.sh
+./tune-rd53a_DIFF1300.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_OL30.json $1 $[${TargetTh_OL}-OvrDrvDiff30OL]
+#./measure_Current.sh
+./tune-rd53a_DIFF1300.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_OL35.json $1 $[${TargetTh_OL}-OvrDrvDiff35OL]
+#./measure_Current.sh
 
 echo "Tune SYNC FE, Vddd trimed at 1.2 V"
 
-./tune-rd53a_SYN1000.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_IL45.json $1 ${TargetTh_IL}
-./measure_Current.sh
-./tune-rd53a_SYN1000.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_IL50.json $1 ${TargetTh_IL}
-./measure_Current.sh
-./tune-rd53a_SYN1300.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_OL30.json $1 ${TargetTh_OL}
-./measure_Current.sh
-./tune-rd53a_SYN1300.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_OL35.json $1 ${TargetTh_OL}
-./measure_Current.sh
+./tune-rd53a_SYN1000.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_IL45.json $1 $[${TargetTh_IL}-OvrDrvSyn40IL]
+#./measure_Current.sh
+./tune-rd53a_SYN1000.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_IL50.json $1 $[${TargetTh_IL}-OvrDrvSyn50IL]
+#./measure_Current.sh
+./tune-rd53a_SYN1300.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_OL30.json $1 $[${TargetTh_OL}-OvrDrvSyn30OL]
+#./measure_Current.sh
+./tune-rd53a_SYN1300.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_OL35.json $1 $[${TargetTh_OL}-OvrDrvSyn35OL]
+#./measure_Current.sh
 
 
 echo "Tune LIN FE, Vddd trimed to 1.2 V"
@@ -66,14 +129,14 @@ if [ "$#" -gt 2 ]; then
         value=`grep SldoDigitalTrim configs/rd53a_${2}_IL55.json | grep -o '[0-9]*'`
 	sed -i "s/\"SldoDigitalTrim\": ${value}/\"SldoDigitalTrim\": ${3}/g" configs/rd53a_${2}_IL55.json
 fi
- 
-./tune-rd53a_LIN1000.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_IL45.json $1 ${TargetTh_IL}
-./measure_Current.sh
-./tune-rd53a_LIN1000.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_IL50.json $1 ${TargetTh_IL}
-./measure_Current.sh
-./tune-rd53a_LIN1300.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_OL30.json $1 ${TargetTh_OL}
-./measure_Current.sh
-./tune-rd53a_LIN1300.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_OL35.json $1 ${TargetTh_OL}
-./measure_Current.sh
+
+./tune-rd53a_LIN1000.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_IL45.json $1 $[${TargetTh_IL}-OvrDrvLin40IL]
+#./measure_Current.sh
+./tune-rd53a_LIN1000.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_IL50.json $1 $[${TargetTh_IL}-OvrDrvLin50IL]
+#./measure_Current.sh
+./tune-rd53a_LIN1300.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_OL30.json $1 $[${TargetTh_OL}-OvrDrvLin30OL]
+#./measure_Current.sh
+./tune-rd53a_LIN1300.sh configs/controller/specCfg.json configs/connectivity/example_rd53a_OL35.json $1 $[${TargetTh_OL}-OvrDrvLin35OL]
+#./measure_Current.sh
 
 
