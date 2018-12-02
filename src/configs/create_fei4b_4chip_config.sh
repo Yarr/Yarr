3b813f602054946083433d42ccc2cf8b0b6a37e6
Eunchong Kim <eunchong.kim@cern.ch>
2018-12-02 19:21:37 +0900

    Modified for versatility

--- src/configs/create_fei4b_4chip_config.sh
+++ src/configs/create_fei4b_4chip_config.sh
@@ -3,6 +3,10 @@
 mod_id=$1
 now=`date +"%y%m%d%H%M"`
 
+# Change fixed tx channel and rx start channel
+tx_fix=4
+rx_start=4
+
 for i in 1 2 3 4
 do
     if [ -e ${mod_id}_fei4b_chipId$i.json ]; then
@@ -12,9 +16,9 @@ do
     sed -i "/chipId/s/0/$i/g" ${mod_id}_fei4b_chipId$i.json
     sed -i "/            \"rxChannel\": 99,/d" ${mod_id}_fei4b_chipId$i.json
     sed -i "/            \"txChannel\": 99,/d" ${mod_id}_fei4b_chipId$i.json
-    j=`expr $i - 1 + 4`
+    j=`expr $i - 1 + $rx_start`
     sed -i "/rxChannel/s/0/$j/g" ${mod_id}_fei4b_chipId$i.json
-    sed -i "/txChannel/s/0/4/g" ${mod_id}_fei4b_chipId$i.json
+    sed -i "/txChannel/s/0/$tx_fix/g" ${mod_id}_fei4b_chipId$i.json
     sed -i "/\"name\"/s/Example/${mod_id}_chipId$i/g" ${mod_id}_fei4b_chipId$i.json
 done
 
@@ -22,6 +26,5 @@ sed "s/__MODULE_ID__/${mod_id}/g" connectivity/fei4module_default.json > connect
 
 cd ../
 
-source ../setup.sh
-
+# Register module and 4 chips component to DB
 ./bin/dbRegisterComponent configs/connectivity/${mod_id}_fei4module.json
