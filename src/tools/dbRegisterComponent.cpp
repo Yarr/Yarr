740571f035dc29b4124078b30ac6e2c083d87eb6
ArisaKubota <arisa.kubota@cern.ch>
2019-01-19 01:59:45 +0900

    Modified to use test run info json in create shellscript

--- src/tools/dbRegisterComponent.cpp
+++ src/tools/dbRegisterComponent.cpp
@@ -10,20 +10,69 @@
 
 #include "Database.h"
 
+void printHelp();
+
 int main(int argc, char *argv[]){
-	if(argc == 1) {
-		std::cout << "No imput json name" << std::endl;
-		return -1;
-	}
-	std::cout << "json : " << argv[1] << std::endl;
+
+  std::string dbInfoJsonPath = "";
+	std::string dbConnPath = "";
+  
+  int c;
+  while ((c = getopt(argc, argv, "hc:I:")) != -1 ){
+    switch (c) {
+        case 'h':
+            printHelp();
+            return 0;
+            break;
+        case 'c':
+            dbConnPath = std::string(optarg);
+            break;
+        case 'I':
+            dbInfoJsonPath = std::string(optarg);
+            break;
+        case '?':
+            if(optopt == 'I'){
+                std::cerr << "-> Option " << (char)optopt
+                          << " requires a parameter! (Proceeding with default)"
+                          << std::endl;
+            }else if(optopt == 'c'){
+                std::cerr << "-> Option " << (char)optopt
+                          << " requires a parameter! Aborting... " << std::endl;
+                return -1;
+            } else {
+                std::cerr << "-> Unknown parameter: " << (char)optopt << std::endl;
+            }
+            break;
+        default:
+            std::cerr << "-> Error while parsing command line parameters!" << std::endl;
+            return -1;
+    }
+  }
+
+  if (dbConnPath.size() == 0) {
+      std::cerr << "Error: no connectivity config file given, please specify file name under -c option!" << std::endl;
+      std::cerr << "       create config files by running configs/create_fei4b_4chip_config.sh if file does not exist." << std::endl;
+      return -1;
+  }
+
+	std::cout << "connecitivity config file : " << dbConnPath << std::endl;
+	std::cout << "environmental config file : " << dbInfoJsonPath << std::endl;
 	std::string collection = "component";
-	std::string json = argv[1];
 
   Database *database = new Database();
-	database->registerFromConnectivity(json);
+  database->setTestRunInfo(dbInfoJsonPath);
+	database->registerFromConnectivity(dbConnPath);
 
   delete database;
 
 	return 0;
 }
+
+void printHelp() {
+    std::cout << "Help:" << std::endl;
+    std::cout << " -h: Shows this." << std::endl;
+    std::cout << " -c <conn.json> : Provide connectivity configuration." << std::endl;
+    std::cout << " -I <user.json> : Provide environmental configuration, default user = $USER, institution = $HOSTNAME." << std::endl;
+}
+
 
