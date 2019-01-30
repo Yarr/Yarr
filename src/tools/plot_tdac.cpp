6f583701e9f231156b678d650eb8483ee6456c00
Magne Lauritzen <mag.lauritzen@gmail.com>
2019-01-30 13:40:21 +0100

    Fixed incorrect row border between Linear and Differential. Cleaned up pixel indexing.

--- src/tools/plot_tdac.cpp
+++ src/tools/plot_tdac.cpp
@@ -21,18 +21,17 @@ int main(int argc, char *argv[]) {
     Histo1d h1("TDacDistLin", 16, -0.5, 15.5, typeid(void));
     Histo1d h2("TDacDistDiff", 31, -15.5, 15.5, typeid(void));
     Histo2d h3("TDacMap", 400, 0.5, 400.5, 192, 0.5, 192.5, typeid(void));
-    for (unsigned col = 129; col<400; col++) {
-        for (unsigned row = 1; row<=192; row++) {
-            int tdac = cfg["RD53A"]["PixelConfig"][col-1]["TDAC"][row-1];
-	    if (col<254){
-		h1.fill(tdac);     
+    for (unsigned col = 128; col<400; col++) {
+        for (unsigned row = 0; row<192; row++) {
+            int tdac = cfg["RD53A"]["PixelConfig"][col]["TDAC"][row];
+	    if (col<264){
+            h1.fill(tdac);
 	    }else{
-		h2.fill(tdac);
+            h2.fill(tdac);
 	    }
             h3.fill(col, row, tdac);
         }
     }
-    
     h1.plot("config", "");
     h2.plot("config", "");
     h3.plot("config", "");
