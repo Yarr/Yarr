b0a98be6ab8626ace7cca3e16be1db169fcd42f0
Timon Heim <timon.heim@cern.ch>
2019-02-24 21:25:43 -0800

    Update tuning script

--- src/tune-rd53a.sh
+++ src/tune-rd53a.sh
@@ -10,22 +10,32 @@ fi
 #./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/std_thresholdscan.json -p -m
 #./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/std_totscan.json -p -m 0 -t $3
 
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/diff_tune_globalthreshold.json -t $2 -p -m 0
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/diff_tune_pixelthreshold.json -t $2 -p -m 0
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/diff_tune_globalpreamp.json -t $3 $4 -p -m 0
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/diff_tune_pixelthreshold.json -t $2 -p -m 0
+# Tune diff FE
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/diff_tune_globalthreshold.json -t $2 -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/diff_tune_pixelthreshold.json -t $2 -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/diff_tune_globalpreamp.json -t $3 $4 -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/diff_tune_pixelthreshold.json -t $2 -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/diff_tune_finepixelthreshold.json -t $2 -p
 
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_tune_globalthreshold.json -t $1 -p -m 0
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_tune_pixelthreshold.json -t $1 -p -m 0
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_retune_globalthreshold.json -t $2 -p -m 0
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_retune_pixelthreshold.json -t $2 -p -m 0
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_tune_globalpreamp.json -t $3 $4 -p -m 0
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_retune_pixelthreshold.json -t $2 -p -m 0
+# Tune lin FE
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_tune_globalthreshold.json -t $1 -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_tune_pixelthreshold.json -t $1 -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_retune_globalthreshold.json -t $2 -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_retune_pixelthreshold.json -t $2 -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_tune_globalpreamp.json -t $3 $4 -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_retune_pixelthreshold.json -t $2 -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/lin_tune_finepixelthreshold.json -t $2 -p
 
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/syn_tune_globalthreshold.json -t $2 -p -m 0
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/syn_tune_globalpreamp.json -t $3 $4 -p -m 0
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/syn_tune_globalthreshold.json -t $2 -p -m 0
+# Tune sync FE
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/syn_tune_globalthreshold.json -t $2 -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/syn_tune_globalpreamp.json -t $3 $4 -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/syn_tune_globalthreshold.json -t $2 -p
 
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/std_thresholdscan.json -p -m 0
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/std_totscan.json -p -m 0 -t $3
-./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/std_noisescan.json -p
+# Finals threshold & tot scans
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/std_thresholdscan.json -p
+./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/std_totscan.json -p -t $3
+
+# Create final mask for operation
+#./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/std_digitalscan.json -p -m 1
+#./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/std_analogscan.json -p
+#./bin/scanConsole -r $5 -c ${@:6} -s configs/scans/rd53a/std_noisescan.json -p
