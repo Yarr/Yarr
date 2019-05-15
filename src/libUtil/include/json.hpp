dc612bc23c345ea24de0fce139397683d5b17c5f
Timon Heim <timon.heim@cern.ch>
2019-05-15 09:04:40 -0700

    Modify to have pretty_print be more compact for arrays

--- src/libUtil/include/json.hpp
+++ src/libUtil/include/json.hpp
@@ -12172,31 +12172,20 @@ class serializer
 
                 if (pretty_print)
                 {
-                    o->write_characters("[\n", 2);
-
-                    // variable to hold indentation for recursive calls
-                    const auto new_indent = current_indent + indent_step;
-                    if (JSON_UNLIKELY(indent_string.size() < new_indent))
-                    {
-                        indent_string.resize(indent_string.size() * 2, ' ');
-                    }
+                    o->write_character('[');
 
                     // first n-1 elements
                     for (auto i = val.m_value.array->cbegin();
                             i != val.m_value.array->cend() - 1; ++i)
                     {
-                        o->write_characters(indent_string.c_str(), new_indent);
-                        dump(*i, true, ensure_ascii, indent_step, new_indent);
-                        o->write_characters(",\n", 2);
+                        dump(*i, true, ensure_ascii, indent_step, current_indent);
+                        o->write_character(',');
                     }
 
                     // last element
                     assert(not val.m_value.array->empty());
-                    o->write_characters(indent_string.c_str(), new_indent);
-                    dump(val.m_value.array->back(), true, ensure_ascii, indent_step, new_indent);
+                    dump(val.m_value.array->back(), true, ensure_ascii, indent_step, current_indent);
 
-                    o->write_character('\n');
-                    o->write_characters(indent_string.c_str(), current_indent);
                     o->write_character(']');
                 }
                 else
