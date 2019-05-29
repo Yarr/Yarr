a4c111171163a2f47e9ade37a379e76d3374006b
Bruce Gallop <bruce.gallop@cern.ch>
2019-05-29 09:38:23 -0700

    Read data (there should be HPR somewhere)

--- src/tools/star_test.cpp
+++ src/tools/star_test.cpp
@@ -39,5 +39,39 @@ int main(int argc, char *argv[]) {
     spec.writeFifo((LCB::IDLE << 16) + LCB::IDLE);
     spec.releaseFifo();
 
+    spec.setRxEnable(0x40); // Channe 6
+
+    RawData *data = spec.readData();
+
+    for(int i=0; i<1000; i++) {
+      if(data != nullptr) break;
+
+      data = spec.readData();
+    }
+
+    if(data == nullptr) {
+      std::cout << "No data\n";
+      return 1;
+    }
+
+    std::cout << data->adr << " " << data->buf << " " << data->words << "\n";
+
+    for (unsigned j=0; j<data->words;j++) {
+      auto word = data->buf[j];
+      if((j%2) && (word == 0xd3400000)) continue;
+      if(!(j%2) && ((word&0xff) == 0xff)) continue;
+
+      if((word&0xff) == 0x5f) continue;
+
+      if(word == 0x1a0d) continue; // Ilde on chan 6
+      if(word == 0x19f2) continue; // Ilde on chan 6
+
+      word &= 0xffffc3ff; // Strip of channel number
+
+      std::cout << "[" << j << "] = 0x" << std::hex << word << std::dec << " " << std::bitset<32>(word) << std::endl;
+    }
+
+    spec.setRxEnable(0x0);
+
     return 0;
 }
