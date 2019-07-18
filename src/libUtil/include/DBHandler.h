50d9131e8027837e73c2ee6d38be27a41920ba51
ArisaKubota <arisa.kubota@cern.ch>
2019-07-19 04:33:10 +0900

    Updating Local DB Scheme based on the latest YARR and separating the cache function from YARR

--- src/libUtil/include/DBHandler.h
+++ src/libUtil/include/DBHandler.h
@@ -22,21 +22,6 @@
 
 using json=nlohmann::basic_json<std::map, std::vector, std::string, bool, std::int32_t, std::uint32_t, float>;
 
-#ifdef MONGOCXX_INCLUDE // When use macro "-DMONGOCXX_INCLUDE" in makefile
-
-// mongocxx v3.2 driver
-#include <mongocxx/client.hpp>
-#include <mongocxx/exception/exception.hpp>
-#include <mongocxx/exception/error_code.hpp>
-#include <mongocxx/exception/logic_error.hpp>
-#include <mongocxx/exception/operation_exception.hpp>
-#include <mongocxx/exception/server_error_code.hpp>
-#include <mongocxx/instance.hpp>
-// openssl
-#include <openssl/sha.h>
-
-#endif // End of ifdef MONGOCXX_INCLUDE
-
 class DBHandler {
     public:
         DBHandler();
@@ -66,27 +51,10 @@ class DBHandler {
                    std::string i_type="error");
 
         /***
-        Check mongocxx library
-        ***/
-        int checkLibrary();
-
-        /***
-        Check the connection to Local DB
-        * i_db_cfg_path: path to database config
-        ***/
-        int checkConnection(std::string /*i_db_cfg_path*/);
-
-        /***
         Check cache directory/function 
         TODO to be implemented
         ***/
         int checkCacheStatus();
-
-        /***
-        Check module list in the Local DB or local cache directory 
-        TODO do be implemented
-        ***/
-        int checkModuleList();
 
         /***
         Setting function for using Database
@@ -134,130 +102,14 @@ class DBHandler {
                            std::string i_serial_number="");
 
         /***
-        Register data into Local DB from cache files storing in scanConsole 
-        mongocxx library is required.
-        * i_cache_dir: path to cache directory TODO to be automized by daemon
-        ***/
-        void setCache(std::string /*i_cache_dir*/);
-
-        /***
         Clean up veriables after scanConsole
         ***/
         void cleanUp(std::string i_dir="");
 
-        /***
-        Retrieve data from Local DB and Master Server
-        mongocxx library is required.
-        * i_info_file_path: path to retrieve information file
-        * i_get_type: retrieve type (testRunLog: test log, 
-           - testRunLog: test data log
-           - testRunId: test data id labeled by MongoDB
-           - scan: latest scan data TODO to be enabled to retrieve latest config file set for module
-           - config: config file for chip
-           - userId: user data id labeled by MongoDB
-           - dat: dat data for scan
-        ***/
-        void getData(std::string /*i_info_file_path*/,
-                     std::string /*i_get_type*/);
     protected:
-
-        /// register data from cache
-        void writeScan(std::string /*i_cache_dir*/);
-        void writeDCS(std::string /*i_cache_dir*/);
-
-        /// register data into DB
-        std::string registerUser(std::string /*i_user_name*/, 
-                                 std::string /*i_institution*/, 
-                                 std::string /*i_user_identity*/);
-        std::string  registerSite(std::string /*i_adress*/,
-                                  std::string /*i_name*/,
-                                  std::string /*i_site*/);
-        void registerConnCfg(std::vector<std::string> /*i_conn_paths*/);
-        std::string registerComponent(std::string /*i_serial_number*/,
-                                      std::string /*i_component_type*/,
-                                      int /*i_chip_id*/,
-                                      int /*i_chips*/);
-        void registerChildParentRelation(std::string /*i_parent_oid_str*/,
-                                         std::string /*i_child_oid_str*/,
-                                         int /*i_chip_id*/);
-        void registerConfig(std::string /*i_ctr_oid_str*/, 
-                            std::string /*i_file_path*/, 
-                            std::string /*i_filename*/,
-                            std::string /*i_title*/,
-                            std::string /*i_collection*/); 
-        void registerAttachment(std::string /*i_ctr_oid_str*/, 
-                                std::string /*i_file_path*/, 
-                                std::string /*i_histo_name*/);
-        std::string registerJsonCode(std::string /*i_file_path*/, 
-                                     std::string /*i_filename*/, 
-                                     std::string /*i_title*/, 
-                                     std::string /*i_type*/);
-        void registerComponentTestRun(std::string /*i_conn_path*/, 
-                                      std::string /*i_tr_oid_str*/, 
-                                      std::string /*i_test_type*/, 
-                                      int /*i_run_number*/);
-        std::string registerTestRun(std::string /*i_test_type*/, 
-                                    int /*i_run_number*/, 
-                                    int /*i_target_charge*/, 
-                                    int /*i_target_tot*/,
-                                    int /*i_time*/,
-                                    std::string /*i_serial_number*/,
-                                    std::string /*i_type*/,
-                                    std::string i_tr_oid_str="");
-        void registerDCS(std::string /*i_dcs_path*/,
-                         std::string /*i_tr_path*/);
-
-        std::string writeGridFsFile(std::string /*i_file_path*/, 
-                                    std::string /*i_filename*/);
-        std::string writeJsonCode_Msgpack(std::string /*i_file_path*/, 
-                                          std::string /*i_filename*/, 
-                                          std::string /*i_title*/);
-        std::string writeJsonCode_Gridfs(std::string /*i_file_path*/, 
-                                         std::string /*i_filename*/, 
-                                         std::string /*i_title*/);
-
-
-        /// get data from DB
-        std::string getValue(std::string /*i_collection_name*/, 
-                             std::string /*i_member_key*/, 
-                             std::string /*i_member_value*/, 
-                             std::string /*i_member_bson_type*/, 
-                             std::string /*i_key*/, 
-                             std::string i_bson_type="string");
-        std::string getComponent(std::string /*i_serial_number*/);
-
-        /// add value into document in DB
-        void addComment(std::string /*i_collection_name*/, 
-                        std::string /*i_oid_str*/, 
-                        std::string /*i_comment*/);
-        void addValue(std::string /*i_oid_str*/, 
-                      std::string /*i_collection_name*/, 
-                      std::string /*i_key*/, 
-                      std::string /*i_value*/,
-                      std::string i_type="string");
-        void addAttachment(std::string /*i_oid_str*/, 
-                           std::string /*i_collection_name*/, 
-                           std::string /*i_file_oid_str*/, 
-                           std::string /*i_title*/, 
-                           std::string /*i_description*/,
-                           std::string /*i_content_type*/, 
-                           std::string /*i_filename*/);
-        void addDefect(std::string /*i_oid_str*/, 
-                       std::string /*i_collection_name*/, 
-                       std::string /*i_defect_name*/, 
-                       std::string /*i_description*/);
-        void addUser(std::string /*i_collection_name*/, 
-                     std::string /*i_oid_str*/);
-        void addSys(std::string /*i_oid_str*/, 
-                    std::string /*i_collection_name*/);
-        void addVersion(std::string /*i_collection_name*/, 
-                        std::string /*i_member_key*/, 
-                        std::string /*i_member_value*/, 
-                        std::string /*i_member_bson_type*/);
-
-        /// Get SHA-1 hash
-        std::string getHash(std::string /*i_file_path*/);
- 
+        void getTestRunData(std::string /*i_tr_oid_str*/,
+                            std::string /*i_serial_number*/, 
+                            int /*i_time*/);
         /// chaching scheme
         void cacheUser(std::string /*i_user_path*/);
         void cacheSite(std::string /*i_address_path*/);
@@ -307,19 +159,6 @@ class DBHandler {
                          std::string /*i_key*/,
                          int /*i_num*/); 
 
-        /// retrieve function
-        void getTestRunLog(json /*i_info_json*/);
-        void getTestRunId(json /*i_info_json*/);
-        void getConfigId(json /*i_info_json*/);
-        void getUserId(json /*i_info_json*/);
-        void getDatData(json /*i_info_json*/);
-        void getConfigData(json /*i_info_json*/); 
-        void getScan(json /*i_info_json*/);
-
-        void getTestRunData(std::string /*i_tr_oid_str*/,
-                            std::string /*i_serial_number*/,
-                            int /*i_time*/);
-
         /// check json
         json toJson(std::string /*i_file_path*/,
                     std::string i_file_type="json");
@@ -334,13 +173,6 @@ class DBHandler {
         void mkdir(std::string /*i_dir_path*/);
       
     private:
-#ifdef MONGOCXX_INCLUDE // When use macro "-DMONGOCXX_INCLUDE" in makefile
-        // Mongo c++
-        mongocxx::client client;
-        mongocxx::database db;
-
-#endif // End of ifdef MONGOCXX_INCLUDE
-
         std::string m_option;
         std::string m_db_cfg_path;
         std::string m_user_oid_str;
