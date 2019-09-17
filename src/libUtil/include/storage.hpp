1a5061d6137e0bcd566aa08adfb19c8a1dc8313d
Matthias Wittgen <wittgen@rddev111.slac.stanford.edu>
2019-09-17 17:13:39 -0400

    add variant as external project

--- src/libUtil/include/storage.hpp
+++ src/libUtil/include/storage.hpp
@@ -1,6 +1,6 @@
 #ifndef _STORAGE_HPP_
 #define  _STORAGE_HPP_
-#if 0
+#if defined(USE_JSON)
 #include <json.hpp>
 using json=nlohmann::basic_json<std::map, std::vector, std::string, bool, std::int32_t, std::uint32_t, float>;
 #else
