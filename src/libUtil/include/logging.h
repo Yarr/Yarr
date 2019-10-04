fff9fcf3b40a768da83746c224a7b346a4fbf0f6
Bruce Gallop <bruce.gallop@cern.ch>
2019-10-04 14:22:52 +0100

    Add logging header which makes empty logger (no sink by default)

---
+++ src/libUtil/include/logging.h
@@ -0,0 +1,20 @@
+#ifndef YARR_LOGGING_H
+#define YARR_LOGGING_H
+
+#include <memory>
+
+#include "spdlog/logger.h"
+#include "spdlog/spdlog.h"
+
+namespace logging {
+
+// Make a logger without any sinks
+inline std::shared_ptr<spdlog::logger> make_log(std::string name) {
+  auto tmp = std::make_shared<spdlog::logger>(name, spdlog::sinks_init_list{});
+  spdlog::register_logger(tmp);
+  return tmp;
+}
+
+} // End logging namespace
+
+#endif
