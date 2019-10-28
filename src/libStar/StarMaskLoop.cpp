26fa4f6aeafa3f31697f0b3526845916dd232f44
Olivier Arnaez <olivier.arnaez@cern.ch>
2019-10-28 15:00:56 +0100

    Star: Implementing generic pattern building in StarMaskLoop

--- src/libStar/StarMaskLoop.cpp
+++ src/libStar/StarMaskLoop.cpp
@@ -2,59 +2,57 @@
 #include "StarMask_CalEn.h"
 
 StarMaskLoop::StarMaskLoop() : LoopActionBase() {
-    m_mask = MASK_STRIP_1;
     min = 0;
     max = 16;
     step = 1;
     m_cur = 0;
-    enable_sCap = false;
-    enable_lCap = false;
+    m_nMaskedStripsPerGroup=0;
+    m_nEnabledStripsPerGroup=0;
+    m_EnabledMaskedShift=0;
     loopType = typeid(this);
     verbose=false;
 }
 
 
+void StarMaskLoop::initMasks() {
+  uint32_t groupmask = (1 << m_nMaskedStripsPerGroup) -1;
+  uint32_t groupenabled = (1 << m_nEnabledStripsPerGroup) -1;
+  short pos=0;
+  while (pos<256) {
+    m_maskedChannelsRing.fill(groupmask, m_nMaskedStripsPerGroup);
+    pos += m_nMaskedStripsPerGroup;
+    m_maskedChannelsRing.fill(0x0, max-m_nMaskedStripsPerGroup);
+    pos += max-m_nMaskedStripsPerGroup;
+  }
+  for (pos=0; pos<m_EnabledMaskedShift; pos++) m_enabledChannelsRing.fill(0x0,1);
+  while (pos<256) {
+    m_enabledChannelsRing.fill(groupmask, m_nEnabledStripsPerGroup);
+    pos += m_nEnabledStripsPerGroup;
+    m_enabledChannelsRing.fill(0x0, max-m_nEnabledStripsPerGroup);
+    pos += max-m_nEnabledStripsPerGroup;
+  }
+}
+
 void StarMaskLoop::init() {
-	//printMask(0,12,2,0);
-//	std::cout << std::endl;
-//	std::cout << "calEN" << std::endl;
-//	printMask(0, 5,1,3);
-    m_done = false;
     if (verbose)
         std::cout << __PRETTY_FUNCTION__ << std::endl;
-//    // Shift Mask into all pixels
-//    keeper->globalFe<Star>()->writeRegister(&Star::Colpr_Mode, 0x3);
-//    keeper->globalFe<Star>()->writeRegister(&Star::Colpr_Addr, 0x0);
-//    keeper->globalFe<Star>()->initMask(MASK_1);
-//    if (enable_lCap) keeper->globalFe<Star>()->loadIntoPixel(1 << 6);
-//    if (enable_sCap) keeper->globalFe<Star>()->loadIntoPixel(1 << 7);
-//    keeper->globalFe<Star>()->initMask(m_mask);
-//    keeper->globalFe<Star>()->loadIntoPixel(1 << 0);
+
+    m_done = false;
     m_cur = min;
+    if (m_nMaskedStripsPerGroup) initMasks();
+    
     for ( FrontEnd* fe : keeper->feList ) {
-//    	if (dynamic_cast<FrontEndCfg*>(fe)->getName().find("hcc") != std::string::npos) {continue;}  ///fix me, maybe @@@
     	if (!fe->isActive()) {continue;}
-
-    	for(int iChip = 1; iChip < static_cast<StarChips*> (fe)->m_nABC+1; ++iChip){ //exclude iChip=0 which is the Hcc
-
-//    		std::cout << "static_cast<Hcc*> (fe)->m_nABC: " << static_cast<Hcc*> (fe)->m_nABC << "  " << iChip << " ichip with id: " << this_chipID << std::endl;
-//    		    	std::cout << "Star_masks[" << m_cur << "]: " << std::hex <<  Star_masks[m_cur] <<std::dec << std::endl;
-//    		    	std::cout << "Star_calEn[" << m_cur << "]: " << std::hex <<  Star_calEn[m_cur] << std::dec<< std::endl;
-    		int index =0;
-    		for (int i=16; i<24; i++){
-//    			std::cout << "write mask: "<< i << "  0x" << std::hex << std::setfill('0') << std::setw(8)<< Star_masks[min][index] <<std::dec<< std::endl;
-    			static_cast<StarChips*> (fe)->setAndWriteABCRegister(i, star_masks[min][index], iChip );  // strip 0's mask starts from reg 16,
-    			index++;
-    		}
-    		index =0;
-    		for (int i=104; i<112; i++){
-//    			std::cout << "write cal: "<< i  << " 0x" << std::hex << std::setfill('0') << std::setw(8)<< Star_calEn[min][index] << std::dec<< std::endl;
-    			static_cast<StarChips*> (fe)->setAndWriteABCRegister(i, star_calEn[min][index], iChip);  // strip 0's mask starts from reg 16,
-    			index++;
-    		}
-    	}
+	if (!m_nEnabledStripsPerGroup)
+	  applyEncodedMask(static_cast<StarChips*>(fe), m_cur);
+	else {
+	  m_maskedChannelsRing.pos=m_cur;
+	  m_enabledChannelsRing.pos=m_cur;
+	  const uint32_t * masks = m_maskedChannelsRing.read();
+	  const uint32_t * enables = m_enabledChannelsRing.read();
+	  applyMask(static_cast<StarChips*>(fe), masks, enables);
+	}
     }
-
 
     while(g_tx->isCmdEmpty() == 0);
 }
@@ -62,106 +60,90 @@ void StarMaskLoop::init() {
 void StarMaskLoop::end() {
     if (verbose)
         std::cout << __PRETTY_FUNCTION__ << std::endl;
-//    // Disable all pixels
-//    keeper->globalFe<Star>()->writeRegister(&Star::Colpr_Mode, 0x3);
-//    keeper->globalFe<Star>()->writeRegister(&Star::Colpr_Addr, 0x0);
-//    keeper->globalFe<Star>()->initMask(MASK_NONE);
-//    keeper->globalFe<Star>()->loadIntoPixel(1 << 0);
-//    if (enable_lCap) keeper->globalFe<Star>()->loadIntoPixel(1 << 6);
-//    if (enable_sCap) keeper->globalFe<Star>()->loadIntoPixel(1 << 7);
     while(g_tx->isCmdEmpty() == 0);
 }
 
 void StarMaskLoop::execPart1() {
-    if (verbose)
-        std::cout << __PRETTY_FUNCTION__ << std::endl;
+  if (verbose) {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    std::cout << " ---> Mask Stage " << m_cur << std::endl;
+  }
 
-//    std::cout << " ---> Mask Stage " << m_cur << std::endl;
-    g_stat->set(this, m_cur);
+  g_stat->set(this, m_cur);
+}
+
+void StarMaskLoop::printMask(const uint32_t chans[8]) {
+  std::cout << "write mask: [";
+  for (int j=0; j<8; j++)
+    std::cout << "  0x" << std::hex << std::setfill('0') << std::setw(8)<< chans[j] <<std::dec;
+  std::cout << "]" << std::endl;
+}
+
+void StarMaskLoop::applyMask(StarChips* fe, const uint32_t masks[8], const uint32_t enables[8]) {
+  if (verbose) {
+    std::cout << "Apply masks:" << std::endl;
+    printMask(masks);
+    std::cout << "Enable channels:" << std::endl;
+    printMask(enables);
+  }
+  
+  for( int iChip = 1; iChip < fe->m_nABC+1; ++iChip){ //exclude iChip=0 which is the Hcc
+    //Looping over MaskInput registers
+    int index=0;
+    for (int j=16; j<24; j++){
+      //    				 std::cout << "write mask: "<< i << "  0x" << std::hex << std::setfill('0') << std::setw(8)<< Star_masks[m_cur][index] <<std::dec<< std::endl;
+      fe->setAndWriteABCRegister(j, masks[index], iChip);  // strip 0's mask starts from reg 16,
+      index++;
+    }
+    //Looping over CAL ENABLE registers
+    index=0;
+    for (int j=104; j<112; j++){
+      //    				 std::cout << "write cal: "<< i  << " 0x" << std::hex << std::setfill('0') << std::setw(8)<< Star_calEn[m_cur][index] <<std::dec<< std::endl;
+      fe->setAndWriteABCRegister(j, enables[index], iChip);  // strip 0's mask starts from reg 104,.
+      index++;
+    }
+  }//end of loop over ABCs
+}
+
+void StarMaskLoop::applyEncodedMask(StarChips* fe, unsigned int curStep) {
+  applyMask(fe, star_masks[curStep], star_calEn[curStep]);
 }
 
 void StarMaskLoop::execPart2() {
     if (verbose)
         std::cout << __PRETTY_FUNCTION__ << std::endl;
     m_cur += step;
-    if (!((int)m_cur < max)) m_done = true;
-    else{
-    // Shift Enable mask by step size
-    for(unsigned i=0; i<step; i++) {
-//        keeper->globalFe<Star>()->writeRegister(&Star::Colpr_Mode, 0x3);
-//        keeper->globalFe<Star>()->writeRegister(&Star::Colpr_Addr, 0x0);
-//        keeper->globalFe<Star>()->shiftMask();
-//        keeper->globalFe<Star>()->loadIntoPixel(1 << 0);
-        //if (enable_lCap) keeper->globalFe<Star>()->loadIntoPixel(1 << 6);
-        //if (enable_sCap) keeper->globalFe<Star>()->loadIntoPixel(1 << 7);
+    if (!((int)m_cur < max))
+      m_done = true;
+    else {
+      // Shift Enable mask by step size
 
+      for ( FrontEnd* fe : keeper->feList ) {
+	if (!fe->isActive()) {continue;}
+	
+	if (!m_nEnabledStripsPerGroup)
+	  applyEncodedMask(static_cast<StarChips*>(fe), m_cur);
+	else {
+	  m_maskedChannelsRing.pos=m_cur;
+	  m_enabledChannelsRing.pos=m_cur;
+	  const uint32_t * masks = m_maskedChannelsRing.read();
+	  const uint32_t * enables = m_enabledChannelsRing.read();
+	  applyMask(static_cast<StarChips*>(fe), masks, enables);
+	}
+      }
 
-    	 for ( FrontEnd* fe : keeper->feList ) {
-//    		 if (dynamic_cast<FrontEndCfg*>(fe)->getName().find("hcc") != std::string::npos) {continue;}  ///fix me, maybe @@@
-    		 if (!fe->isActive()) {continue;}
-//    		 std::cout << "\033[1;32m " << "TxCore " << dynamic_cast<FrontEndCfg*>(fe)->getTxChannel()<<"\033[0m"<<std::endl;
-    		 //static_cast<Star*> (fe)->setRunMode();
-    		 for( int iChip = 1; iChip < static_cast<StarChips*> (fe)->m_nABC+1; ++iChip){ //exclude iChip=0 which is the Hcc
-//    			     	 std::cout << "Star_masks[" << m_cur << "]: " << std::hex <<  Star_masks[m_cur] <<std::dec << std::endl;
-//    			     	 std::cout << "Star_calEn[" << m_cur << "]: " << std::hex <<  Star_calEn[m_cur] << std::dec<< std::endl;
-//    	    	 std::cout << "static_cast<Hcc*> (fe)->m_nABC: " << static_cast<Hcc*> (fe)->m_nABC << "  " << iChip << " ichip with id: " << this_chipID << std::endl;
-
-    			 int index=0;
-    			 for (int j=16; j<24; j++){
-//    				 std::cout << "write mask: "<< i << "  0x" << std::hex << std::setfill('0') << std::setw(8)<< Star_masks[m_cur][index] <<std::dec<< std::endl;
-    				 static_cast<StarChips*> (fe)->setAndWriteABCRegister(j, star_masks[m_cur][index], iChip);  // strip 0's mask starts from reg 16,
-    				 index++;
-    			 }
-    			 index=0;
-    			 for (int j=104; j<112; j++){
-//    				 std::cout << "write cal: "<< i  << " 0x" << std::hex << std::setfill('0') << std::setw(8)<< Star_calEn[m_cur][index] <<std::dec<< std::endl;
-    				 static_cast<StarChips*> (fe)->setAndWriteABCRegister(j, star_calEn[m_cur][index], iChip);  // strip 0's mask starts from reg 104,.
-    				 index++;
-    			 }
-
-    		 }
-    	 }
-
-
-
-        while(g_tx->isCmdEmpty() == 0){std::cout << __PRETTY_FUNCTION__ << "\033[1;32m " << "cmd not empty: " << "\033[0m"<<  std::endl;};
-    }
+      while(g_tx->isCmdEmpty() == 0){std::cout << __PRETTY_FUNCTION__ << "\033[1;32m " << "cmd not empty: " << "\033[0m"<<  std::endl;};
     }
 }
 
-void StarMaskLoop::setMaskStage(enum MASK_STAGE_STRIP mask) {
-    switch (mask) {
-        case MASK_STRIP_1: //all are masked
-            m_mask = MASK_STRIP_1;
-            max = 1;
-            break;
-
-    }
-}
-
-void StarMaskLoop::setMaskStage(uint32_t mask) {
-    m_mask = mask;
-}
-/*
-uint32_t StarMaskLoop::getMaskStage() {
-    return m_mask;
-}
-
-void StarMaskLoop::setIterations(unsigned it) {
-    m_it = it;
-}
-
-unsigned StarMaskLoop::getIterations() {
-    return m_it;
-}*/
 
 void StarMaskLoop::writeConfig(json &config) {
     config["min"] = min;
     config["max"] = max;
     config["step"] = step;
-    config["mask"] = (uint32_t) m_mask;
-    config["enable_lcap"] = enable_lCap;
-    config["enable_scap"] = enable_sCap;
+    config["nMaskedStripsPerGroup"] = m_nMaskedStripsPerGroup;
+    config["nEnabledStripsPerGroup"] = m_nEnabledStripsPerGroup;
+    config["EnabledMaskedShift"] = m_EnabledMaskedShift;
 }
 
 void StarMaskLoop::loadConfig(json &config) {
@@ -169,7 +151,9 @@ void StarMaskLoop::loadConfig(json &config) {
     max = config["max"];
     step = config["step"];
     verbose = config["verbose"];
-//    m_mask = (uint32_t) config["mask"];
-//    enable_lCap = config["enable_lcap"];
-//    enable_sCap = config["enable_scap"];
+    m_nMaskedStripsPerGroup = config["nMaskedStripsPerGroup"];
+    m_nEnabledStripsPerGroup = config["nEnabledStripsPerGroup"];
+    m_EnabledMaskedShift = config["EnabledMaskedShift"];
+    if (m_nEnabledStripsPerGroup) step=1;
+    if (verbose) std::cout << "Loaded StarMaskLoop configuration with nMaskedStripsPerGroup=" << m_nMaskedStripsPerGroup << ", nEnabledStripsPerGroup=" << m_nEnabledStripsPerGroup << ", shifted by " << m_EnabledMaskedShift << " strips, min=" << min << ", max=" << max << ", step=" << step << std::endl;
 }
--- src/libStar/include/StarMaskLoop.h
+++ src/libStar/include/StarMaskLoop.h
@@ -1,43 +1,72 @@
 #ifndef STARMASKLOOP_H
 #define STARMASKLOOP_H
 
+/* Class: StarMaskLoop 
+Author: O. Arnaez <Olivier.Arnaez@cern.ch>
+Date: October 2019
+
+Applies some pattern of masked/enabled channels during scans.
+Two modes are available: 
+  1) reading masks/cal.enable register values from StarMaskCal_En.cpp if nEnabledStripsPerGroup is set to 0 in the configuration. In this case the values between indices min and max from this file are read every "step" entries.
+  2) applying patterns corresponding to groups of nEnabledStripsPerGroup (nMaskedStripsPerGroup) strips in a row that are shifted N="max" times for the pattern applied to CAL ENABLE registers (resp. masked channels) ; in this case the two patterns applied to enabled/masked channels are shifted by EnabledMaskedShift channels.
+*/
+
 #include <iostream>
 
 #include "StarChips.h"
 #include "LoopActionBase.h"
 
 enum MASK_STAGE_STRIP { //TODO-change this or delete it
-    MASK_STRIP_1  = 0xFFFFFFFF
+  MASK_STRIP_FROMFILE=16
+};
+
+class ChannelRing {
+ public:
+  ChannelRing() {};
+  ~ChannelRing() {};
+  uint16_t pos=0;
+  void fill(uint32_t newBits, short size) {
+    for (int i=size-1; i>=0; i--) {
+      bits[pos] = (newBits >> i) & 0x1;
+      pos++;
+    }
+  };
+  const uint32_t * read() {
+    uint32_t * masks = new uint32_t[8]; for (unsigned short i=0;i<8;i++) masks[i]=0;
+    for (unsigned int curpos=0; curpos<256; curpos++) {
+      masks[curpos/32] |= bits[(pos+curpos)%256]<< (31-(curpos%32));
+    }
+    return masks;
+  };
+ private:
+  bool bits[512]={0};
 };
 
 
+
 class StarMaskLoop : public LoopActionBase {
-    public:
-        StarMaskLoop();
+ public:
+  StarMaskLoop();
 
-        void setMaskStage(enum MASK_STAGE_STRIP mask);
-        void setMaskStage(uint32_t mask);
-        //uint32_t getMaskStage();
-        //void setIterations(unsigned it);
-        //unsigned getIterations();
-        void setScap(bool v=true) {enable_sCap = v;}
-        bool getScap() {return enable_sCap;}
-        void setLcap(bool v=true) {enable_lCap = v;}
-        bool getLcap() {return enable_lCap;}
+  void writeConfig(json &config);
+  void loadConfig(json &config);
 
-        void writeConfig(json &config);
-        void loadConfig(json &config);
-        
-    private:
-        uint32_t m_mask;
-        unsigned m_cur;
-        bool enable_sCap;
-        bool enable_lCap;
+ protected:
+  void applyMask(StarChips* fe, const uint32_t masks[8], const uint32_t enables[8]);
+  void applyEncodedMask(StarChips* fe, unsigned int curStep);
+  void printMask(const uint32_t chans[8]);
+  void initMasks();
+  
+ private:
+  short m_nMaskedStripsPerGroup,m_nEnabledStripsPerGroup, m_EnabledMaskedShift;
+  unsigned m_cur;
 
-        void init();
-        void end();
-        void execPart1();
-        void execPart2();
+  ChannelRing m_maskedChannelsRing, m_enabledChannelsRing;
+  
+  void init();
+  void end();
+  void execPart1();
+  void execPart2();
 };
 
 #endif
