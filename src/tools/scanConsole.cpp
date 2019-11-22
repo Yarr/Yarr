8e7a7bcce63aaf42d66dcbd27ae2128963858858
Timon Heim <timon.heim@cern.ch>
2019-11-22 13:51:17 -0800

    MAc fix for missing hostname

--- src/tools/scanConsole.cpp
+++ src/tools/scanConsole.cpp
@@ -665,7 +665,13 @@ int main(int argc, char *argv[]) {
 
 void printHelp() {
     std::string home = getenv("HOME");
-    std::string hostname = getenv("HOSTNAME");
+    std::string hostname = "default_host";
+    // HOSTNAME does not exist like this on mac, need to work around it
+    if (getenv("HOSTNAME")) {
+        hostname = getenv("HOSTNAME");
+    } else {
+        std::cout << "HOSTNAME environmental variable not found ..." << std::endl;
+    }
     std::string dbDirPath = home+"/.yarr/localdb";
 
     std::cout << "Help:" << std::endl;
