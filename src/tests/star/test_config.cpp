74c9be57d5c56303642321d6ec1623cd86ba2049
Bruce Gallop <bruce.gallop@cern.ch>
2020-01-28 17:20:01 +0000

    Add test of Trim settings

--- src/tests/star/test_config.cpp
+++ src/tests/star/test_config.cpp
@@ -36,3 +36,74 @@ TEST_CASE("StarCfg", "[star][config]") {
   REQUIRE (test_config.getABCRegister(ABCStarRegister::CREG0, abc_id) == 0x8a554321);
   REQUIRE (test_config.getSubRegisterParentValue(1, "TESTPATT1") == 0x8a554321);
 }
+
+TEST_CASE("StarCfgTrims", "[star][config]") {
+  StarCfg test_config;
+  test_config.setHCCChipId(2);
+  const int abc_id = 3;
+  test_config.addABCchipID(abc_id);
+  test_config.m_nABC ++;
+
+  test_config.initRegisterMaps();
+
+  // Default to 15 on all strips
+  // Two rows of 128 strips (odd and even)
+  for(unsigned l = 0; l < 2; l++) {
+    for(unsigned s = 0; s < 128; s++) {
+      CAPTURE (l, s);
+
+      // Currently expects indices base 1
+      CHECK (test_config.getTrimDAC(s+1, l+1) == 15);
+    }
+  }
+
+  test_config.setTrimDAC(10, 2, 18);
+  REQUIRE (test_config.getTrimDAC(10, 2) == 18);
+
+  for(unsigned r = 0; r < 40; r++) {
+    CAPTURE (r);
+
+    // Defaults
+    uint32_t expected = r<32?0xffffffff:0;
+
+    // The ones we've updated
+    if (r == 32) expected = 0x00080000;
+    if (r == 2) expected = 0xffff2fff;
+
+    CHECK (test_config.getABCRegister(ABCStarRegister::TrimDAC0 + r, abc_id) == expected);
+  }
+
+  // Two rows of 128 strips (odd and even)
+  for(unsigned l = 0; l < 2; l++) {
+    for(unsigned s = 0; s < 128; s++) {
+      CAPTURE (l, s);
+
+      int set_trim = s%32;
+      // Currently expects indices base 1
+      test_config.setTrimDAC(s+1, l+1, set_trim);
+      CHECK (test_config.getTrimDAC(s+1, l+1) == set_trim);
+    }
+  }
+
+  for(unsigned r = 0; r < 40; r++) {
+    CAPTURE (r);
+
+    uint32_t expected = 0;
+    if(r<32) {
+      switch(r%4) {
+      case 0: expected = 0x32321010; break;
+      case 1: expected = 0x76765454; break;
+      case 2: expected = 0xbaba9898; break;
+      case 3: expected = 0xfefedcdc; break;
+      }
+    } else {
+      if(r%2) expected = 0xffffffff;
+    }
+
+    CHECK (test_config.getABCRegister(ABCStarRegister::TrimDAC0 + r, abc_id) == expected);
+  }
+
+  //  test_config.setTrimDAC();
+  // void setTrimDAC(unsigned col, unsigned row, int value);
+  // int getTrimDAC(unsigned col, unsigned row);
+}
