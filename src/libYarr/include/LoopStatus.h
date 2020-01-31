99041d97b68a7232dba4d44a2606390fc44ce64e
Bruce Gallop <bruce.gallop@cern.ch>
2020-01-31 14:03:00 +0000

    Move LoopInfo out of LoopStatus, shared among users, avoid copying extra map and vector

--- src/libYarr/include/LoopStatus.h
+++ src/libYarr/include/LoopStatus.h
@@ -6,61 +6,63 @@
 #ifndef LOOPSTATUS_H
 #define LOOPSTATUS_H
 
+#include <memory>
 #include <vector>
 #include <map>
 
+/// Structure of a scan loop
+struct LoopInfo {
+        std::map<void*, unsigned*> statMap;
+        std::vector<void*> loopVec;
+};
+
+/// Where we are in the scan engine
 class LoopStatus {
     public:
-        LoopStatus() {}
-        LoopStatus(const LoopStatus &l) {
-            statMap = l.getMap();
+        LoopStatus()
+          : statInfo(new LoopInfo)
+        {}
+
+        LoopStatus(const LoopStatus &l)
+          : statInfo(l.statInfo) {
             statVec = l.getVector();
-            loopVec = l.getPointer();
         }
 
         LoopStatus& operator=(const LoopStatus &l) {
-            statMap = l.getMap();
+            statInfo = l.statInfo;
             statVec = l.getVector();
-            loopVec = l.getPointer();
             return *this;
         }
 
         bool operator==(const LoopStatus &l){
-            if (l.size() != this->size()) {
-                return false;
-            } else {
-                for (unsigned i=0; i<l.size(); i++) {
-                    if (l.get(i) != this->get(i)) {
-                        return false;
-                    }
-                }
-            }
-            return true;
+            return statVec == l.statVec;
         }
 
         void init(unsigned i) {
             statVec.resize(i);
-            loopVec.resize(i);
+            statInfo->loopVec.resize(i);
         }
         void addLoop(unsigned i, void *loop) {
-            statMap[loop] = &statVec[i];
-            loopVec[i] = loop;
+            statInfo->statMap[loop] = &statVec[i];
+            statInfo->loopVec[i] = loop;
         }
         
         void set(unsigned i, unsigned v) {statVec[i] = v;}
-        void set(void *l, unsigned v) {*(statMap[l]) = v;}
+        void set(void *l, unsigned v) {*(statInfo->statMap[l]) = v;}
         unsigned get(unsigned i) const {return statVec[i];}
-        void* getPointer(unsigned i) const {return loopVec[i];}
-        unsigned get(void *l) {return *(statMap[l]);}
+        void* getPointer(unsigned i) const {return statInfo->loopVec[i];}
+        unsigned get(void *l) {return *(statInfo->statMap[l]);}
         unsigned size() const {return statVec.size();}
 
-        std::map<void*, unsigned*> getMap() const {return statMap;}
-        std::vector<unsigned> getVector() const {return statVec;}
-        std::vector<void*> getPointer() const {return loopVec;}
+        const std::map<void*, unsigned*> getMap() const {return statInfo->statMap;}
+        const std::vector<unsigned> getVector() const {return statVec;}
+        const std::vector<void*> getPointer() const {return statInfo->loopVec;}
     private:
-        std::map<void*, unsigned*> statMap;
-        std::vector<void*> loopVec;
+        /// Location within scan loop hierarchy
         std::vector<unsigned> statVec;
+
+        /// Information used to lookup types of scan loops
+        std::shared_ptr<LoopInfo> statInfo;
 };
 
 #endif
