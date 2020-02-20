b28cc50759297610c6447818183f6c9823679fe1
Nicholas Dreyer <dreyern@u.washington.edu>
2020-02-20 11:02:44 -0800

    Remove "renamed" files in commit a29eecd from git tracking.  This should be considered part of that commit.

--- scripts/cc_scan_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bin/scanConsole -r configs/controller/test_emuCfg_rd53a.json -c configs/connectivity/example_rd53a_setup.json -s configs/scans/rd53a/test_std_digitalscan.json -p
-bin/scanConsole -r configs/controller/test_emuCfg_rd53a.json -c configs/connectivity/example_rd53a_setup.json -s configs/scans/rd53a/test_std_analogscan.json -p
-bin/scanConsole -r configs/controller/test_emuCfg_rd53a.json -c configs/connectivity/example_rd53a_setup.json -s configs/scans/rd53a/test_std_thresholdscan.json -p
-bin/test_main
