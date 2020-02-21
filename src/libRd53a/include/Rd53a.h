207847a5c9b892a022be9b8e62c1b4907fd9414a
Timon Heim <timon.heim@cern.ch>
2020-02-21 08:03:01 -0800

    Remove spurious cout

--- src/libRd53a/include/Rd53a.h
+++ src/libRd53a/include/Rd53a.h
@@ -52,7 +52,6 @@ class Rd53a : public FrontEnd, public Rd53aCfg, public Rd53aCmd {
         void writeNamedRegister(std::string name, uint16_t value) override;
         
         void setInjCharge(double charge, bool sCap=true, bool lCap=true) override {
-            std::cout << __PRETTY_FUNCTION__ << " " << charge << std::endl;
             this->writeRegister((Rd53aReg Rd53aGlobalCfg::*)&Rd53aGlobalCfg::InjVcalDiff, this->toVcal(charge));
         }
         
