06155c5d118e3f39dd092f28e584a78001d9e44e
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2020-03-05 17:07:08 -0800

    Set initial/reset value of stopHPR bit of ABCStar to 0 instead of 1

--- src/libStar/AbcCfg.cpp
+++ src/libStar/AbcCfg.cpp
@@ -142,7 +142,7 @@ void AbcCfg::configure_ABC_Registers() {
 
     //// Initialize 32-bit register with default values
     ////#special reg
-    m_registerMap[ABCStarRegister::SCReg]->setValue(0x00000004);
+    m_registerMap[ABCStarRegister::SCReg]->setValue(0x00000000);
 
     ////#Analog and DCS regs
     for (unsigned int iReg=ABCStarRegister::ADCS1; iReg<=ABCStarRegister::ADCS7; iReg++)
