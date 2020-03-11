16caa4bd8fbd0e66e42b5a887bef8d238cd6a041
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2020-03-11 16:04:19 -0700

    Cast register address to int when printing

--- src/libStar/include/StarChipPacket.h
+++ src/libStar/include/StarChipPacket.h
@@ -240,7 +240,7 @@ class StarChipPacket{
         this->error_block->print(os);
     }else if(this->type == TYP_ABC_RR || this->type == TYP_HCC_RR || this->type == TYP_ABC_HPR || this->type == TYP_HCC_HPR ){
       os << std::hex << std::setfill('0');
-      os << "Packet info: Address " << std::setw(2) << address
+      os << "Packet info: Address " << std::setw(2) << (unsigned)address
          << ", Value " << std::setw(8) << value << "\n";
       os << std::dec << std::setfill(' ');
     }
@@ -255,7 +255,7 @@ class StarChipPacket{
     }else if(this->type == TYP_ABC_RR || this->type == TYP_HCC_RR || this->type == TYP_ABC_HPR || this->type == TYP_HCC_HPR ){
       os << "ABC " << channel_abc << ", Address ";
       os << std::hex << std::setfill('0');
-      os << std::setw(2) << address << ", Value " << std::setw(8) << value;
+      os << std::setw(2) << (unsigned)address << ", Value " << std::setw(8) << value;
       os << std::dec << std::setfill(' ');
     }
     os << "\n";
