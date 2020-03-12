a9dfce0a01610ece8d4dcc150035304601374173
Timon Heim <timon.heim@cern.ch>
2020-03-12 09:10:34 -0700

    Cleanup leftovers

--- syn/kintex7/version.vhd
+++ syn/kintex7/version.vhd
@@ -3,5 +3,5 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
 package version_pkg is
-    constant c_FW_VERSION : std_logic_vector(31 downto 0) := x"08244c3a";
+    constant c_FW_VERSION : std_logic_vector(31 downto 0) := x"03d57fb3";
 end version_pkg;
