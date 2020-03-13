a02fd64d09b6a6be84bcf422dd4dcc4cda3681f5
Timon Heim <timon.heim@cern.ch>
2020-03-13 13:15:08 -0700

    Forgot board files, remove old files

---
+++ syn/kintex7/rd53_ohio_16x1_160Mbps/board_pkg.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+package board_pkg is
+    constant c_FW_IDENT : std_logic_vector(31 downto 0) := x"00030212";
+    constant c_TX_ENCODING : string := "OSERDES";
+    constant c_TX_CHANNELS : integer := 4;
+    constant c_RX_CHANNELS : integer := 16;
+    constant c_FE_TYPE : string := "RD53";
+    constant c_RX_NUM_LANES : integer := 1;
+    constant c_RX_SPEED : string := "0160";
+    constant c_TX_IDLE_WORD : std_logic_vector(31 downto 0) := x"69696969";
+    constant c_TX_SYNC_WORD : std_logic_vector(31 downto 0) := x"817e6969";
+    constant c_TX_SYNC_INTERVAL : unsigned(7 downto 0) := to_unsigned(31,8);
+    constant c_TX_AZ_WORD : std_logic_vector(31 downto 0) := x"5c5ca69a";
+    constant c_TX_AZ_INTERVAL : unsigned(15 downto 0) := to_unsigned(500,16);
+    constant c_TX_40_DIVIDER : unsigned(3 downto 0) := to_unsigned(4,4);
+end board_pkg;
+
--- syn/kintex7/version.vhd
+++ syn/kintex7/version.vhd
@@ -3,5 +3,5 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
 package version_pkg is
-    constant c_FW_VERSION : std_logic_vector(31 downto 0) := x"085057fe";
+    constant c_FW_VERSION : std_logic_vector(31 downto 0) := x"0ad0542f";
 end version_pkg;
