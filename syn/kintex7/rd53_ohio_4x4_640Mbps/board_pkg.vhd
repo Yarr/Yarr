b83f24e0d9359fbeb1bf8e861e9e3bda6b53e8a6
Timon Heim <timon.heim@cern.ch>
2020-03-16 12:18:45 -0700

    Forgot to add HW identifier

--- syn/kintex7/rd53_ohio_4x4_640Mbps/board_pkg.vhd
+++ syn/kintex7/rd53_ohio_4x4_640Mbps/board_pkg.vhd
@@ -2,8 +2,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
+library work;
+use work.hw_type_pkg.all;
+
 package board_pkg is
-    constant c_FW_IDENT : std_logic_vector(31 downto 0) := x"00030231";
+    constant c_FW_IDENT : std_logic_vector(31 downto 0) := c_HW_IDENT & x"030231";
     constant c_TX_ENCODING : string := "OSERDES";
     constant c_TX_CHANNELS : integer := 4;
     constant c_RX_CHANNELS : integer := 4;
