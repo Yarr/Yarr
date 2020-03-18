8868d19a7e6a04b23d5268ddd9730c9e70db5a69
Bruce Gallop <bruce.gallop@cern.ch>
2020-03-18 16:16:55 +0000

    Create empty config doesn't wait for anything so succeeds

--- src/tests/feedback/test_feedback.cpp
+++ src/tests/feedback/test_feedback.cpp
@@ -1,16 +1,36 @@
 #include "catch.hpp"
 
+#include "AllChips.h"
 #include "AllStdActions.h"
 #include "ScanFactory.h"
 
+#include "../EmptyHw.h"
+
+class MyHardware : public EmptyHw {
+  uint32_t trigEnable;
+public:
+  MyHardware() : trigEnable(0) {}
+
+  void setTrigEnable(uint32_t value) override { trigEnable = value;}
+  uint32_t getTrigEnable() override { return trigEnable; }
+};
+
 // Just create scan engine side
-TEST_CASE("FeedbackTests", "[Feedback]") {
-    Bookkeeper bookie(nullptr, nullptr);
+TEST_CASE("FeedbackTestEmpty", "[Feedback]") {
+    MyHardware empty;
+    Bookkeeper bookie(&empty, &empty);
     ScanFactory scan(&bookie);
 
+    auto g_fe = StdDict::getFrontEnd("FEI4B");
+    g_fe->makeGlobal();
+    g_fe->init(&empty, 0, 0);
+    bookie.initGlobalFe(g_fe.release());
+
+    // Stripped down version of OccGlobalThresholdTune
     json js;
     js["scan"]["name"] = "TestScan";
     js["scan"]["loops"][0]["loopAction"] = "Fei4GlobalFeedback";
+    js["scan"]["loops"][0]["config"]["parameter"] = "Vthin_Fine";
     js["scan"]["loops"][1]["loopAction"] = "Fei4MaskLoop";
     js["scan"]["loops"][2]["loopAction"] = "Fei4DcLoop";
     js["scan"]["loops"][3]["loopAction"] = "Fei4TriggerLoop";
@@ -20,7 +40,6 @@ TEST_CASE("FeedbackTests", "[Feedback]") {
 
     scan.init();
 
-    // s->preScan();
+    // Skip pre/post scan
     scan.run();
-    // s->postScan();
 }
