90626eabc672caeca2e768e62f65b9ebdd9ef157
Bruce Gallop <bruce.gallop@cern.ch>
2020-03-19 17:45:19 +0000

    Remove mutexMap from Bookkeeper, in favour of map in feedback classes

--- src/libRd53a/Rd53aPixelFeedback.cpp
+++ src/libRd53a/Rd53aPixelFeedback.cpp
@@ -79,7 +79,7 @@ void Rd53aPixelFeedback::feedback(unsigned channel, Histo2d *h) {
             }
         }
     }
-    keeper->mutexMap[channel].unlock();
+    m_fbMutex[channel].unlock();
 }
 
 void Rd53aPixelFeedback::writePixelCfg(Rd53a *fe) {
@@ -123,7 +123,7 @@ void Rd53aPixelFeedback::execPart1() {
     // Lock all mutexes
     for (auto fe : keeper->feList) {
         if (fe->getActive()) {
-            keeper->mutexMap[dynamic_cast<FrontEndCfg*>(fe)->getRxChannel()].try_lock();
+            m_fbMutex[dynamic_cast<FrontEndCfg*>(fe)->getRxChannel()].try_lock();
             this->writePixelCfg(dynamic_cast<Rd53a*>(fe));
         }
     }
@@ -135,7 +135,7 @@ void Rd53aPixelFeedback::execPart2() {
     for (auto fe: keeper->feList) {
         if (fe->getActive()) {
             unsigned rx = dynamic_cast<FrontEndCfg*>(fe)->getRxChannel();
-            keeper->mutexMap[rx].lock();
+            m_fbMutex[rx].lock();
         }
     }
     m_cur++;
