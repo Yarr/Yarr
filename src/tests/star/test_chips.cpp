998f76456fb8dcff4e2e714f8e398ece555e3389
Bruce Gallop <bruce.gallop@cern.ch>
2020-04-07 15:40:13 +0100

    Introduce simple HwController to record what is sent

--- src/tests/star/test_chips.cpp
+++ src/tests/star/test_chips.cpp
@@ -2,6 +2,44 @@
 
 #include "StarChips.h"
 
+#include "../EmptyHw.h"
+
+/**
+   Override TxCore to record what is written to FIFO.
+ */
+class MyTxCore : public EmptyTxCore {
+public:
+  std::vector<std::vector<uint32_t>> buffers;
+
+  void writeFifo(uint32_t word) override {
+    if(buffers.empty()) {
+      buffers.push_back(std::vector<uint32_t>{});
+    }
+    buffers.back().push_back(word);
+  }
+
+  void releaseFifo() override {
+    buffers.push_back(std::vector<uint32_t>{});
+  }
+};
+
+class MyHwController
+  : public HwController, public MyTxCore, public EmptyRxCore {
+public:
+  MyHwController() {}
+  ~MyHwController() {}
+
+  void loadConfig(json &j) override {}
+};
+
+TEST_CASE("StarBasicConfig", "[star][chips]") {
+  MyHwController hw;
+
+  MyTxCore &tx = hw;
+
+  REQUIRE (tx.buffers.empty());
+}
+
 TEST_CASE("StarChips", "[star][chips]") {
   // Side-effect of checking it's not abstract is intentional
   StarChips test_config;
