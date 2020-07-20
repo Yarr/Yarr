ed78a65cf2a0bffd0360b021b7e5f1f5a9347c04
Bruce Gallop <bruce.gallop@cern.ch>
2020-07-20 15:15:06 +0100

    Tidy up StarChips creation, note that hccID shouldn't be before init

--- src/libStar/StarChips.cpp
+++ src/libStar/StarChips.cpp
@@ -23,6 +23,8 @@ StdDict::registerFrontEnd
 StarChips::StarChips()
 : StarCmd(), FrontEnd()
 {
+	m_txcore  = nullptr;
+
 	txChannel = 99;
 	rxChannel = 99;
 	active = false;
@@ -75,6 +77,7 @@ StarChips::StarChips(HwController *arg_core, unsigned arg_txChannel, unsigned ar
 }
 
 void StarChips::init(HwController *arg_core, unsigned arg_txChannel, unsigned arg_rxChannel) {
+	logger->debug("Running init {} {} {}", (void*)arg_core, arg_txChannel, arg_rxChannel);
 	m_txcore  = arg_core;
 	m_rxcore = arg_core;
 	txChannel = arg_txChannel;
@@ -90,6 +93,11 @@ void StarChips::init(HwController *arg_core, unsigned arg_txChannel, unsigned ar
 void StarChips::setHccId(unsigned hccID) {
   //First step will consist in setting the HCC ID (serial number might be different depending on fuse !)
   //Load the eFuse serial number (and stop HPR)
+  if(!m_txcore) {
+    logger->warn("Set HCC ID (to {}) called before init", hccID);
+    return;
+  }
+
   sendCmd(write_hcc_register(16, 0x5, 0xf));
   //Let's reset the HCC ID with a broadcast write of the HCCID+SN on reg 17
   uint32_t newReg17val = (hccID<<28) | m_sn;
