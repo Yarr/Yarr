145becbf24f4157697633b42ce0c6e9c2f2c252e
Bruce Gallop <bruce.gallop@cern.ch>
2020-07-21 09:58:20 +0100

    Implement rx enable/disable via fixed stream config

--- src/libItsdaqFW/ItsdaqRxCore.cpp
+++ src/libItsdaqFW/ItsdaqRxCore.cpp
@@ -2,6 +2,8 @@
 
 #include <iomanip>
 
+const size_t max_stream = 256;
+
 std::chrono::steady_clock::time_point bridge_watcher;
 
 ItsdaqRxCore::ItsdaqRxCore(ItsdaqHandler&h)
@@ -16,23 +18,47 @@ void ItsdaqRxCore::init() {
   bridge_watcher = std::chrono::steady_clock::now();
 }
 
-void ItsdaqRxCore::setRxEnable(uint32_t val) {
+void ItsdaqRxCore::setRxEnable(uint32_t stream) {
   // Set stream mask (fixed params, capture?)
-  std::cout << "Skip setRxEnable " << val << '\n';
+  std::cout << "Set Rx enable only stream " << stream << '\n';
+
+  disableRx();
+
+  const uint16_t conf_mask = 0xffff;
+  const uint16_t conf_value = 0x0001;
+
+  // Mask, stream, value
+  std::array<uint16_t, 3> buffer = {conf_mask, (uint16_t)stream, conf_value};
+  uint16_t opcode = 0x0050;
+  m_h.SendOpcode(opcode, buffer.data(), 3);
 
   bridge_watcher = std::chrono::steady_clock::now();
 }
 
 void ItsdaqRxCore::disableRx() {
-  std::cout << "Skip disableRx\n";
+  // Disable all streams
+  // Mask, (stream, value)*
+  static const size_t length = 1 + max_stream *2;
+  std::array<uint16_t, length> buffer;
+  buffer[0] = 1; // Changing mask
+  for(size_t s=0; s<max_stream; s++) {
+    buffer[s*2+1] = s;
+    buffer[s*2+2] = 0; // Mask set to off
+  }
+
+  uint16_t opcode = 0x0050;
+  m_h.SendOpcode(opcode, buffer.data(), length);
 }
 
 void ItsdaqRxCore::setRxEnable(std::vector<uint32_t> channels) {
-  std::cout << "Skip setRxEnable chan list\n";
-    // this->disableAllChannels();
-    // for (uint32_t channel : channels) {
-    //     this->enableChannel(channel);
-    // }
+  std::cout << "Skip setRxEnable list of " << channels.size() << " streams\n";
+
+  // First disable all
+  disableRx();
+
+  for(uint32_t c: channels) {
+    setRxEnable(c);
+  }
 
   bridge_watcher = std::chrono::steady_clock::now();
 }
