5d60df677402175e83f274a5d199163fa33292c9
Timon Heim <timon.heim@cern.ch>
2020-07-23 08:35:26 -0700

    Add parameter loop

---
+++ src/libRd53b/Rd53bParameterLoop.cpp
@@ -0,0 +1,87 @@
+// #################################
+// # Author: Timon Heim
+// # Email: timon.heim at cern.ch
+// # Project: Yarr
+// # Description: Parameter Loop for RD53B
+// # Date: 07/2020
+// ################################
+
+#include "Rd53bParameterLoop.h"
+
+#include "logging.h"
+
+namespace {
+    auto logger = logging::make_log("Rd53bParameterLoop");
+}
+
+Rd53bParameterLoop::Rd53bParameterLoop() : LoopActionBase(LOOP_STYLE_PARAMETER) {
+    loopType = typeid(this);
+    min = 0;
+    max = 100;
+    step = 1;
+
+}
+
+Rd53bParameterLoop::Rd53bParameterLoop(Rd53bReg Rd53bGlobalCfg::*ref) : LoopActionBase(LOOP_STYLE_PARAMETER), parPtr(ref) {
+    loopType = typeid(this);
+    min = 0;
+    max = 100;
+    step = 1;
+    //TODO parName not set
+
+}
+
+void Rd53bParameterLoop::init() {
+    SPDLOG_LOGGER_TRACE(logger, "");
+
+    m_done = false;
+    m_cur = min;
+    parPtr = keeper->globalFe<Rd53b>()->getNamedRegister(parName);
+    this->writePar();
+    //std::this_thread::sleep_for(std::chrono::milliseconds(10));
+}
+
+void Rd53bParameterLoop::execPart1() {
+    SPDLOG_LOGGER_TRACE(logger, "");
+    logger->debug("ParameterLoop at -> {}", m_cur);
+    g_stat->set(this, m_cur);
+    //std::this_thread::sleep_for(std::chrono::milliseconds(10));
+}
+
+void Rd53bParameterLoop::execPart2() {
+    m_cur += step;
+    if ((int)m_cur > max) m_done = true;
+    this->writePar();
+}
+
+void Rd53bParameterLoop::end() {
+    // Reset to min
+    m_cur = min;
+    this->writePar();
+}
+
+void Rd53bParameterLoop::writePar() {
+    keeper->globalFe<Rd53b>()->writeRegister(parPtr, m_cur);
+    while(!g_tx->isCmdEmpty());
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+}
+
+void Rd53bParameterLoop::writeConfig(json &j) {
+    j["min"] = min;
+    j["max"] = max;
+    j["step"] = step;
+    j["parameter"] = parName;
+}
+
+void Rd53bParameterLoop::loadConfig(json &j) {
+    if (!j["min"].empty())
+        min = j["min"];
+    if (!j["max"].empty())
+        max = j["max"];
+    if (!j["step"].empty())
+        step = j["step"];
+    if (!j["parameter"].empty()) {
+        logger->info("Linking parameter: {}", std::string(j["parameter"]));
+        parName = j["parameter"];
+    }
+}
--- src/libRd53b/include/AllRd53bActions.h
+++ src/libRd53b/include/AllRd53bActions.h
@@ -1,3 +1,4 @@
 #include "Rd53bTriggerLoop.h"
 #include "Rd53bMaskLoop.h"
 #include "Rd53bCoreColLoop.h"
+#include "Rd53bParameterLoop.h"
