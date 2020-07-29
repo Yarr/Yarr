728dc0683d55a98938a2b5aa98e457c0a18305b4
Timon Heim <timon.heim@cern.ch>
2020-07-29 13:27:22 -0700

    Test script to run s-curve

--- src/tools/rd53bTest.cpp
+++ src/tools/rd53bTest.cpp
@@ -136,15 +136,17 @@ int main (int argc, char *argv[]) {
  
     logger->info("Enable Tx");
     hwCtrl->setCmdEnable(0);
+    hwCtrl->setTrigEnable(0x0);
 
     logger->info("Configure chip ...");
     rd53b.configureInit();
     rd53b.configureGlobal();
 
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    rd53b.setReg(0, 0, 1, 1, 1, 0); // Enable first pixel
-    rd53b.setReg(0, 1, 1, 1, 1, 0); // Enable first pixel
-    rd53b.setReg(0, 30, 1, 1, 1, 0); // Enable first pixel
+    rd53b.setReg(7, 340, 1, 1, 1, 0); // Enable first pixel
+    //rd53b.setReg(4, 6, 1, 1, 1, 0); // Enable first pixel
+    //rd53b.setReg(5, 5, 1, 1, 1, 0); // Enable first pixel
+    //rd53b.setReg(6, 6, 1, 1, 1, 0); // Enable first pixel
     rd53b.configurePixels();
     while(!hwCtrl->isCmdEmpty());
 
@@ -153,55 +155,140 @@ int main (int argc, char *argv[]) {
     hwCtrl->setRxEnable(0);
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
-    logger->info("Sending read register");
-    rd53b.readRegister(&Rd53b::InjVcalHigh);
-    std::array<uint16_t, 3> cal = rd53b.genCal(15, 1, 0, 30, 0, 0);
     
-    hwCtrl->writeFifo(cal[0] << 16 | cal[1]);
-    hwCtrl->writeFifo(cal[2] << 16 | 0xAAAA); // 4bc
-    hwCtrl->writeFifo(0xAAAAAAAA);// 8bc
-    hwCtrl->writeFifo(0xAAAAAAAA);// 8bc
-    hwCtrl->writeFifo(0x566a566c);
-    hwCtrl->writeFifo(0x56715672);
-    hwCtrl->writeFifo(0x568b568d);
+    /*
+    rd53b.writeRegister(&Rd53b::MonitorEnable, 1);
+    rd53b.writeRegister(&Rd53b::MonitorV, 1);
+    rd53b.writeRegister(&Rd53b::MonitorI, 16);
+    rd53b.writeRegister(&Rd53b::DiffPreampL, 0);
+    rd53b.writeRegister(&Rd53b::DiffPreampR, 0);
+    rd53b.writeRegister(&Rd53b::DiffPreampTL, 0);
+    rd53b.writeRegister(&Rd53b::DiffPreampTR, 0);
+    rd53b.writeRegister(&Rd53b::DiffPreampM, 0);
+    */
+    rd53b.writeRegister(&Rd53b::InjVcalHigh, 1200);
+    rd53b.writeRegister(&Rd53b::InjVcalMed, 2000);
+    rd53b.writeRegister(&Rd53b::InjVcalRange, 1);
     
-    while(!hwCtrl->isCmdEmpty());
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    //hwCtrl->writeFifo(0x56a65671);
-    rd53b.readRegister(&Rd53b::PixAutoRow);
-    while(!hwCtrl->isCmdEmpty());
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
-    logger->info("Reading data");
-    RawData *data = hwCtrl->readData();
-    if  (data) {
-        logger->info("read {} words", data->words);
-        std::pair<uint32_t, uint32_t> answer = rd53bTest::decodeSingleRegRead(data->buf[0], data->buf[1]);
-        logger->info("Answer: {} {}", answer.first, answer.second);
-        for (unsigned j=0; j<data->words; j++)
-            logger->info("[{}] = 0x{:x}", j, data->buf[j]);
-        delete data;
+    /*
+    logger->info("Done init, starting in 4s");
+    std::this_thread::sleep_for(std::chrono::seconds(4));
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    for (unsigned int i=0; i<1023; i+=250) {
+        logger->info("{}", i);
+        rd53b.writeRegister(&Rd53b::DiffPreampM, i);
+        //rd53b.writeRegister(&Rd53b::InjVcalHigh, i);
+        //rd53b.writeRegister(&Rd53b::InjVcalMed, i);
+        //rd53b.writeRegister(&Rd53b::DiffTh1M, i/4);
+        while(!hwCtrl->isCmdEmpty());
+        std::this_thread::sleep_for(std::chrono::seconds(5));
     }
-    logger->info("Reading data");
-    data = hwCtrl->readData();
-    if  (data) {
-        logger->info("read {} words", data->words);
-        std::pair<uint32_t, uint32_t> answer = rd53bTest::decodeSingleRegRead(data->buf[0], data->buf[1]);
-        logger->info("Answer: {} {}", answer.first, answer.second);
-        for (unsigned j=0; j<data->words; j++)
-            logger->info("[{}] = 0x{:x}", j, data->buf[j]);
-        delete data;
+    */
+
+    rd53b.writeRegister(&Rd53b::Latency, 36);
+    //rd53b.writeRegister(&Rd53b::InjDigEn, 1);
+   
+    std::array<unsigned, 32> tags = {0};
+
+    const unsigned bins = 100;
+    const unsigned scaler = 10;
+    std::array<unsigned, bins> scurve = {0};
+
+    std::array<uint32_t, 32> m_trigWord;
+    unsigned m_trigMultiplier = 32;
+    unsigned delay = 32;
+
+    m_trigWord.fill(0xAAAAAAAA);
+    
+    // Injection command
+    std::array<uint16_t, 3> calWords = rd53b.genCal(16, 0, 0, 1, 0, 0);
+    m_trigWord[31] = 0xAAAA0000 | calWords[0];
+    m_trigWord[30] = ((uint32_t)calWords[1]<<16) | calWords[2];
+    
+    uint64_t trigStream = 0;
+
+    // Generate stream of ones for each trigger
+    uint64_t one = 1;
+    for (unsigned i=0; i<m_trigMultiplier; i++)
+        trigStream |= (one << i);
+    trigStream = trigStream << delay%8;
+
+    for (unsigned i=0; i<(m_trigMultiplier/8)+1; i++) {
+        if (((30-(delay/8)-i) > 2) && delay > 30) {
+            uint32_t bc1 = (trigStream >> (2*i*4)) & 0xF;
+            uint32_t bc2 = (trigStream >> ((2*i*4)+4)) & 0xF;
+            m_trigWord[30-(delay/8)-i] = ((uint32_t)rd53b.genTrigger(bc1, 2*i)[0] << 16) |  rd53b.genTrigger(bc2, (2*i)+1)[0];
+        } else {
+            logger->error("Delay is either too small or too large!");
+        }
     }
-    logger->info("Reading data");
-    data = hwCtrl->readData();
-    if  (data) {
-        logger->info("read {} words", data->words);
-        std::pair<uint32_t, uint32_t> answer = rd53bTest::decodeSingleRegRead(data->buf[0], data->buf[1]);
-        logger->info("Answer: {} {}", answer.first, answer.second);
-        for (unsigned j=0; j<data->words; j++)
-            logger->info("[{}] = 0x{:x}", j, data->buf[j]);
-        delete data;
+
+    // Rearm
+    std::array<uint16_t, 3> armWords = rd53b.genCal(16, 1, 0, 0, 0, 0);
+    m_trigWord[1] = 0xAAAA0000 | armWords[0];
+    m_trigWord[0] = ((uint32_t)armWords[1]<<16) | armWords[2];
+    
+    hwCtrl->setTrigConfig(INT_COUNT);
+    hwCtrl->setTrigFreq(500);
+    hwCtrl->setTrigCnt(20);
+    hwCtrl->setTrigWord(&m_trigWord[0], 32);
+    hwCtrl->setTrigWordLength(32);
+
+    logger->info("Trigger buffer set to:");
+    for (unsigned i=0; i<32; i++) {
+      logger->info("[{}: 0x{:x}", 31-i, m_trigWord[31-i]);
     }
+    
+    for (unsigned cal=0; cal<bins; cal++) {
+        rd53b.writeRegister(&Rd53b::InjVcalHigh, 2000+(cal*scaler));
+        while(!hwCtrl->isCmdEmpty());
+        rd53b.sendClear(15);
+        while(!hwCtrl->isCmdEmpty());
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        unsigned counter = 0;
+            
+        hwCtrl->setTrigEnable(0x1);
+
+        while(!hwCtrl->isTrigDone());
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        hwCtrl->setTrigEnable(0x0);
+        
+        //logger->info("Reading data");
+        RawData *data = hwCtrl->readData();
+        while (data) {
+            if  (data) {
+                //logger->info("Read {} words", data->words);
+                //std::pair<uint32_t, uint32_t> answer = rd53bTest::decodeSingleRegRead(data->buf[0], data->buf[1]);
+                //logger->info("Answer: {} {}", answer.first, answer.second);
+                for (unsigned j=0; j<data->words; j+=2) {
+                    unsigned tag = (data->buf[j] & 0x7F800000) >> 23;
+                    unsigned ccol = (data->buf[j] & 0x007E0000) >> 17;
+                    if (ccol > 0) {
+                        counter++;
+                        if(tag<32)
+                            tags[tag]++;
+                    }
+                    logger->info("[{}] = 0x{:x} = Tag({}) CCOL({})", j, data->buf[j], tag, ccol);
+                    logger->info("[{}] = 0x{:x} ", j+1, data->buf[j+1]);
+                }
+                delete data;
+            }
+            data = hwCtrl->readData();
+        }
+
+        scurve[cal] = counter;
+        logger->info("[{}] = {}", cal*scaler, counter);
+    }
+    
+    
+    for (unsigned i=0; i<32; i++)
+        logger->info("[{}] = {}", i, tags[i]);
+
+    for (unsigned i=0; i<bins; i++)
+        std::cout << scurve[i] << " ";
+        //logger->info("[{}] = {}", i*scaler, scurve[i]);
+    std::cout << std::endl;
 
     logger->info("... done! bye!");
     hwCtrl->disableRx();
