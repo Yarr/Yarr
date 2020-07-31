3dc16ac3581299ebc1c86023b550658512a6c99c
Rafael Gama <rafael.gama@cern.ch>
2020-07-31 12:30:07 +0200

    Renamed BdaqCmdRd53 to BdaqDriver (not RD53A specific anymore) + new features.

---
+++ src/libBdaq/BdaqDriver.cpp
@@ -0,0 +1,135 @@
+#include "BdaqDriver.h"
+#include "logging.h"
+
+namespace {
+  auto logger = logging::make_log("BdaqDriver");
+}
+
+
+void BdaqDriver::checkVersion () {
+	BdaqRegister::checkVersion(requireVersion, "auroraRx");
+}
+
+void BdaqDriver::init() {
+	memSize = getMemSize();
+}
+
+uint16_t BdaqDriver::getMemSize() {
+	return (*this)["MEM_BYTES"];
+}
+
+uint16_t BdaqDriver::getCmdSize() {
+	return (*this)["SIZE"];
+}
+
+void BdaqDriver::start() {
+	(*this)["START"] = 0xff;
+}
+
+void BdaqDriver::setSize(uint16_t value) {
+	//CMD buffer size
+	(*this)["SIZE"] = value;
+}
+
+uint16_t BdaqDriver::getSize() { //same as getCmdSize()
+	//CMD buffer size
+	return (*this)["SIZE"];
+}
+
+void BdaqDriver::setRepetitions(uint16_t value) {
+	//CMD repetitions
+	(*this)["REPETITIONS"] = value;
+}
+
+uint16_t BdaqDriver::getRepetitions() {
+	//CMD repetitions
+	return (*this)["REPETITIONS"];
+}
+
+void BdaqDriver::setExtTrigger(bool value) { 
+	//External trigger input enable
+	(*this)["EXT_TRIGGER_EN"] = value;
+}
+
+bool BdaqDriver::getExtTrigger() { 
+	//External trigger input enable
+	return (*this)["EXT_TRIGGER_EN"];
+}
+
+void BdaqDriver::setExtStart(bool value) { 
+	//External start input enable
+	(*this)["EXT_START_EN"] = value;
+}
+
+bool BdaqDriver::getExtStart() { 
+	//External start input enable
+	return (*this)["EXT_START_EN"];
+}
+
+void BdaqDriver::setOutputEn(bool value) {
+	//CMD output driver. False=high impedance
+	(*this)["OUTPUT_EN"] = value;
+}
+
+void BdaqDriver::setBypassMode(bool value) {
+	//CDS bypass mode (KC705+FMC_LPC). Enables output drivers and sends cmd and serializer clock to the chip.
+	//Probably not useful for YARR-BDAQ integration, since the idea is to support the BDAQ hardware onlye (no KC705).
+	(*this)["BYPASS_MODE"] = value;
+}
+
+bool BdaqDriver::getBypassMode() {
+	//CDS bypass mode (KC705+FMC_LPC). Enables output drivers and sends cmd and serializer clock to the chip.
+	//Probably not useful for YARR-BDAQ integration, since the idea is to support the BDAQ hardware onlye (no KC705).
+	return (*this)["BYPASS_MODE"];
+}
+
+void BdaqDriver::setAutoSync(bool value) {
+	(*this)["AUTO_SYNC"] = value;
+}
+
+bool BdaqDriver::getAutoSync() {
+	return (*this)["AUTO_SYNC"];
+}
+
+bool BdaqDriver::isDone() {
+	return (*this)["READY"];
+}
+
+void BdaqDriver::setAzVetoCycles(uint16_t value) {
+	//Veto clock cycles in 1/160 Mhz during AZ
+	(*this)["AZ_VETO_CYCLES"] = value;
+}
+
+uint16_t BdaqDriver::getAzVetoCycles() {
+	//Veto clock cycles in 1/160 Mhz during AZ
+	return (*this)["AZ_VETO_CYCLES"];
+}
+
+void BdaqDriver::setChipType(uint8_t value) {
+	(*this)["CHIP_TYPE"] = value;
+}
+
+void BdaqDriver::setData (std::vector<uint8_t>& data, uint8_t addr) {
+	if (memSize < data.size()) {
+		std::string error = "BdaqDriver::setData(): Size of data (" + 
+		std::to_string(data.size()) + " bytes) is bigger than memory (" + 
+		std::to_string(memSize) + " bytes).";
+		logger->critical(error);
+		exit(-1);
+	}
+	intf.write(base + memOffset + addr, data);
+}
+
+void BdaqDriver::getData(std::vector<uint8_t>& data, uint8_t size, uint8_t addr) {
+	if (memSize < size) {
+		std::string error = "Bdaq_i2c::getData(): Size of data (" + 
+		std::to_string(data.size()) + " bytes) is bigger than memory (" + 
+		std::to_string(memSize) + " bytes).";
+		logger->critical(error);
+		exit(-1);
+	}
+	if (size == 0)
+		intf.read(base + memOffset + addr, data, memSize);
+	else 
+		intf.read(base + memOffset + addr, data, size);
+}
---
+++ src/libBdaq/include/BdaqDriver.h
@@ -0,0 +1,69 @@
+#ifndef BDAQDRIVER_H
+#define BDAQDRIVER_H
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+#include "BdaqRegister.h"
+#include "BdaqRBCP.h"
+
+class BdaqDriver : public BdaqRegister<BdaqRBCP> {
+	public:
+		const uint requireVersion = 2;
+
+		BdaqDriver(BdaqRBCP &_rbcp) : BdaqRegister(_rbcp) {
+			registers = {
+				//Name            ,Addr, Size, Offset, Mode
+				{"RESET"          , { 0,  8, 0, Register::WRITE}},
+				{"VERSION"        , { 0,  8, 0, Register::READ}},
+				{"START"          , { 1,  8, 0, Register::WRITE}},
+				
+				{"READY"          , { 2,  1, 0, Register::READ}},
+				{"SYNCING"        , { 2,  1, 1, Register::READ}},
+				{"EXT_START_EN"   , { 2,  1, 2, Register::READ_WRITE}},
+				{"EXT_TRIGGER_EN" , { 2,  1, 3, Register::READ_WRITE}},
+				{"OUTPUT_EN"      , { 2,  1, 4, Register::READ_WRITE}},
+				{"BYPASS_MODE"    , { 2,  1, 5, Register::READ_WRITE}},
+				{"CHIP_TYPE"      , { 2,  2, 6, Register::READ_WRITE}},
+
+				{"SIZE"           , { 3, 16, 0, Register::READ_WRITE}},
+				{"REPETITIONS"    , { 5, 16, 0, Register::READ_WRITE}},
+				{"MEM_BYTES"      , { 7, 16, 0, Register::READ}},
+				{"AZ_VETO_CYCLES" , { 9, 16, 0, Register::READ_WRITE}},
+
+				{"AUTO_SYNC"      , {11,  1, 2, Register::WRITE}}
+			};
+		}
+
+		void checkVersion ();
+		void init();
+		uint16_t getMemSize();
+		uint16_t getCmdSize();
+		void start();
+		void setSize(uint16_t value);
+		uint16_t getSize(); //same as getCmdSize()
+		void setRepetitions(uint16_t value);
+		uint16_t getRepetitions();
+		void setExtTrigger(bool value); 
+		bool getExtTrigger(); 
+		void setExtStart(bool value); 
+		bool getExtStart(); 
+		void setOutputEn(bool value);
+		void setBypassMode(bool value);
+		bool getBypassMode();
+		void setAutoSync(bool value);
+		bool getAutoSync();
+		bool isDone();
+		void setAzVetoCycles(uint16_t value);
+		uint16_t getAzVetoCycles();
+		void setChipType(uint8_t value);
+		void setData (std::vector<uint8_t>& data, uint8_t addr=0);
+		void getData(std::vector<uint8_t>& data, uint8_t size, uint8_t addr=0);
+
+	protected:
+		const uint8_t memOffset = 16;
+		uint16_t memSize = 0;
+};
+
+#endif
