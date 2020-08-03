34a15f562131859532e40244b27807a6cdf908b9
Timon Heim <timon.heim@cern.ch>
2020-08-03 13:59:54 -0700

    Change board files to be default compatible with Rd53B

--- syn/kintex7/rd53_ohio_4x4_160Mbps/board_pkg.vhd
+++ syn/kintex7/rd53_ohio_4x4_160Mbps/board_pkg.vhd
@@ -13,10 +13,10 @@ package board_pkg is
     constant c_FE_TYPE : string := "RD53";
     constant c_RX_NUM_LANES : integer := 4;
     constant c_RX_SPEED : string := "0160";
-    constant c_TX_IDLE_WORD : std_logic_vector(31 downto 0) := x"69696969";
-    constant c_TX_SYNC_WORD : std_logic_vector(31 downto 0) := x"817e6969";
-    constant c_TX_SYNC_INTERVAL : unsigned(7 downto 0) := to_unsigned(31,8);
-    constant c_TX_AZ_WORD : std_logic_vector(31 downto 0) := x"5c5ca69a";
+    constant c_TX_IDLE_WORD : std_logic_vector(31 downto 0) := x"AAAAAAAA";
+    constant c_TX_SYNC_WORD : std_logic_vector(31 downto 0) := x"817e817e";
+    constant c_TX_SYNC_INTERVAL : unsigned(7 downto 0) := to_unsigned(16,8);
+    constant c_TX_AZ_WORD : std_logic_vector(31 downto 0) := x"00000000";
     constant c_TX_AZ_INTERVAL : unsigned(15 downto 0) := to_unsigned(500,16);
     constant c_TX_40_DIVIDER : unsigned(3 downto 0) := to_unsigned(4,4);
 end board_pkg;
