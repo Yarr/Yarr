913410bfe06bc2f631088b76d805c852a0588847
Timon Heim <timon.heim@cern.ch>
2020-08-03 13:58:48 -0700

    Add 1.28Gbps firmware files

---
+++ syn/kintex7/rd53_ohio_4x4_1280Mbps/xpressk7_160/Manifest.py
@@ -0,0 +1,35 @@
+# To list file
+# ls -1 | xargs -I % echo \"%\",
+
+target = "xilinx"
+action = "synthesis"
+
+syn_device = "xc7k160"
+syn_grade = "-2"
+syn_package = "tfbg676"
+syn_top = "top_level"
+syn_project = "yarr"
+syn_tool = "vivado"
+
+#library = "work"
+
+modules = {
+"local" : ["../../../../rtl/common","../../../../rtl/kintex7","../../../../rtl/","../../../../ip-cores/kintex7"],
+#"git" : ["https://bitbucket.org/levkurilenko/aurora_rx.git"]
+}
+
+fetchto = "../../../../ip-cores"
+
+files = [
+#TOP
+"../board_pkg.vhd",
+"../../xpressk7_160_type.vhd",
+"../../bram_yarr_trenz.vhd",
+"../../app_pkg.vhd",
+"../../app.vhd",
+"../../xpressk7.xdc",
+"../../xpressk7-fmc-ohio.xdc",
+"../../xpressk7-timing.xdc",
+"../../version.vhd",
+]
+
