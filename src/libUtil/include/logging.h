c32d234d85a5efd4d6ecaa8ac723dfa8751daff0
Daniel Joseph Antrim <dantrim@lbl.gov>
2020-09-10 12:17:26 -0700

    revert back to updated logger

--- src/libUtil/include/logging.h
+++ src/libUtil/include/logging.h
@@ -13,9 +13,14 @@ typedef std::shared_ptr<spdlog::logger> LoggerStore;
 
 // Make a logger without any sinks
 inline std::shared_ptr<spdlog::logger> make_log(std::string name) {
-  auto tmp = std::make_shared<spdlog::logger>(name, spdlog::sinks_init_list{});
-  spdlog::register_logger(tmp);
-  return tmp;
+  auto log = spdlog::get(name);
+  if(log == nullptr)
+  {
+    auto tmp = std::make_shared<spdlog::logger>(name, spdlog::sinks_init_list{});
+    spdlog::register_logger(tmp);
+    return tmp;
+  }
+  return log;
 }
 
 } // End logging namespace
