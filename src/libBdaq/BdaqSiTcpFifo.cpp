b17280c9a46cb6875e36e7c85ee6a78f63b059dd
Rafael Gama <rafael.gama@cern.ch>
2021-03-12 18:35:04 +0100

    Re-enabled TCP buffer reserve().

--- src/libBdaq/BdaqSiTcpFifo.cpp
+++ src/libBdaq/BdaqSiTcpFifo.cpp
@@ -31,7 +31,7 @@ std::size_t BdaqSiTcpFifo::readData(uint32_t* buffer) {
 
 void BdaqSiTcpFifo::readData(std::vector<uint32_t>& buffer, std::size_t size) {
 	//std::size_t wCount = getAvailableWords();
-	//buffer.reserve(size);
+	buffer.reserve(size);
 	std::vector<uint8_t> buf(size * 4);
 	tcp.read(buf);
 	uint32_t temp;
