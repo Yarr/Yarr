b2dc57fc2046aea91ee6c6f6e06edc3e941828c0
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2021-11-22 12:21:14 -0800

    Read hit counters and data packets

--- src/tools/test_star.cpp
+++ src/tools/test_star.cpp
@@ -26,8 +26,6 @@ static void printHelp();
 
 void sendCommand(const std::array<uint16_t, 9>& cmd, HwController& hwCtrl);
 void sendCommand(uint16_t cmd, HwController& hwCtrl);
-void configureChips(StarCmd &star, HwController& hwCtrl, bool doReset);
-void runTests(StarCmd &star, HwController& hwCtrl, bool hprOff);
 void reportData(RawData &data, bool do_spec_specific=false);
 int packetFromRawData(StarChipPacket& packet, RawData& data);
 std::unique_ptr<RawData, void(*)(RawData*)> readData(HwController&, std::function<bool(RawData&)>, uint32_t timeout=1000);
@@ -43,6 +41,9 @@ bool probeABCs(HwController& hwCtrl, std::vector<Hybrid>& hccStars);
 void configureABC(HwController& hwCtrl, bool doReset);
 bool testHCCRegisterAccess(HwController& hwCtrl, std::vector<Hybrid>& hccStars);
 bool testABCRegisterAccess(HwController& hwCtrl, std::vector<Hybrid>& hccStars);
+bool testHitCounts(HwController& hwCtrl);
+bool testDataPacketsStatic(HwController& hwCtrl, bool do_spec_specific);
+bool testDataPacketsPulse(HwController& hwCtrl, bool do_spec_specific);
 
 int main(int argc, char *argv[]) {
     std::string controller;
@@ -196,28 +197,22 @@ int main(int argc, char *argv[]) {
     // Configure ABCs
     configureABC(*hwCtrl, true);
 
-    // For now
-    //configureChips(star, *hwCtrl, true);
-    runTests(star, *hwCtrl, true);
+    //////////
+    // Read ABC hit counters
+    bool hitCountsOk = testHitCounts(*hwCtrl);
+    if (not hitCountsOk)
+      return 1;
 
-    uint32_t timeout = 2000;
-    auto rdc = readAllData(
-      *hwCtrl,
-      [](RawData& d){
-        return not(isPacketType(d,TYP_HCC_HPR) or isPacketType(d,TYP_ABC_HPR));
-      },
-      timeout
-      );
+    // Read ABC data packets
+    bool dataStaticOk = testDataPacketsStatic(*hwCtrl, controllerType=="spec");
+    if (not dataStaticOk)
+      return 1;
 
-    for (unsigned c = 0; c < rdc.size(); c++) {
-      RawData d(rdc.adr[c], rdc.buf[c], rdc.words[c]);
-      reportData(d, controllerType == "spec");
-    }
+    bool dataPulseOk = testDataPacketsPulse(*hwCtrl, controllerType=="spec");
+    if (not dataPulseOk)
+      return 1;
 
     hwCtrl->disableRx();
-
-    if (rdc.size() == 0)
-      return 1;
 
     return 0;
 }
@@ -230,85 +225,6 @@ void printHelp() {
   std::cout << " -t <channel1> [<channel2> ...] : Tx channels to enable. Can take multiple arguments.\n";
   std::cout << " -l <log_config> : Configure loggers.\n";
   std::cout << " -d : Modify HCCStar IDs when probing.\n";
-}
-
-void runTests(StarCmd &star, HwController& hwCtrl, bool hprOff) {
-
-  if (hprOff) {
-    // Turn off HCC HPR
-    sendCommand( star.write_hcc_register(43, 0x00000100), hwCtrl);
-    sendCommand( star.write_hcc_register(16, 0x00000001), hwCtrl);
-
-    // Turn off ABC HPR
-    sendCommand( star.write_abc_register(32, 0x00000740), hwCtrl);
-    sendCommand( star.write_abc_register(0, 0x00000004), hwCtrl);
-  }
-
-  //////////
-  // Read HCCStar registers
-  // register 17: Addressing
-  sendCommand( star.read_hcc_register(17), hwCtrl);
-  // register 40: ICenable
-  sendCommand( star.read_hcc_register(40), hwCtrl);
-
-  //////////
-  // Read ABCStar registers
-  // register 32: CREG0
-  sendCommand( star.read_abc_register(32), hwCtrl);
-  // register 34: CREG2
-  sendCommand( star.read_abc_register(34), hwCtrl);
-
-  //////////
-  // Read data packets
-
-  // Set some mask registers
-  sendCommand( star.write_abc_register(19, 0xfffe0000), hwCtrl); // MaskInput3
-  sendCommand( star.write_abc_register(23, 0xfffe0000), hwCtrl); // MaskInput7
-
-  // Enable hit counters
-  sendCommand( star.write_abc_register(32, 0x00000760), hwCtrl);
-
-  // Reset and start ABCStar hit counters
-  sendCommand(LCB::fast_command(LCB::ABC_HIT_COUNT_RESET, 0), hwCtrl);
-  sendCommand(LCB::fast_command(LCB::ABC_HIT_COUNT_START, 0), hwCtrl);
-  // Enable PR & LP
-  sendCommand(LCB::fast_command(LCB::HCC_START_PRLP, 0), hwCtrl);
-  // BC Reset
-  sendCommand(LCB::lonely_bcr(), hwCtrl);
-
-  /////
-  // Static test mode: TM = 1
-  sendCommand( star.write_abc_register(32, 0x00010760), hwCtrl);
-
-  // Send a trigger
-  sendCommand( LCB::l0a_mask(1, 42, false), hwCtrl);
-
-  /////
-  // Test pulse mode: TM = 2
-  // Single digital pulse: TestPulseEnable = 1, TestPattEnable = 0
-  sendCommand( star.write_abc_register(32, 0x00020770), hwCtrl);
-
-  // Set the L0 pipeline latency to a smaller value: 15 BC
-  sendCommand( star.write_abc_register(34, 0x0000000f), hwCtrl);
-
-  // Also set BCIDrstDelay in the HCC to L0 latency - 2 to avoid BCID errors
-  sendCommand( star.write_hcc_register(44, 0x0000000d), hwCtrl);
-
-  sendCommand( LCB::lonely_bcr(), hwCtrl);
-
-  // Send a digital pulse followed by a trigger
-  std::array<uint16_t, 9> cmd = {
-    LCB::IDLE, LCB::IDLE,
-    LCB::fast_command(LCB::ABC_DIGITAL_PULSE, 0), LCB::IDLE,
-    LCB::IDLE, LCB::IDLE,
-    LCB::l0a_mask(1, 43, false), LCB::IDLE,
-    LCB::IDLE
-  };
-  sendCommand(cmd, hwCtrl);
-
-  // Read a ABCStar hit count
-  // register 191: HitCountREG63
-  sendCommand(star.read_abc_register(191), hwCtrl);
 }
 
 int packetFromRawData(StarChipPacket& packet, RawData& data) {
@@ -843,4 +759,183 @@ bool testABCRegisterAccess(HwController& hwCtrl, std::vector<Hybrid>& hccStars) 
   }
 
   return success;
+}
+
+bool testHitCounts(HwController& hwCtrl) {
+  logger->info("Test ABCStar hit counters");
+
+  // Enable hit counters
+  logger->debug(" Enable hit counters and set TM to 1 (static test mode)");
+  // TM: 1; RR mode: 1; LP and PR enabled; EnCount: 1;
+  sendCommand(star.write_abc_register(32, 0x00010720), hwCtrl);
+
+  // Set some mask registers
+  logger->debug(" Set MaskInput7 to 0xff000000");
+  sendCommand(star.write_abc_register(23, 0xff000000), hwCtrl); // MaskInput7
+
+  // Reset and start ABCStar hit counters
+  logger->debug(" Reset hit counters");
+  sendCommand(LCB::fast_command(LCB::ABC_HIT_COUNT_RESET, 0), hwCtrl);
+  logger->debug(" Start hit counters");
+  sendCommand(LCB::fast_command(LCB::ABC_HIT_COUNT_START, 0), hwCtrl);
+
+  // Enable PR & LP
+  logger->debug(" Start PRLP");
+  sendCommand(LCB::fast_command(LCB::HCC_START_PRLP, 0), hwCtrl);
+
+  // Send BC reset
+  logger->debug(" Send BC reset");
+  sendCommand(LCB::lonely_bcr(), hwCtrl);
+
+  // Send triggers
+  logger->debug(" Send a trigger 10 times");
+  for (int i=0; i<10; i++) {
+    sendCommand(LCB::l0a_mask(1,42+i,false), hwCtrl);
+  }
+
+  // Stop hit counters
+  logger->debug(" Stop hit counters");
+  sendCommand(LCB::fast_command(LCB::ABC_HIT_COUNT_STOP, 0), hwCtrl);
+
+  // Read a hit counter register that corresponds to the masked strips
+  logger->debug(" Read register 191: HitCountREG63");
+  sendCommand(star.read_abc_register(191), hwCtrl);
+
+  auto data = readData(
+    hwCtrl,
+    [](RawData& d) {return isPacketType(d, TYP_ABC_RR);}
+    );
+
+  hwCtrl.flushBuffer();
+
+  if (data) {
+    StarChipPacket packet;
+    packetFromRawData(packet, *data);
+
+    if (logger->should_log(spdlog::level::debug)) {
+      // print packet
+      std::stringstream os;
+      packet.print_more(os);
+      std::string str = os.str();
+      str.erase(str.end()-1); // strip the extra \n
+      logger->debug(" Received register read packet: {}", str);
+    }
+
+    // expected hit counts
+    unsigned hitcounts_exp = 0x0a0a0a0a;
+    if (packet.value == hitcounts_exp) {
+      logger->info("Hit counts: OK");
+    } else {
+      logger->error("Hit counts do not agree with what is expected: 0x{:08x} (read) 0x{:08x} (expect)", packet.value, hitcounts_exp);
+      return false;
+    }
+  } else {
+    logger->error("Fail to read the hit counter register");
+    return false;
+  }
+
+  return true;
+}
+
+bool testDataPacketsStatic(HwController& hwCtrl, bool do_spec_specific) {
+  logger->info("Read ABCStar data packets in static mode");
+
+  // Static test mode first
+  logger->debug(" Set TM to 1");
+  // TM: 1; RR mode: 1; LP and PR enabled
+  sendCommand(star.write_abc_register(32, 0x00010700), hwCtrl);
+
+  // Set some mask registers
+  logger->debug(" Set MaskInput3 to 0xfffe0000");
+  sendCommand(star.write_abc_register(19, 0xfffe0000), hwCtrl); // MaskInput3
+  logger->debug(" Set MaskInput7 to 0xfffe0000");
+  sendCommand(star.write_abc_register(23, 0xfffe0000), hwCtrl); // MaskInput7
+
+  // Enable PR & LP
+  sendCommand(LCB::fast_command(LCB::HCC_START_PRLP, 0), hwCtrl);
+
+  // BC Reset
+  sendCommand(LCB::lonely_bcr(), hwCtrl);
+
+  // Send a trigger
+  sendCommand(LCB::l0a_mask(1, 233, false), hwCtrl);
+
+  // Read the data packets
+  auto rdc = readAllData(
+    hwCtrl, [](RawData& d) {return isPacketType(d, TYP_LP);}
+    );
+
+  hwCtrl.flushBuffer();
+
+  for (unsigned c = 0; c < rdc.size(); c++) {
+    RawData d(rdc.adr[c], rdc.buf[c], rdc.words[c]);
+    reportData(d, do_spec_specific);
+  }
+
+  if (rdc.size() > 0) {
+    logger->info("Read data packets in static test mode: Ok");
+  } else {
+    logger->error("Fail to read data packets in static test mode");
+    return false;
+  }
+
+  return true;
+}
+
+bool testDataPacketsPulse(HwController& hwCtrl, bool do_spec_specific) {
+  logger->info("Read ABCStar data packets in test pulse mode");
+
+  // Test pulse mode
+  logger->debug(" Set TM to 2 and enable test pulse");
+  // TM: 2; RR mode: 1; LP and PR enabled; TestPulseEnable: 1
+  sendCommand(star.write_abc_register(32, 0x00020710), hwCtrl);
+
+  // Set the L0 pipeline latency to a smaller value: 15
+  logger->debug(" Set L0 latency to 15");
+  sendCommand(star.write_abc_register(34, 0x0000000f), hwCtrl);
+
+  // Set BCIDrstDelay of the HCC to L0 latency - 2 so we won't get BCID errors
+  sendCommand(star.write_hcc_register(44, 0x0000000d), hwCtrl);
+
+  // Set some mask registers
+  logger->debug(" Set MaskInput3 to 0x0000ffff");
+  sendCommand(star.write_abc_register(19, 0x0000ffff), hwCtrl); // MaskInput3
+  logger->debug(" Set MaskInput7 to 0x00ffff00");
+  sendCommand(star.write_abc_register(23, 0x00ffff00), hwCtrl); // MaskInput7
+
+  // BC reset
+  logger->debug(" Send BC reset");
+  sendCommand(LCB::lonely_bcr(), hwCtrl);
+
+  // Send a digital pulse followed by a trigger
+  logger->debug(" Send a digital pulse followed by a trigger");
+  std::array<uint16_t, 9> cmd = {
+    LCB::IDLE, LCB::IDLE,
+    LCB::fast_command(LCB::ABC_DIGITAL_PULSE, 0), LCB::IDLE,
+    LCB::IDLE, LCB::IDLE,
+    LCB::l0a_mask(1, 43, false),
+    LCB::IDLE, LCB::IDLE
+  };
+  sendCommand(cmd, hwCtrl);
+
+  // Read the data packets
+  auto rdc = readAllData(
+    hwCtrl, [](RawData& d) {return isPacketType(d, TYP_LP);}
+    );
+
+  hwCtrl.flushBuffer();
+
+  for (unsigned c = 0; c < rdc.size(); c++) {
+    RawData d(rdc.adr[c], rdc.buf[c], rdc.words[c]);
+    reportData(d, do_spec_specific);
+  }
+
+  if (rdc.size() > 0) {
+    logger->info("Read data packets in test pulse mode: Ok");
+  } else {
+    logger->error("Fail to read data packets in test pulse mode");
+    return false;
+  }
+
+  return true;
 }
