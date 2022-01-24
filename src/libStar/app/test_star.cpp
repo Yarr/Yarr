8daf87f4229981da2b2263166e8feba382e455b6
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2022-01-24 10:18:42 -0800

    Use LoopStatus default constructor

--- src/libStar/app/test_star.cpp
+++ src/libStar/app/test_star.cpp
@@ -125,7 +125,7 @@ RawDataContainer readAllData(
 {
   bool nodata = true;
 
-  RawDataContainer rdc(LoopStatus({},{}));
+  RawDataContainer rdc(LoopStatus{});
 
   std::unique_ptr<RawData, void(*)(RawData*)> data(
     hwCtrl.readData(),
