f35bbfcb257b8f9dbdbde5b1b078101278472093
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2022-02-11 13:50:12 -0800

    add codechecker pipeline run only on demand

---
+++ scripts/fix_cdb.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import json
+ 
+# Opening JSON file
+f = open('checks/compilation_cmds.json')
+ 
+# returns JSON object as
+# a dictionary
+units=list()
+data = json.load(f)
+f.close()
+for unit in data:
+    if  "directory" in unit:
+        if unit["directory"].find("/external") == -1:
+            units.append(unit)
+    else:
+        units.append(unit)
+    
+print(json.dumps(units, indent=4))
+
