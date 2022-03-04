b7614057c19ed9a4b8e493742e7c6da28855a4f5
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2022-03-04 11:28:07 -0800

    Use felix software v4.2

--- src/libFelixClient/FelixController.cpp
+++ src/libFelixClient/FelixController.cpp
@@ -21,18 +21,17 @@ void FelixController::loadConfig(const json &j) {
     fcConfig.on_disconnect_callback = std::bind(&FelixController::on_disconnect, this, std::placeholders::_1);
 
     // Properties
-    // See https://gitlab.cern.ch/atlas-tdaq-felix/felix-interface/-/blob/felix-04-01-01/felix/felix_client_properties.h
-    // felix-interface linked by felix-client-thread tag "felix-04-01-01" is outdated. Use strings directly instead.
-    fcConfig.property["local_ip_or_interface"] = std::string(j["FelixClient"]["local_ip_or_interface"]);
-    fcConfig.property["log_level"] = std::string(j["FelixClient"]["log_level"]);
-    fcConfig.property["timeout"] = std::to_string(unsigned(j["FelixClient"]["timeout"]));
-    fcConfig.property["netio_pages"] = std::to_string(unsigned(j["FelixClient"]["netio_pages"]));
-    fcConfig.property["netio_pagesize"] = std::to_string(unsigned(j["FelixClient"]["netio_pagesize"]));
-    fcConfig.property["bus_interface"] = std::string(j["FelixClient"]["bus_interface"]);
-    fcConfig.property["bus_group_name"] = std::string(j["FelixClient"]["bus_group_name"]);
-    fcConfig.property["bus_dir"] = std::string(j["FelixClient"]["bus_dir"]); // not seem to be used in 4.1.1
-    fcConfig.property["verbose_bus"] = j["FelixClient"]["verbose_bus"] ? "True" : "False";
-    fcConfig.property["verbose_zyre"] = j["FelixClient"]["verbose_zyre"] ? "True" : "False";
+    // See https://gitlab.cern.ch/atlas-tdaq-felix/felix-interface/-/blob/4.2.x/felix/felix_client_properties.h
+    fcConfig.property[FELIX_CLIENT_LOCAL_IP_OR_INTERFACE] = std::string(j["FelixClient"]["local_ip_or_interface"]);
+    fcConfig.property[FELIX_CLIENT_LOG_LEVEL] = std::string(j["FelixClient"]["log_level"]);
+    fcConfig.property[FELIX_CLIENT_TIMEOUT] = std::to_string(unsigned(j["FelixClient"]["timeout"]));
+    fcConfig.property[FELIX_CLIENT_NETIO_PAGES] = std::to_string(unsigned(j["FelixClient"]["netio_pages"]));
+    fcConfig.property[FELIX_CLIENT_NETIO_PAGESIZE] = std::to_string(unsigned(j["FelixClient"]["netio_pagesize"]));
+    fcConfig.property[FELIX_CLIENT_BUS_INTERFACE] = std::string(j["FelixClient"]["bus_interface"]);
+    fcConfig.property[FELIX_CLIENT_BUS_GROUP_NAME] = std::string(j["FelixClient"]["bus_group_name"]);
+    fcConfig.property[FELIX_CLIENT_BUS_DIR] = std::string(j["FelixClient"]["bus_dir"]);
+    fcConfig.property[FELIX_CLIENT_VERBOSE_BUS] = j["FelixClient"]["verbose_bus"] ? "True" : "False";
+    fcConfig.property[FELIX_CLIENT_VERBOSE_ZYRE] = j["FelixClient"]["verbose_zyre"] ? "True" : "False";
 
     // Construct felix client
     client = std::make_unique<FelixClientThread>(fcConfig);
