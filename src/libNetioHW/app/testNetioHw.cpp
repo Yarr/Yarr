82b27c1fe54592885933183ba7e91bf433c78520
Timon Heim <timon.heim@cern.ch>
2022-03-15 09:29:28 -0700

    Clean codestyle NULL => nullptr

--- src/libNetioHW/app/testNetioHw.cpp
+++ src/libNetioHW/app/testNetioHw.cpp
@@ -402,7 +402,7 @@ int main(int argc, char **argv) {
     RawDataContainer datav{LoopStatus()};
     do {
         std::shared_ptr<RawData> data = rxcore->readData();
-        if (data == NULL) {
+        if (data == nullptr) {
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
             cout << "." << flush;
             continue;
