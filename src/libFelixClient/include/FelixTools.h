869faf4bd4fbf88e037a2b50f8a76088f3d39bf6
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2022-03-16 11:43:50 -0700

    Helper function to compute 64-bit Felix ID

---
+++ src/libFelixClient/include/FelixTools.h
@@ -0,0 +1,15 @@
+#ifndef FELIXTOOLS_H
+#define FELIXTOOLS_H
+
+#include <cstdint>
+#include <bitset>
+
+namespace FelixTools {
+
+  using FelixID_t = uint64_t;
+
+  FelixID_t get_fid(uint8_t detectorID, uint16_t connectorID, bool is_virtual, uint16_t linkID, uint8_t elink, bool from_felix, uint8_t protocol, uint8_t streamID);
+
+}
+
+#endif
