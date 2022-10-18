93b53332c0c83710de760d4c4febba733c6f3e2a
Alex Toldaiev <alex.toldaiev@cern.ch>
2022-10-18 21:44:49 +0200

    multi-HCC ready feedback-based DataLoop
    still tested on the emulator, with 4 identical hccs
    
    	modified:   src/libYarr/StdDataLoopFeedback.cpp

--- src/libYarr/StdDataLoopFeedback.cpp
+++ src/libYarr/StdDataLoopFeedback.cpp
@@ -18,14 +18,25 @@ void StdDataLoopFeedback::execPart2() {
     SPDLOG_LOGGER_TRACE(sdllog, "");
     unsigned iterations = 0;
 
+    // all RX channels in the scan
     std::map<uint32_t, uint32_t> channelReceivedTriggersCnt;
     std::map<uint32_t, uint32_t> channelReceivedRRCnt;
     std::map<uint32_t, uint32_t> channelReceivedHPRCnt;
-    bool not_received_all_triggers = true;
+
+    for (unsigned id=0; id<keeper->getNumOfEntries(); id++) {
+        channelReceivedTriggersCnt[id] = 0;
+        channelReceivedRRCnt[id]       = 0;
+        channelReceivedHPRCnt[id]      = 0;
+    }
+
+    // the RX channels that actually received data, and expect feedback
+    std::map<uint32_t, uint32_t> activeChannels;
+
+    bool received_all_triggers = true;
     bool still_time = false;
     uint32_t n_triggers_to_receive = 500; // g_tx->getTrigCnt(); // TODO find this number
     std::chrono::microseconds time_elapsed;
-    std::chrono::microseconds wait_time = 1000 * 10 * g_rx->getWaitTime();
+    std::chrono::microseconds wait_time = 1000 * 10 * g_rx->getWaitTime() * 10; // emulator is slow x10
     std::chrono::microseconds wait_time_2 = 50 * g_rx->getWaitTime();
     std::chrono::time_point<Clock> time_start = Clock::now();
 
@@ -51,9 +62,10 @@ void StdDataLoopFeedback::execPart2() {
                             if (rdcMap[uid] == nullptr) {
                                 rdcMap[uid] = std::make_unique<RawDataContainer>(g_stat->record());
                                 // init active channels with data
-                                channelReceivedTriggersCnt[uid] += 0; // TODO make this logic nicer
-                                channelReceivedRRCnt[uid]       += 0;
-                                channelReceivedHPRCnt[uid]      += 0;
+                                //channelReceivedTriggersCnt[uid] += 0; // TODO make this logic nicer
+                                //channelReceivedRRCnt[uid]       += 0;
+                                //channelReceivedHPRCnt[uid]      += 0;
+                                activeChannels[uid] += 0;
                             }
 
                             rdcMap[uid]->add(dataChunk);
@@ -63,6 +75,11 @@ void StdDataLoopFeedback::execPart2() {
                 }
             } while (newData.size() > 0);
         }
+
+        std::chrono::time_point<Clock> time_current = Clock::now();
+        time_elapsed =
+            std::chrono::duration_cast<std::chrono::microseconds>(time_current - time_start);
+        SPDLOG_LOGGER_DEBUG(sdllog, "--> StdDataLoopFeedback::execPart2 : time_elapsed after first RX = {} (wait time {})", time_elapsed.count(), wait_time.count());
 
         // Gather rest of data after timeout (defined by controller)
         //std::this_thread::sleep_for(g_rx->getWaitTime());
@@ -84,9 +101,10 @@ void StdDataLoopFeedback::execPart2() {
                         if (rdcMap[uid] == nullptr) {
                             rdcMap[uid] = std::make_unique<RawDataContainer>(g_stat->record());
                             // init active channels with data
-                            channelReceivedTriggersCnt[uid] += 0; // TODO make this logic nicer
-                            channelReceivedRRCnt[uid]       += 0;
-                            channelReceivedHPRCnt[uid]      += 0;
+                            //channelReceivedTriggersCnt[uid] += 0; // TODO make this logic nicer
+                            //channelReceivedRRCnt[uid]       += 0;
+                            //channelReceivedHPRCnt[uid]      += 0;
+                            activeChannels[uid] += 0;
                         }
 
                         rdcMap[uid]->add(dataChunk);
@@ -95,6 +113,11 @@ void StdDataLoopFeedback::execPart2() {
                 }
             }
         } while (newData.size() > 0 || g_rx->getCurCount() != 0);
+
+        time_current = Clock::now();
+        time_elapsed =
+            std::chrono::duration_cast<std::chrono::microseconds>(time_current - time_start);
+        SPDLOG_LOGGER_DEBUG(sdllog, "--> StdDataLoopFeedback::execPart2 : time_elapsed after second RX = {} (wait time {})", time_elapsed.count(), wait_time.count());
 
         // push data for processing
         for (auto &[id, rdc] : rdcMap) {
@@ -117,14 +140,16 @@ void StdDataLoopFeedback::execPart2() {
             received_feedback = false;
             std::this_thread::sleep_for(g_rx->getWaitTime());
 
-            //for (const auto &item : rdcMap) {
             // loop over all active data channels, not just the ones from the last push
-            for (const auto &item : channelReceivedTriggersCnt) {
+            //for (const auto &item : channelReceivedTriggersCnt)
+            for (const auto &item : activeChannels) { // TODO: filter channels that received all triggers
                 uint32_t channel_n = item.first;
 
-                received_feedback |= feedbackFromRawDataProcessing->at(channel_n).waitNotEmptyOrDoneOrTimeout(std::chrono::microseconds(100));
+                //SPDLOG_LOGGER_DEBUG(sdllog, "--> StdDataLoopFeedback::execPart2 to receive feedback on channel {}", channel_n);
+                bool received_on_chan = feedbackFromRawDataProcessing->at(channel_n).waitNotEmptyOrDoneOrTimeout(std::chrono::microseconds(100));
+                received_feedback |= received_on_chan;
 
-                if (!received_feedback) continue;
+                if (!received_on_chan) continue;
 
                 // params is a unique_ptr of FeedbackProcessingInfo
                 auto params = feedbackFromRawDataProcessing->at(channel_n).popData();
@@ -141,19 +166,20 @@ void StdDataLoopFeedback::execPart2() {
         } while (received_feedback); // add time out here too?
 
         // test whether all channels received all expected triggers
+        received_all_triggers = true;
         for (auto &[id, received_triggers] : channelReceivedTriggersCnt) {
             SPDLOG_LOGGER_DEBUG(sdllog, "--> StdDataLoopFeedback::execPart2 : chan {} received {} triggers from {}", id, received_triggers, n_triggers_to_receive);
-            not_received_all_triggers = received_triggers < n_triggers_to_receive;
-            if (not_received_all_triggers) break;
+            received_all_triggers &= received_triggers >= n_triggers_to_receive;
+            //if (received_all_triggers) break;
         }
 
-        std::chrono::time_point<Clock> time_current = Clock::now();
+        time_current = Clock::now();
         time_elapsed =
             std::chrono::duration_cast<std::chrono::microseconds>(time_current - time_start);
         still_time = time_elapsed.count() < wait_time.count();
         SPDLOG_LOGGER_DEBUG(sdllog, "--> StdDataLoopFeedback::execPart2 : still_time {} = {} < {}", still_time, time_elapsed.count(), wait_time.count());
 
-    } while (not_received_all_triggers && still_time);
+    } while (!received_all_triggers && still_time);
 
     // send end-of-iteration empty container with LoopStatus::is_end_of_iteration = true
     LoopStatus loop_status_iteration_end({0}, {LoopStyle::LOOP_STYLE_GLOBAL_FEEDBACK});
@@ -163,7 +189,11 @@ void StdDataLoopFeedback::execPart2() {
         keeper->getEntry(id).fe->clipRawData.pushData(std::move(c_iter_end));
     }
 
-    SPDLOG_LOGGER_DEBUG(sdllog, "--> StdDataLoopFeedback::execPart2 feedback FINISH : received {} triggers from {} ({}) elapsed time {} from delay limit {} {}", channelReceivedTriggersCnt[0], n_triggers_to_receive, not_received_all_triggers, time_elapsed.count(), wait_time.count(), time_elapsed.count() < wait_time.count());
+    for (auto &[id, received_triggers] : channelReceivedTriggersCnt) {
+        SPDLOG_LOGGER_DEBUG(sdllog, "--> StdDataLoopFeedback::execPart2 FINISH : chan {} received {} triggers from {}", id, received_triggers, n_triggers_to_receive);
+    }
+
+    SPDLOG_LOGGER_DEBUG(sdllog, "--> StdDataLoopFeedback::execPart2 FINISH : received all triggers = {}, elapsed time {} from delay limit {} => there is still time = {}", received_all_triggers, time_elapsed.count(), wait_time.count(), time_elapsed.count() < wait_time.count());
 
     m_done = true;
     //counter++;
