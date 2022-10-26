01752d0d0c31d952960fc5d772320a94e01d2e8a
Bruce Gallop <bruce.gallop@cern.ch>
2022-10-26 13:59:47 +0100

    Update for merge with updated StarJsonData

--- src/libStar/StarStrobeDelayAnalysis.cpp
+++ src/libStar/StarStrobeDelayAnalysis.cpp
@@ -180,20 +180,20 @@ void StarStrobeDelayFitter::end() {
 
   // For each chip, find max left edge and min right edge then define optimal strobe delay as the 57% point between the two 
   for (unsigned int iChip=0; iChip<(nCol/128); iChip++){
-    upJD->initialiseStarChannelsDataAtProp("ABCStar_" + std::to_string(iChip) + "/OptimalStrobeDelay", 1);
+    upJD->initialiseStarChannelsDataAtProp({"ABCStar_" + std::to_string(iChip), "OptimalStrobeDelay"}, 1);
     double maxLeftEdgeForChip = 0.0;
     double minRightEdgeForChip = 100.0;
     for (unsigned row=0; row<2; row++) {
-      upJD->initialiseStarChannelsDataAtProp("ABCStar_" + std::to_string(iChip) + "/LeftEdge/Row" + std::to_string(row));
-      upJD->initialiseStarChannelsDataAtProp("ABCStar_" + std::to_string(iChip) + "/RightEdge/Row" + std::to_string(row));
+      upJD->initialiseStarChannelsDataAtProp({"ABCStar_" + std::to_string(iChip), "LeftEdge", "Row" + std::to_string(row)});
+      upJD->initialiseStarChannelsDataAtProp({"ABCStar_" + std::to_string(iChip), "RightEdge", "Row" + std::to_string(row)});
       for (unsigned iStrip=0; iStrip<128; iStrip++) {
 	unsigned iChannel = iStrip + row*nCol + 128*iChip;
 	double leftEdgeThisChannel = m_leftEdgeMap.at(iChannel);
 	double rightEdgeThisChannel = m_rightEdgeMap.at(iChannel);
 	hDistLeftEdge->fill(leftEdgeThisChannel);
 	hDistRightEdge->fill(rightEdgeThisChannel);
-	upJD->setValForProp("ABCStar_" + std::to_string(iChip) + "/LeftEdge/Row" + std::to_string(row), iStrip, leftEdgeThisChannel);
-	upJD->setValForProp("ABCStar_" + std::to_string(iChip) + "/RightEdge/Row" + std::to_string(row), iStrip, rightEdgeThisChannel);
+	upJD->setValForProp({"ABCStar_" + std::to_string(iChip), "LeftEdge", "Row" + std::to_string(row)}, iStrip, leftEdgeThisChannel);
+        upJD->setValForProp({"ABCStar_" + std::to_string(iChip), "RightEdge", "Row" + std::to_string(row)}, iStrip, rightEdgeThisChannel);
 	if (leftEdgeThisChannel > maxLeftEdgeForChip){
 	  maxLeftEdgeForChip = leftEdgeThisChannel;
 	}
@@ -208,7 +208,7 @@ void StarStrobeDelayFitter::end() {
       strobeDelayOpt = maxLeftEdgeForChip + 0.57*(minRightEdgeForChip - maxLeftEdgeForChip);
     }
     alog->debug("  Found optimal strobe delay = {} for chip {}", strobeDelayOpt, iChip);
-    upJD->setValForProp("ABCStar_" + std::to_string(iChip) + "/OptimalStrobeDelay", 0, strobeDelayOpt);   
+    upJD->setValForProp({"ABCStar_" + std::to_string(iChip), "OptimalStrobeDelay"}, 0, strobeDelayOpt);   
   } // end loop over chips
 
   double leftEdgeMean = hDistLeftEdge->getMean();
@@ -232,9 +232,9 @@ void StarStrobeDelayFitter::end() {
     for (unsigned int iChip=0; iChip<(nCol/128); iChip++) {
       Histo1d * hOccVsStrobeDelayPerRow = m_hOccVsStrobeDelayVsChannelPerRow[iChip*2 + row]->profileY();
       const unsigned n_par = 4;
-      upJD->initialiseStarChannelsDataAtProp("ABCStar_" + std::to_string(iChip) + "/FitParamsLeft/Row" + std::to_string(row), n_par);
-      upJD->initialiseStarChannelsDataAtProp("ABCStar_" + std::to_string(iChip) + "/FitParamsRight/Row" + std::to_string(row), n_par);
-      upJD->initialiseStarChannelsDataAtProp("ABCStar_" + std::to_string(iChip) + "/Width/Row" + std::to_string(row), 1);
+      upJD->initialiseStarChannelsDataAtProp({"ABCStar_" + std::to_string(iChip), "FitParamsLeft", "Row" + std::to_string(row)}, n_par);
+      upJD->initialiseStarChannelsDataAtProp({"ABCStar_" + std::to_string(iChip), "FitParamsRight", "Row" + std::to_string(row)}, n_par);
+      upJD->initialiseStarChannelsDataAtProp({"ABCStar_" + std::to_string(iChip), "Width", "Row" + std::to_string(row)}, 1);
       for (unsigned int iStrip=0; iStrip<128; iStrip++){
 	for (unsigned int sd=m_strobeDelayMin; sd<=m_strobeDelayMax; sd+=m_strobeDelayStep){
 	  int binNum = m_hOccVsStrobeDelayVsChannelPerRow[iChip*2 + row]->binNum(iStrip, sd);
@@ -244,10 +244,10 @@ void StarStrobeDelayFitter::end() {
       } // end loop over strips
       std::vector<std::vector<double>> fitParamsPerRow = fitDoubleScurve(hOccVsStrobeDelayPerRow);
       for (unsigned int ipar=0; ipar<n_par; ipar++){
-	upJD->setValForProp("ABCStar_" + std::to_string(iChip) + "/FitParamsLeft/Row" + std::to_string(row), ipar, fitParamsPerRow[0][ipar]);
-	upJD->setValForProp("ABCStar_" + std::to_string(iChip) + "/FitParamsRight/Row" + std::to_string(row), ipar, fitParamsPerRow[1][ipar]);
+	upJD->setValForProp({"ABCStar_" + std::to_string(iChip), "FitParamsLeft", "Row" + std::to_string(row)}, ipar, fitParamsPerRow[0][ipar]);
+	upJD->setValForProp({"ABCStar_" + std::to_string(iChip), "FitParamsRight", "Row" + std::to_string(row)}, ipar, fitParamsPerRow[1][ipar]);
       }
-      upJD->setValForProp("ABCStar_" + std::to_string(iChip) + "/Width/Row" + std::to_string(row), 0, fitParamsPerRow[1][0] - fitParamsPerRow[0][0]);
+      upJD->setValForProp({"ABCStar_" + std::to_string(iChip), "Width", "Row" + std::to_string(row)}, 0, fitParamsPerRow[1][0] - fitParamsPerRow[0][0]);
       std::unique_ptr<Histo1d> uphOccVsStrobeDelayPerRow;
       uphOccVsStrobeDelayPerRow.reset(hOccVsStrobeDelayPerRow);
       output->pushData(std::move(uphOccVsStrobeDelayPerRow));
