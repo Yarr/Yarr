42c8ed60c69a7596781e3c550fa181d3744b8bc3
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2022-12-02 15:53:02 -0800

    Some cleanup

--- src/libYarr/include/YarrDiagram.h
+++ src/libYarr/include/YarrDiagram.h
@@ -7,118 +7,8 @@
 #include "Bookkeeper.h"
 #include "DataProcessor.h"
 
-struct Node {
-
-  virtual ~Node() = default;
-
-  // connections
-  std::vector<Node*> upstreams;
-  std::vector<Node*> downstreams;
-
-  // labels
-  std::string label;
-  std::string label_left;
-  std::string label_right;
-
-  // position
-  std::pair<float,float> position;
-
-  // size
-  float length {4};
-  float width {1};
-
-  const void* objPtr;
-
-  virtual void setPosition(float, float) = 0;
-
-  virtual void toJson(json&) const = 0;
-};
-
-// Rectangle
-struct Box : Node {
-
-  float x0;
-  float x1;
-  float y0;
-  float y1;
-
-  void setPosition(float x, float y) override {
-    position = {x, y};
-
-    x0 = x - length/2;
-    x1 = x + length/2;
-    y0 = y - width/2;
-    y1 = y + width/2;
-  }
-
-  void toJson(json& j) const override {
-    j["shape"] = "rectangle";
-    j["x"] = {x0, x1};
-    j["y"] = {y0, y1};
-
-    if (not label.empty())
-      j["label"] = label;
-
-    if (not label_left.empty())
-      j["label_left"] = label_left;
-
-    if (not label_right.empty())
-      j["label_right"] = label_right;
-  }
-
-};
-
-// Hexagon
-struct Hex : Node {
-
-  std::vector<std::pair<float, float>> vertices;
-
-  void setPosition(float x, float y) override {
-    position = {x , y};
-
-    vertices.clear();
-    vertices.emplace_back(position.first - 0.45*length, position.second - 0.5*width);
-    vertices.emplace_back(position.first - 0.50*length, position.second);
-    vertices.emplace_back(position.first - 0.45*length, position.second + 0.5*width);
-    vertices.emplace_back(position.first + 0.45*length, position.second + 0.5*width);
-    vertices.emplace_back(position.first + 0.50*length, position.second);
-    vertices.emplace_back(position.first + 0.45*length, position.second - 0.5*width);
-    vertices.emplace_back(position.first - 0.45*length, position.second - 0.5*width);
-  }
-
-  void toJson(json& j) const override {
-
-    j["shape"] = "hexagon";
-    j["x"] = json::array();
-    j["y"] = json::array();
-    for (unsigned v = 0; v < vertices.size(); v++) {
-      j["x"][v] = vertices[v].first;
-      j["y"][v] = vertices[v].second;
-    }
-
-    if (not label.empty())
-      j["label"] = label;
-
-    if (not label_left.empty())
-      j["label_left"] = label_left;
-
-    if (not label_right.empty())
-      j["label_right"] = label_right;
-  }
-
-};
-
-// Straing Line
-struct Line {
-  std::pair<float, float> start;
-  std::pair<float, float> end;
-
-  void toJson(json& j) const {
-    j["shape"] = "line";
-    j["x"] = {start.first, end.first};
-    j["y"] = {start.second, end.second};
-  }
-};
+class Node;
+class Line;
 
 class YarrDiagram {
 
@@ -128,7 +18,7 @@ class YarrDiagram {
     ~YarrDiagram() = default;
 
     void makeDiagram(
-      Bookkeeper& keeper, 
+      Bookkeeper& keeper,
       const std::map<unsigned, std::unique_ptr<DataProcessor>>& data_procs,
       const std::map<unsigned, std::unique_ptr<DataProcessor>>& hist_procs,
       const std::map<unsigned, std::vector<std::unique_ptr<DataProcessor>>>& ana_procs
@@ -175,6 +65,116 @@ class YarrDiagram {
     float margin_bottom {0.5};
     float margin_left {1.5};
     float margin_right {0.5};
+};
+
+struct Node {
+
+  virtual ~Node() = default;
+
+  // connections
+  std::vector<Node*> upstreams;
+  std::vector<Node*> downstreams;
+
+  // labels
+  std::string label;
+  std::string label_left;
+  std::string label_right;
+
+  // position
+  std::pair<float,float> position;
+
+  // size
+  float length {4};
+  float width {1};
+
+  const void* objPtr;
+
+  virtual void setPosition(float, float) = 0;
+
+  virtual void toJson(json& j) const {
+    if (not label.empty())
+      j["label"] = label;
+
+    if (not label_left.empty())
+      j["label_left"] = label_left;
+
+    if (not label_right.empty())
+      j["label_right"] = label_right;
+  }
+};
+
+// Rectangle
+struct Box : Node {
+
+  float x0;
+  float x1;
+  float y0;
+  float y1;
+
+  void setPosition(float x, float y) override {
+    position = {x, y};
+
+    x0 = x - length/2;
+    x1 = x + length/2;
+    y0 = y - width/2;
+    y1 = y + width/2;
+  }
+
+  void toJson(json& j) const override {
+    j["shape"] = "rectangle";
+    j["x"] = {x0, x1};
+    j["y"] = {y0, y1};
+
+    // Add labels
+    Node::toJson(j);
+  }
+
+};
+
+// Hexagon
+struct Hex : Node {
+
+  std::vector<std::pair<float, float>> vertices;
+
+  void setPosition(float x, float y) override {
+    position = {x , y};
+
+    vertices.clear();
+    vertices.emplace_back(position.first - 0.45*length, position.second - 0.5*width);
+    vertices.emplace_back(position.first - 0.50*length, position.second);
+    vertices.emplace_back(position.first - 0.45*length, position.second + 0.5*width);
+    vertices.emplace_back(position.first + 0.45*length, position.second + 0.5*width);
+    vertices.emplace_back(position.first + 0.50*length, position.second);
+    vertices.emplace_back(position.first + 0.45*length, position.second - 0.5*width);
+    vertices.emplace_back(position.first - 0.45*length, position.second - 0.5*width);
+  }
+
+  void toJson(json& j) const override {
+
+    j["shape"] = "hexagon";
+    j["x"] = json::array();
+    j["y"] = json::array();
+    for (unsigned v = 0; v < vertices.size(); v++) {
+      j["x"][v] = vertices[v].first;
+      j["y"][v] = vertices[v].second;
+    }
+
+    // Add labels
+    Node::toJson(j);
+  }
+
+};
+
+// Straing Line
+struct Line {
+  std::pair<float, float> start;
+  std::pair<float, float> end;
+
+  void toJson(json& j) const {
+    j["shape"] = "line";
+    j["x"] = {start.first, end.first};
+    j["y"] = {start.second, end.second};
+  }
 };
 
 #endif
