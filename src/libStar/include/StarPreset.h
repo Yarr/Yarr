43b2deadd475078a0e3e1c2d31bbaedc43a3f9bc
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2023-03-22 15:51:04 -0700

    Add an 'offset' to HybridInfo and use it to associate ABC IDs to HCC input channels

--- src/libStar/include/StarPreset.h
+++ src/libStar/include/StarPreset.h
@@ -7,45 +7,45 @@
 namespace StarPreset {
 
   struct HybridInfo {
-    std::string name; unsigned hccID; unsigned numABCs; unsigned tx; unsigned rx;
+    std::string name; unsigned hccID; unsigned numABCs;  unsigned offset; unsigned tx; unsigned rx;
   };
 
   // 14 HCCStars on one side of a long strip stave
   // An example configuration based on the ITSDAQ GLIB setup
   const std::array<HybridInfo, 14> lsstave {{
-      {.name="LS0",  .hccID=0,  .numABCs=10, .tx=100, .rx=13 },
-      {.name="LS1",  .hccID=1,  .numABCs=10, .tx=100, .rx=12 },
-      {.name="LS2",  .hccID=2,  .numABCs=10, .tx=100, .rx=11 },
-      {.name="LS3",  .hccID=3,  .numABCs=10, .tx=100, .rx=10 },
-      {.name="LS4",  .hccID=4,  .numABCs=10, .tx=101, .rx=9  },
-      {.name="LS5",  .hccID=5,  .numABCs=10, .tx=101, .rx=8  },
-      {.name="LS6",  .hccID=6,  .numABCs=10, .tx=101, .rx=7  },
-      {.name="LS7",  .hccID=7,  .numABCs=10, .tx=101, .rx=6  },
-      {.name="LS8",  .hccID=8,  .numABCs=10, .tx=102, .rx=5  },
-      {.name="LS9",  .hccID=9,  .numABCs=10, .tx=102, .rx=4  },
-      {.name="LS10", .hccID=10, .numABCs=10, .tx=102, .rx=3  },
-      {.name="LS11", .hccID=11, .numABCs=10, .tx=102, .rx=2  },
-      {.name="LS12", .hccID=12, .numABCs=10, .tx=102, .rx=1  },
-      {.name="LS13", .hccID=13, .numABCs=10, .tx=103, .rx=0  }
+      {.name="LS0",  .hccID=0,  .numABCs=10, .offset=1, .tx=100, .rx=13 },
+      {.name="LS1",  .hccID=1,  .numABCs=10, .offset=1, .tx=100, .rx=12 },
+      {.name="LS2",  .hccID=2,  .numABCs=10, .offset=1, .tx=100, .rx=11 },
+      {.name="LS3",  .hccID=3,  .numABCs=10, .offset=1, .tx=100, .rx=10 },
+      {.name="LS4",  .hccID=4,  .numABCs=10, .offset=1, .tx=101, .rx=9  },
+      {.name="LS5",  .hccID=5,  .numABCs=10, .offset=1, .tx=101, .rx=8  },
+      {.name="LS6",  .hccID=6,  .numABCs=10, .offset=1, .tx=101, .rx=7  },
+      {.name="LS7",  .hccID=7,  .numABCs=10, .offset=1, .tx=101, .rx=6  },
+      {.name="LS8",  .hccID=8,  .numABCs=10, .offset=1, .tx=102, .rx=5  },
+      {.name="LS9",  .hccID=9,  .numABCs=10, .offset=1, .tx=102, .rx=4  },
+      {.name="LS10", .hccID=10, .numABCs=10, .offset=1, .tx=102, .rx=3  },
+      {.name="LS11", .hccID=11, .numABCs=10, .offset=1, .tx=102, .rx=2  },
+      {.name="LS12", .hccID=12, .numABCs=10, .offset=1, .tx=102, .rx=1  },
+      {.name="LS13", .hccID=13, .numABCs=10, .offset=1, .tx=103, .rx=0  }
   }};
 
   // 14 HCCStars on one side of a petal
   // An example configuration based on the ITSDAQ GLIB setup
   const std::array<HybridInfo, 14> petal {{
-      {.name="R0_H0", .hccID=0,  .numABCs=8,  .tx=102, .rx=16 },
-      {.name="R0_H1", .hccID=1,  .numABCs=9,  .tx=102, .rx=24 },
-      {.name="R1_H0", .hccID=2,  .numABCs=10, .tx=102, .rx=20 },
-      {.name="R1_H1", .hccID=3,  .numABCs=11, .tx=102, .rx=26 },
-      {.name="R2_H0", .hccID=4,  .numABCs=6,  .tx=101, .rx=22 },
-      {.name="R2_H1", .hccID=5,  .numABCs=6,  .tx=101, .rx=12 },
-      {.name="R3_H0", .hccID=6,  .numABCs=7,  .tx=101, .rx=14 },
-      {.name="R3_H1", .hccID=7,  .numABCs=7,  .tx=101, .rx=18 },
-      {.name="R3_H2", .hccID=8,  .numABCs=7,  .tx=101, .rx=0  },
-      {.name="R3_H3", .hccID=9,  .numABCs=7,  .tx=101, .rx=2  },
-      {.name="R4_H0", .hccID=10, .numABCs=8,  .tx=103, .rx=4  },
-      {.name="R4_H1", .hccID=11, .numABCs=8,  .tx=103, .rx=8  },
-      {.name="R5_H0", .hccID=12, .numABCs=9,  .tx=103, .rx=6  },
-      {.name="R5_H1", .hccID=13, .numABCs=9,  .tx=103, .rx=10 }
+      {.name="R0_H0", .hccID=0,  .numABCs=8,  .offset=3, .tx=102, .rx=16 },
+      {.name="R0_H1", .hccID=1,  .numABCs=9,  .offset=0, .tx=102, .rx=24 },
+      {.name="R1_H0", .hccID=2,  .numABCs=10, .offset=1, .tx=102, .rx=20 },
+      {.name="R1_H1", .hccID=3,  .numABCs=11, .offset=0, .tx=102, .rx=26 },
+      {.name="R2_H0", .hccID=4,  .numABCs=6,  .offset=5, .tx=101, .rx=22 },
+      {.name="R2_H1", .hccID=5,  .numABCs=6,  .offset=5, .tx=101, .rx=12 },
+      {.name="R3_H0", .hccID=6,  .numABCs=7,  .offset=4, .tx=101, .rx=14 },
+      {.name="R3_H1", .hccID=7,  .numABCs=7,  .offset=4, .tx=101, .rx=18 },
+      {.name="R3_H2", .hccID=8,  .numABCs=7,  .offset=0, .tx=101, .rx=0  },
+      {.name="R3_H3", .hccID=9,  .numABCs=7,  .offset=0, .tx=101, .rx=2  },
+      {.name="R4_H0", .hccID=10, .numABCs=8,  .offset=3, .tx=103, .rx=4  },
+      {.name="R4_H1", .hccID=11, .numABCs=8,  .offset=3, .tx=103, .rx=8  },
+      {.name="R5_H0", .hccID=12, .numABCs=9,  .offset=2, .tx=103, .rx=6  },
+      {.name="R5_H1", .hccID=13, .numABCs=9,  .offset=2, .tx=103, .rx=10 }
   }};
 
   std::tuple<json, std::vector<json>> createConfigStarObject(
@@ -73,12 +73,20 @@ namespace StarPreset {
 
       // Add ABCStars
       feCfg.clearABCchipIDs();
-      uint32_t maskHccIn = 0;
-      for (int iABC=0; iABC<modules[i].numABCs; iABC++) {
-        feCfg.addABCchipID(iABC);
-        maskHccIn = (maskHccIn<<1) + 1;
+
+      unsigned maskHccIn = 0;
+
+      for (unsigned iABC=0; iABC<modules[i].numABCs; iABC++) {
+        // ABC chip ID
+        unsigned abcID = iABC + modules[i].offset;
+        // HCC input channel
+        unsigned hccIn = feCfg.hccVersion() > 0 ? abcID : 10-abcID;
+
+        feCfg.addABCchipID(abcID, hccIn);
+        maskHccIn += (1<<hccIn);
       }
-      // And set HCC's IC_Enable accordingly
+
+      // Set HCC's IC_Enable accordingly
       feCfg.hcc().setSubRegisterValue("ICENABLE", maskHccIn);
 
       json chipCfg;
@@ -101,7 +109,7 @@ namespace StarPreset {
     // And set HCC's IC_Enable accordingly
     feCfg.hcc().setSubRegisterValue("ICENABLE", 0x1);
 
-    // Config for one HCCStar + several ABCStars
+    // Config for one HCCStar + one ABCStar
     json cfg;
     feCfg.writeConfig(cfg);
     chips.push_back(std::move(cfg));
