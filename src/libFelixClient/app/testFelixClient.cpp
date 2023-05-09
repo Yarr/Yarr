e0bb3ebbb159e6f3a7d40d9f9b3a216bf3f8f123
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2023-05-09 00:37:23 -0700

    Allow a list of tx and/or rx elinks for testFelixClient

--- src/libFelixClient/app/testFelixClient.cpp
+++ src/libFelixClient/app/testFelixClient.cpp
@@ -15,8 +15,8 @@ void printHelp() {
   std::cout << "Usage: testFelixClient HW_CONFIG [OPTIONS...]" << std::endl;
   std::cout << "  Test FelixClient controller" << std::endl;
   std::cout << " -h : Show this help." << std::endl;
-  std::cout << " -t ELINK : Tx elink for sending data." << std::endl;
-  std::cout << " -r ELINK : Rx elink for receiving data." << std::endl;
+  std::cout << " -t <TX_ELINK1> [<TX_ELINK2> ...] : A list of tx elinks for sending data." << std::endl;
+  std::cout << " -r <RX_ELINK1> [<RX_ELINK2> ...] : A list of rx elinks for receiving data." << std::endl;
   std::cout << " -l LOG_CONFIG : Configuration for the logger." << std::endl;
 }
 
@@ -24,8 +24,8 @@ int main(int argc, char **argv) {
 
   std::string controllerCfg;
   std::string loggerCfg;
-  int elink_tx = -1;
-  int elink_rx = -1;
+  std::vector<unsigned> elinks_tx;
+  std::vector<unsigned> elinks_rx;
 
   int opt;
   while ((opt = getopt(argc, argv, "ht:r:l:")) != -1) {
@@ -34,10 +34,18 @@ int main(int argc, char **argv) {
       printHelp();
       return 0;
     case 't':
-      elink_tx = atoi(optarg);
-      break;
+      elinks_tx.clear();
+      optind -= 1;
+      for (; optind < argc && *argv[optind] != '-'; optind += 1) {
+        elinks_tx.push_back( atoi(argv[optind]) );
+      }
+       break;
     case 'r':
-      elink_rx = atoi(optarg);
+      elinks_rx.clear();
+      optind -= 1;
+      for (; optind < argc && *argv[optind] != '-'; optind += 1) {
+        elinks_rx.push_back( atoi(argv[optind]) );
+      }
       break;
     case 'l':
       loggerCfg = std::string(optarg);
@@ -98,23 +106,39 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  // Send data
-  if (elink_tx >= 0) {
+  // Subscribe to elinks
+  if (not elinks_rx.empty()) {
     try {
-      logger->info("Sending data to chn {}", elink_tx);
-      hwCtrl->setCmdEnable(elink_tx);
+      hwCtrl->setRxEnable(elinks_rx);
+    } catch (std::runtime_error& e) {
+      logger->error("Fail to subscribe: {}", e.what());
+      return 1;
+    }
 
-      // Read FELIX register
+    dynamic_cast<FelixController*>(hwCtrl.get())->runMonitor(true);
+  }
+
+  // Send data
+  if (not elinks_tx.empty()) {
+    try {
+      // Read FELIX registers
       json j_status = hwCtrl->getStatus();
       j_status.dump();
+    } catch (std::runtime_error& e) {
+      logger->warn("Cannot read FELIX registers");
+    }
 
+    try {
+      hwCtrl->setCmdEnable(elinks_tx);
+
+      logger->info("Write 0xdeadbeef to enabled tx elinks");
       hwCtrl->writeFifo(0xdeadbeef);
       hwCtrl->releaseFifo();
 
-      // triggers
-      uint32_t trigWords[2] = {0x01234567, 0x89abcdef};
+      logger->info("Send triggers");
+      uint32_t trigWords[2] = {0x7259cafe, 0x89abcdef};
       hwCtrl->setTrigWord(trigWords, 2);
-      hwCtrl->setTrigFreq(1);
+      hwCtrl->setTrigFreq(10);
       hwCtrl->setTrigCnt(5);
       hwCtrl->setTrigConfig(INT_COUNT);
 
@@ -123,51 +147,41 @@ int main(int argc, char **argv) {
       hwCtrl->setTrigEnable(0);
 
     } catch (std::runtime_error& e) {
-      logger->error("Failed to send: {}", e.what());
+      logger->error("Fail to send data: {}", e.what());
+      return 1;
     }
   }
 
-  // Receive data
-  if (elink_rx >= 0) {
-    try {
-      hwCtrl->setRxEnable(elink_rx);
+  // Collect and report the received data
+  if (not elinks_rx.empty()) {
+    // Wait for data
+    logger->info("Waiting for data...");
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 
-      // start monitoring
-      dynamic_cast<FelixController*>(hwCtrl.get())->runMonitor(true);
+    dynamic_cast<FelixController*>(hwCtrl.get())->stopMonitor();
 
-      // wait for data
-      logger->info("Waiting for data...");
-      std::this_thread::sleep_for(std::chrono::seconds(5));
+    hwCtrl->disableRx();
 
-      dynamic_cast<FelixController*>(hwCtrl.get())->stopMonitor();
-
-      hwCtrl->disableRx();
-
-      // Collect and report data
-      RawDataContainer rdc({});
-      while (hwCtrl->getCurCount()) {
-        auto dataVec = hwCtrl->readData();
-        for(auto data : dataVec) {
-          rdc.add(data);
-        }
+    RawDataContainer rdc({});
+    while (hwCtrl->getCurCount()) {
+      auto dataVec = hwCtrl->readData();
+      for(auto data : dataVec) {
+        rdc.add(data);
       }
+    }
 
-      logger->info("Received {} data", rdc.size());
-      logger->info("Rate: {} B/s", hwCtrl->getDataRate());
+    logger->info("Received {} data", rdc.size());
+    logger->info("Rate: {} B/s", hwCtrl->getDataRate());
 
-      for (auto rdp : rdc.data) {
-        std::stringstream ss;
-        ss << " " << rdp->getAdr() << " " << rdp->getBuf();
-        ss << " " << rdp->getSize() << " : " << std::hex;
+    for (auto rdp : rdc.data) {
+      std::stringstream ss;
+      ss << " " << rdp->getAdr() << " " << rdp->getBuf();
+      ss << " " << rdp->getSize() << " : " << std::hex;
 
-        for (unsigned j=0; j<rdp->getSize(); ++j) {
-          ss << "0x" << rdp->get(j) << " ";
-        }
-        logger->debug(" {}", ss.str());
+      for (unsigned j=0; j<rdp->getSize(); ++j) {
+        ss << "0x" << rdp->get(j) << " ";
       }
-
-    } catch (std::runtime_error& e) {
-      logger->error("Failed to subscribe: {}", e.what());
+       logger->debug(" {}", ss.str());
     }
   }
 
