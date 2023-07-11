416bb174fd5e7ff1ed328d6f09b43161c12d0b90
arastogi <rastogiangira0908@gmail.com>
2023-07-11 15:51:51 -0700

    Adding a new function for trigger multiplier in base class

--- src/libYarr/include/StdTriggerAction.h
+++ src/libYarr/include/StdTriggerAction.h
@@ -10,10 +10,13 @@ class StdTriggerAction {
     public:
         virtual uint32_t getTrigCnt() { return m_trigCnt; }
         void setTrigCnt(uint32_t cnt) { m_trigCnt = cnt; }
-        
+        virtual uint32_t getTrigMulti() { return m_trigMulti; }
+        void setTrigMulti(uint32_t multi) { m_trigMulti = multi; }
+
         virtual ~StdTriggerAction() = default;;
     protected:
         uint32_t m_trigCnt;
+	    uint32_t m_trigMulti;
 };
 
 #endif
