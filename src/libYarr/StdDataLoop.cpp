5f2e8186749e07cb40ca75e1a2751a70201484a8
arastogi <rastogiangira0908@gmail.com>
2023-07-12 16:33:15 -0700

    Replacing with the new function of trigger base class

--- src/libYarr/StdDataLoop.cpp
+++ src/libYarr/StdDataLoop.cpp
@@ -30,9 +30,8 @@ StdDataLoop::StdDataLoop() : LoopActionBase(LOOP_STYLE_DATA) {
 void StdDataLoop::init() {
     m_done = false;
     auto trigAction = keeper->getTriggerAction();
-    if (trigAction != nullptr) n_triggersToReceive = trigAction->getTrigCnt()*trigAction->getTrigMulti() - n_triggersLostTolerance;
+    if (trigAction != nullptr) n_triggersToReceive = trigAction->getExpEvents() - n_triggersLostTolerance;
     SPDLOG_LOGGER_TRACE(sdllog, "");
-    SPDLOG_LOGGER_INFO(sdllog, "Number of triggers to receive={}",n_triggersToReceive);
 }
 
 void StdDataLoop::end() {
