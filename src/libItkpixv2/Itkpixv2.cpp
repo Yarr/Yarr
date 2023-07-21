d85012edab3090a1799475abd7cf4f8ce2708bef
Timon Heim <timon.heim@cern.ch>
2023-07-21 09:34:05 -0700

    Remove rewriting of magic reg

--- src/libItkpixv2/Itkpixv2.cpp
+++ src/libItkpixv2/Itkpixv2.cpp
@@ -127,12 +127,6 @@ void Itkpixv2::configureInit() {
     // Reset register
     this->writeRegister(&Itkpixv2::GlobalPulseConf, 0);
     
-    logger->debug(" ... set global register in writeable mode");
-    this->writeRegister(&Itkpixv2::GcrDefaultConfig, 0xAC75);
-    this->writeRegister(&Itkpixv2::GcrDefaultConfigB, 0x538A);
-    while(!core->isCmdEmpty()){;}
-
-    
     // Reset Core
     logger->debug("Reset Cores!");
     uint16_t tmpRstCoreCol0 = this->RstCoreCol0.read();
