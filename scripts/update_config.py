28b109b012075676f4b5363159d1fdda7cd3054c
Maria Miranova <maria.mironova@cern.ch>
2023-11-07 16:30:54 -0800

    Clean up update config script

--- scripts/update_config.py
+++ scripts/update_config.py
@@ -1,30 +1,55 @@
+'''
+Script to update exisiting chip configs from 640 Mbps to 1.28 Gbps 
+Mainly targeted at moduleQC 
+
+Author: Maria Mironova (maria.mironova@cern.ch)
+
+usage: scripts/update_config.py [-h] [-c CONFIG_FILE] [-t CHIP_TYPE]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CONNECTIVITY_FILE, --connectivity CONNECTIVITY_FILE
+                        Connectivity file
+'''
+
 import json 
 import sys
 import os
-
-config_file=sys.argv[1]
-
-# Opening JSON file
-f = open(config_file)
-  
-# returns JSON object as 
-# a dictionary
-data = json.load(f)
-
-for j in range(0,len(data["chips"])):
-    print("Configuring Chip %s"%j)
-    chip_config=data["chips"][j]['config']
-    f_chip=open(chip_config)
-    data_chip=json.load(f_chip)
-    data_chip["RD53B"]["GlobalConfig"]["CdrClkSel"]=0
-    data_chip["RD53B"]["GlobalConfig"]["CmlBias0"]=800
-    data_chip["RD53B"]["GlobalConfig"]["CmlBias1"]=400
-    data_chip["RD53B"]["GlobalConfig"]["MonitorV"]=32
-
-    data_chip["RD53B"]["GlobalConfig"]["MonitorEnable"]=1
-
-    with open(chip_config,'w') as outfile:
-        outfile.write(json.dumps(data_chip, sort_keys=True, indent=4))
-    outfile.close()
+import argparse
 
 
+
+def update_config(connectivity_file):
+    # Opening JSON file
+    f = open(connectivity_file)
+    
+    # returns JSON object as 
+    # a dictionary
+    data = json.load(f)
+    chip_type=data["chipType"]
+    print(chip_type)
+    for j in range(0,len(data["chips"])):
+        chip_config=data["chips"][j]['config']
+        print("Updating chip config %s"%(data["chips"][j]['config']))
+
+        f_chip=open(chip_config)
+        data_chip=json.load(f_chip)
+        data_chip[chip_type]["GlobalConfig"]["CdrClkSel"]=0
+        data_chip[chip_type]["GlobalConfig"]["CmlBias0"]=800
+        data_chip[chip_type]["GlobalConfig"]["CmlBias1"]=400
+        data_chip[chip_type]["GlobalConfig"]["MonitorV"]=32
+
+        data_chip[chip_type]["GlobalConfig"]["MonitorEnable"]=1
+
+        with open(chip_config,'w') as outfile:
+            outfile.write(json.dumps(data_chip, sort_keys=True, indent=4))
+        outfile.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--connectivity', dest="connectivity_file", help="Connectivity file")
+    args = vars(parser.parse_args())
+
+    connectivity_file=args["connectivity_file"]
+    update_config(connectivity_file)
