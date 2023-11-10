be666718fd32f69de3e0ad010710b189223a5458
Timon Heim <timon.heim@cern.ch>
2023-11-10 11:49:27 -0800

    Add new firmware version hash

--- src/libSpec/include/SpecCom.h
+++ src/libSpec/include/SpecCom.h
@@ -58,7 +58,7 @@ const static std::string specIdentSpeed[] = {"undefined", "160Mbps", "320Mbps", 
 const static std::string specIdentChCfg[] = {"undefined", "4x4", "16x1", "8x4", "32x1", "3x4 TLU", "3x4 Ext Trig", "12x1 TLU", "12x1 Ext Trig"};
 const static uint32_t specIdentLaneCfg[] = {0, 4, 1, 4, 1, 4, 4, 1, 1};
 
-static std::map<uint16_t, std::string> specIdentFw = {{0x4256c32, "v1.3.1"},{0x2779a56, "v1.3"}, {0x4d9ff6d, "v1.2.1"}, {0x1493b73, "v1.1.1"}};
+static std::map<uint16_t, std::string> specIdentFw = {{0xe7985d6, "v1.4.0"},{0x4256c32, "v1.3.1"},{0x2779a56, "v1.3"}, {0x4d9ff6d, "v1.2.1"}, {0x1493b73, "v1.1.1"}};
 
 struct dma_linked_list {
     uint32_t carrier_start;
