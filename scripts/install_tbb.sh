e9702b9019f77857961c105256e11a0954a6a274
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2023-11-30 16:16:39 -0800

    Revert to old tbb

--- scripts/install_tbb.sh
+++ scripts/install_tbb.sh
@@ -2,6 +2,6 @@
 mkdir -p  ${2}/include
 mkdir -p  ${2}/lib
 mkdir -p  ${2}/bin
-cp -r ${1}/external/src/tbb_2021/include/tbb  ${2}/include/
-cp $(find ${1}/external/src/tbb_2021/ -name *.a) ${2}/lib/
+cp -r ${1}/external/src/tbb_2020/include/tbb  ${2}/include/
+cp $(find ${1}/external/src/tbb_2020/ -name *.a) ${2}/lib/
 
