7fb7767b057ecbaa750539b6e1be374886751844
Ondra Kovanda <ondrej.kovanda@cern.ch>
2024-05-16 19:24:14 +0200

    Working itkpixv2 emu tx connection, but still wrongly passed to the chip emu constructor

--- src/libItkpixv2Emu/Itkpixv2Emu.cpp
+++ src/libItkpixv2Emu/Itkpixv2Emu.cpp
@@ -5,15 +5,55 @@
 */
 
 #include "Itkpixv2Emu.h"
+#include <iostream>
 
-Itkpixv2Emu::Itkpixv2Emu(){
+namespace {
+    auto rlog = logging::make_log("emu_itkpixv2");
+}
+
+
+//The constructor needs to attach the virtual tx and rx cables
+Itkpixv2Emu::Itkpixv2Emu(EmuCom* tx, EmuCom* rx): m_tx(tx), m_rx(rx) {
+    
+    //Connect the cables to the virtual pads
+    //m_tx = tx;
+    //m_rx = rx;
+    run  = true;
+    std::cout << "Tx address in Itkpixv2Emu " << &(*m_tx) << " passed is " << &(*tx) << "\n";
 
 }
 
 void Itkpixv2Emu::executeLoop(){
+    //This loop should only run if the chip is turned on
+    if (!run) return;
 
+    //Check for commands in tx
+    if (m_tx->isEmpty()){
+        //std::cout << "waiting\n";
+        //if none, wait a bit and repeat the loop
+        std::this_thread::sleep_for(m_ns10);
+        executeLoop();
+    }
+
+    //Once the commands arrive, read them in
+    readCommand();
+
+    rlog->info("Received command {}", m_commandStream.front());
+
+}
+
+void Itkpixv2Emu::readCommand(){
+
+    //pop the 32-bit word that arrived
+    uint32_t w = m_tx->read32();
+
+    //split it into two 16-bit commands to be buffered
+    m_commandStream.push_back((w >> 16) & 0x0000FFFF);
+    m_commandStream.push_back(w & 0x0000FFFF);
 }
 
 void Itkpixv2Emu::outputLoop(){
 
 }
+
+Itkpixv2Emu::~Itkpixv2Emu() = default;
--- src/libItkpixv2Emu/include/Itkpixv2Emu.h
+++ src/libItkpixv2Emu/include/Itkpixv2Emu.h
@@ -8,18 +8,46 @@
 #define ITKPIXV2EMU_H
 
 #include "ItkpixLayout.h"
+#include "EmuCom.h"
+#include "logging.h"
+#include <atomic>
+#include <chrono>
+#include <thread>
+#include <future>
+#include <memory>
 
 class Itkpixv2Emu {
     public:
         
-        Itkpixv2Emu();
+        Itkpixv2Emu(EmuCom* tx, EmuCom* rx);
+        ~Itkpixv2Emu();
 
         void executeLoop();
 
         void outputLoop();
 
+        //The on/off switch visible to the controller
+        std::atomic<bool> run;
+
     private:
-        ItkpixLayout<uint16_t> ToT_map; //just a dummy
+
+        //Read the command from tx into the buffer
+        void readCommand();
+
+        //internal pointers to the virtual tx and rx lanes,
+        //aka the virtual chip's virtual pads to virtually connect
+        //the virtual cables
+        EmuCom* m_tx;
+        EmuCom* m_rx;
+
+        //Buffer for commands received from tx
+        std::deque<uint16_t> m_commandStream;
+
+        //Pixel representations
+        ItkpixLayout<uint16_t> m_ToT_map; //just a dummy
+
+        //Utilities
+        std::chrono::nanoseconds m_ns10 = std::chrono::nanoseconds(10);
 };
 
 #endif
