e2ab78d45cedb79085d3133e4c85d84b1b71e686
Angira Rastogi <angira.rastogi@cern.ch>
2024-05-24 12:39:26 -0700

    Adding register channel to FrontEndConnectivity in Itkpixv2

--- src/libItkpixv2/app/ITkPixV2CheckPulse.cpp
+++ src/libItkpixv2/app/ITkPixV2CheckPulse.cpp
@@ -56,7 +56,10 @@ std::unique_ptr<FrontEnd> init_fe1(std::unique_ptr<HwController>& hw, json &jcon
         throw std::runtime_error(e.str());
     }
     auto chip_config = chip_configs[fe_num];
-    fe->init(&*hw, FrontEndConnectivity(chip_config["tx"], chip_config["rx"]));
+    unsigned regRx = chip_config["rx"];
+    if(chip_config.contains("regRx")) 
+      regRx = chip_config["regRx"];
+    fe->init(&*hw, FrontEndConnectivity(chip_config["tx"], chip_config["rx"], regRx));
     auto chip_register_file_path1 = chip_config["__config_path__"];
     fs::path pconfig{chip_register_file_path1};
     if(!fs::exists(pconfig)) {
@@ -93,7 +96,10 @@ std::unique_ptr<FrontEnd> init_fe2(std::unique_ptr<HwController>& hw, json &jcon
         throw std::runtime_error(e.str());
     }
     auto chip_config = chip_configs[fe_num];
-    fe->init(&*hw, FrontEndConnectivity(chip_config["tx"], chip_config["rx"]));
+    unsigned regRx = chip_config["rx"]; 
+    if(chip_config.contains("regRx"))
+      regRx = chip_config["regRx"]; 
+    fe->init(&*hw, FrontEndConnectivity(chip_config["tx"], chip_config["rx"], regRx));
     auto chip_register_file_path2 = chip_config["__config_path__"];
     fs::path pconfig{chip_register_file_path2};
     if(!fs::exists(pconfig)) {
