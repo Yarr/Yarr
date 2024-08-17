bf4d3919297f90b973f7224f74c268f96a15c11a
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2024-08-17 15:38:06 -0700

    Fix cmake jobs argumant

--- scripts/run_codechecker.sh
+++ scripts/run_codechecker.sh
@@ -13,11 +13,11 @@ while getopts "j:" o; do
             ;;
     esac
 done
-
+echo Running CodeChecker with ${jobs} jobs
 mkdir checks
 cmake3 -S . -B build -DYARR_CONTROLLERS_TO_BUILD="Spec;Emu;StarEmu;Rd53aEmu;FelixClient" -DYARR_FRONT_ENDS_TO_BUILD="Rd53a;Star;Rd53b;Itkpixv2" -DDISABLE_PLOTTING_TOOLS=On -DYARR_EMULATORS_TO_BUILD="StarEmu;Rd53aEmu"
 CodeChecker version
-CodeChecker log -b  "cmake3 --build build -- -j ${jobs}" -o  checks/compilation_cmds.json
+CodeChecker log -b  "cmake3 --build build --parallel  ${jobs}" -o  checks/compilation_cmds.json
 scripts/fix_cdb.py > checks/compilation_cmds_filtered.json 
 CodeChecker analyze checks/compilation_cmds_filtered.json -i scripts/code_checker.ignore -j ${jobs} -o checks/results \
   --analyzers clang-tidy \
