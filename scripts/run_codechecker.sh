1a02412f738caf12c31e6ed5353a36a1d7ab6930
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2024-08-17 17:37:40 -0700

    Revert back to full build

--- scripts/run_codechecker.sh
+++ scripts/run_codechecker.sh
@@ -15,7 +15,7 @@ while getopts "j:" o; do
 done
 echo Running CodeChecker with ${jobs} jobs
 mkdir checks
-cmake3 -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=On  -DYARR_CONTROLLERS_TO_BUILD="Spec;Emu;StarEmu;Rd53aEmu;FelixClient" -DYARR_FRONT_ENDS_TO_BUILD="Rd53a;Star;Rd53b;Itkpixv2" -DDISABLE_PLOTTING_TOOLS=On -DYARR_EMULATORS_TO_BUILD="StarEmu;Rd53aEmu"
+cmake3 -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=On
 CodeChecker version
 cmake --build build --parallel ${jobs}
 scripts/fix_cdb.py > checks/compilation_cmds_filtered.json
