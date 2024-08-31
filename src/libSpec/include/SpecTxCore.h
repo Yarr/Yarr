30e65c75e7499837d1699f53582fb3a47774bdd5
Luc Tomas Le Pottier <luclepot@berkeley.edu>
2024-08-31 10:21:46 -0700

    Update register mapping information in spec Rx/Tx core, and add register information to specReadWriteReg

--- src/libSpec/include/SpecTxCore.h
+++ src/libSpec/include/SpecTxCore.h
@@ -42,7 +42,7 @@
 #define TX_IDLE_WORD 0x13
 #define TRIG_EXTEND_INTERVAL 0x14
 #define TRIG_ENCODER_ENABLE 0x15
-#define TRIG_TLU_SIMPLE_MODE 0x16
+#define TRIG_CODE_READY_COUNTER 0x16
 
 #define TX_CLK_PERIOD 25e-9
 
@@ -55,6 +55,7 @@
 #define TRIG_LOGIC_DELAY 0x4 // And the next 3 addresses up to 0x7
 #define TRIG_LOGIC_DEADTIME 0x8
 #define TRIG_LOGIC_EUDET_SIMPLE 0x9 // use simple eudet mode
+#define TRIG_LOGIC_MASTER_TRIGGER_COUNTER 0xA // Master trig counter
 
 #define NCHANNELS 4
 
