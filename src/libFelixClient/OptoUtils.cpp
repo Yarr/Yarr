3a7c87073c3338e9f77e4ce8b10b275c17894e55
Laura Clare Nosler <laura.clare.nosler@cern.ch>
2025-02-27 16:35:42 -0800

    adding in new OptoUtils class

---
+++ src/libFelixClient/OptoUtils.cpp
@@ -0,0 +1,146 @@
+#include "OptoUtils.h"
+#include "Utils.h"
+#include "logging.h"
+
+
+
+ bool OptoUtils::communicateLpGBT(lpgbt_addr, i2c_addr, regname, regval, read){
+  regAddr = getRegaddr();
+  data = prepareFrame(addresses);
+  readback(communicate)
+ }
+
+bool OptoUtils::readLpGBTRegister(uint_32t reg_addr, uint8_t& reg_data){
+  std::string i2C_addr_str = std::static_cast<string>(i2c_addr);
+
+  // if we're communicating directly to the primary LpGBT, we only need to send one simple register read
+  if (lpgbt_addr == m_i2c_addr){
+    communicateLpGBT();
+  }
+  // communicating with secondary LpGBTs via I2C channel through the primary LpGBT
+  else {
+    uint8_t NBYTE = 2;
+    readWriteOpticalDeviceReg
+    communicateLpGBT("I2CM" + i2c_addr_str + "DATA0", (scl_drive << 7) | (NBYTE << 2) | freq)
+    communicateLpGBT("I2CM" + i2c_addr_str + "CMD", i2c_write_cr)
+
+    # Send register address
+    communicateLpGBT("I2CM" + i2c_addr_str + "DATA0", divmod(reg_addr,0x100)[1])    # Lower half of register address
+    communicateLpGBT("I2CM" + i2c_addr_str + "DATA1", divmod(reg_addr,0x100)[0])    # Upper half of register address
+
+    communicateLpGBT("I2CM" + i2c_addr_str + "CMD", i2c_w_multi_4byte0)
+    communicateLpGBT("I2CM" + i2c_addr_str + "ADDRESS", lpgbt_addr)
+    communicateLpGBT("I2CM" + i2c_addr_str + "CMD", i2c_write_multi)    # Initiate send of register address and register value
+
+    # read answer from I2C device
+    NBYTE = 1
+    self.comm_wrapper("I2CM" + i2c_addr_str + "DATA0", (scl_drive << 7) | (NBYTE << 2) | freq)
+    self.comm_wrapper("I2CM" + i2c_addr_str + "CMD", i2c_write_cr)
+
+    self.comm_wrapper("I2CM" + i2c_addr_str + "ADDRESS", lpgbt_addr)
+    self.comm_wrapper("I2CM" + i2c_addr_str + "CMD", i2c_read_multi)
+
+    status_value = self.comm_wrapper("I2CM" + i2c_addr_str + "STATUS", None)
+    self.status_info(status_value)     # Read status register
+
+    # Read slave answer from LpGBT master register
+    read_reg, read = self.comm_wrapper("I2CM" + i2c_addr_str + "READ15", None)
+
+  if reg_field is not None:
+
+      reg_field_class = getattr(getattr(eval("self." + self.device_type + "_reg_map"), reg), reg_field)
+      read = read >> getattr(reg_field_class, "offset") & (2**getattr(reg_field_class, "length")-1)
+
+      logger.debug("read - reg_field is not None - offset: %s, length: %s", getattr(reg_field_class, "offset"), getattr(reg_field_class, "length"))
+
+      read_reg = reg + "_" + reg_field
+  else:
+      read_reg = reg      # would otherwise return "I2CM" + str(I2C_master) + "READ15"
+
+  logger.debug("Read from %s - %s: %s (%s, %s)", self.device, read_reg, read, hex(read), bin(read))
+
+  return read
+
+}
+*/
+
+bool OptoUtils::writeLpGBTRegister(uint32_t reg_addr, uint8_t& reg_data){
+
+
+}
+
+bool OptoUtils::readGBCRRegister(uint32_t reg_addr, uint8_t& reg_data){
+
+
+}
+
+bool OptoUtils::writeGBCRRegister(uint32_t reg_addr, uint8_t& reg_data){
+
+
+}
+
+
+
+
+std::vector<uint8_t> FelixController::prepareICDataFrame(const bool read, const uint16_t reg_addr, const uint8_t i2c_addr, const unsigned int device_version, const std::vector<uint8_t>& data){
+  /*
+    Based on itk-ic-over-netio-next communication wrapper, 
+    source: https://gitlab.cern.ch/itk-felix-sw/itk-ic-over-netio-next/-/blob/master/src/itk-ic-over-netio-next.cc?ref_type=heads
+
+    Data frame is structured as follows:
+    header | data | footer
+     * header: contains 6 or 7 bits depending on LpGBT version (first bit is 0 for v.0 LpGBT)
+               constructed of i2CAddress, bit to designate if we're doing a read or write command, number of data bytes
+     * data: if we're writing, we send the data to write, if not we skip this
+     * footer: register address, parity check
+  */
+
+  std::vector<uint8_t> frame = {};
+  
+  size_t header_size = 6;
+  if(deviceVersion == 0){
+    header_size = 7;
+  }
+  constexpr size_t footer_size = 1;
+
+  if (device_version == 1 && data.size() > 511 ){
+    std::cerr << "Size of data packet is too large to send for this version of LpGBT (v.1), maximum size is 511, current size is " << data.size()<< std::endl;
+    return frame;
+  }
+
+  // The size of the frame we send depends on whether this is a read or write command
+  if(!read)
+    frame.reserve(header_size + data.size() + footer_size);
+  else
+    frame.reserve(header_size + footer_size);
+
+  if(device_version == 0)
+    frame.push_back(0); // Reserved in LpGBT v0
+  
+  frame.push_back((i2c_addr << 1) + (read?0x1:0x0)); // GBTX I2C address and read/write bit
+  frame.push_back(1); // Command (not used in GBTX v1 + 2)
+  frame.push_back(data.size() & 0xFF); // Number of data bytes
+  frame.push_back((data.size() >> 8) & 0xFF);
+  
+  frame.push_back(reg_addr & 0xFF); // Register (start) address
+  frame.push_back(reg_addr >> 8 & 0xFF);
+
+  if(!read){
+    for(auto& val: data){
+      frame.push_back(val);
+    }
+  }
+
+  // For GBTx, skip first 2 bytes in parity check (see above)
+  std::size_t NUM_PARITY_BYTES_SKIP{0};
+  if(device_version == 0){
+    NUM_PARITY_BYTES_SKIP=2;
+  }
+  uint8_t parity = 0;
+  for(size_t i = NUM_PARITY_BYTES_SKIP;i < frame.size(); ++i){
+    parity ^= frame[i];
+  }
+  frame.push_back(parity);
+
+  return frame;
+}
---
+++ src/libFelixClient/include/OptoUtils.h
@@ -0,0 +1,125 @@
+#ifndef OPTOUTILS_H
+#define OPTOUTILS_H
+
+#include "FelixController.h"
+#include "Utils.h"
+#include "logging.h"
+
+class OptoUtils {
+
+public:
+    /*
+    Optoboard device communication
+    */
+    /// @brief Read a value from an LpGBT device register
+    /// @param reg_addr Register address (uint32_t)
+    /// @param reg_data Register data we're read back (set by reference) (uint8_t&)
+    /// @return True if operation is successful
+    bool readLpGBTRegister(uint_32t reg_addr, uint8_t& reg_data);
+
+    /// @brief Write a value to an LpGBT device register
+    /// @param reg_addr Register address (uint32_t)
+    /// @param reg_data Register data we want to write (set by reference) (uint8_t&)
+    /// @return True if operation is successful
+    bool writeLpGBTRegister(uint32_t reg_addr, uint8_t& reg_data);
+
+    /// @brief Read a value from a GBCR device register
+    /// @param reg_addr Register address (uint32_t)
+    /// @param reg_data Register data we're read back (set by reference) (uint8_t&)
+    /// @return True if operation is successful
+    uint8_t readGBCRRegister(uint32_t reg_addr, uint8_t& reg_data);
+
+    /// @brief Write a value to a GBCR device register
+    /// @param reg_addr Register address (uint32_t)
+    /// @param reg_data Register data we want to write (set by reference) (uint8_t&)
+    /// @return True if operation is successful
+    bool writeGBCRRegister(uint32_t reg_addr, uint8_t& reg_data);
+
+private:
+    // I2C parameters, defined in: https://gitlab.cern.ch/bat/optoboard_felix/-/blob/main/src/optoboard_felix/driver/Hardware.py#L55 and lpGBTv1 manual ch. 12.2.1
+    uint32_t m_i2c_write_cr{0x0};
+    uint32_t m_i2c_write_msk{0x1};
+    uint32_t m_i2c_1byte_write{0x2};
+    uint32_t m_i2c_1byte_read{0x3};
+    uint32_t m_i2c_1byte_write_ext{0x4};
+    uint32_t m_i2c_1byte_read_ext{0x5};
+    uint32_t m_i2c_1byte_rmw_or{0x6};
+    uint32_t m_i2c_1byte_rmw_xor{0x7};
+    uint32_t m_i2c_w_multi_4byte0{0x8};
+    uint32_t m_i2c_w_multi_4byte1{0x9};
+    uint32_t m_i2c_w_multi_4byte2{0xA};
+    uint32_t m_i2c_w_multi_4byte3{0xB};
+    uint32_t m_i2c_write_multi{0xC};
+    uint32_t m_i2c_read_multi{0xD};
+    uint32_t m_i2c_write_multi_ext{0xE};
+    uint32_t m_i2c_read_multi_ext{0xF};
+    uint32_t m_freq{2};
+    uint32_t m_scldrive{0};
+
+    uint32_t m_i2c_addr{0};
+
+    /*
+        Registers
+    */
+    // Optoboard V0
+    static const uint32_t I2CM0ADDR_V0 {0x0F1};
+    static const uint32_t I2CM0DATA0_V0 {0x0F2};
+    static const uint32_t I2CM0DATA1_V0 {0x0F3};
+    static const uint32_t I2CM0CMD_V0 {0x0F6};
+    static const uint32_t I2CM0STATUS_V0 {0x161};
+    static const uint32_t I2CM0READ15_V0 {0x173};
+    
+    static const uint32_t I2CM1ADDR_V0 {0x0F8};
+    static const uint32_t I2CM1DATA0_V0 {0x0F9};
+    static const uint32_t I2CM1DATA1_V0 {0x0FA};
+    static const uint32_t I2CM1CMD_V0 {0x0FD};
+    static const uint32_t I2CM1STATUS_V0 {0x176};   
+    static const uint32_t I2CM1READ15_V0 {0x188};
+
+    static const uint32_t I2CM2ADDR_V0 {0x0FF};
+    static const uint32_t I2CM2DATA0_V0 {0x100};
+    static const uint32_t I2CM2DATA1_V0 {0x101};
+    static const uint32_t I2CM2CMD_V0 {0x104};
+    static const uint32_t I2CM2STATUS_V0 {0x18B};
+    static const uint32_t I2CM2READ15_V0 {0x19D};
+
+    // Optoboard V1
+    static const uint32_t I2CM0ADDR_V1 {0x101}    
+    static const uint32_t I2CM0DATA0_V1 {0x102};
+    static const uint32_t I2CM0DATA1_V1 {0x103};
+    static const uint32_t I2CM0CMD_V1 {0x106};
+    static const uint32_t I2CM0STATUS_V1 {0x171};
+    static const uint32_t I2CM0READ15_V1 {0x183};
+    
+    static const uint32_t I2CM1ADDR_V1 {0x108};
+    static const uint32_t I2CM1DATA0_V1 {0x109};
+    static const uint32_t I2CM1DATA1_V1 {0x10A};
+    static const uint32_t I2CM1CMD_V1 {0x10D};
+    static const uint32_t I2CM1STATUS_V1 {0x186};   
+    static const uint32_t I2CM1READ15_V1 {0x198};
+
+    static const uint32_t I2CM2ADDR_V1 {0x10F};
+    static const uint32_t I2CM2DATA0_V1 {0x110};
+    static const uint32_t I2CM2DATA1_V1 {0x111};
+    static const uint32_t I2CM2CMD_V1 {0x114};
+    static const uint32_t I2CM2STATUS_V1 {0x19B};
+    static const uint32_t I2CM2READ15_V1 {0x1AD};
+
+
+    uint32_t getLpGBTAddress(unsigned int LpGBT_num, unsigned int link_num);
+
+    /*
+    IC connection send/receive utilities
+    */
+    /// @brief Constructs the dataframe to send over an IC channel (used for example in LpGBT register reads)
+    /// @param read Whether we will be reading or writing data (const bool, true for read, false for write)
+    /// @param regAddr Address of the register to read (const uint16_t)
+    /// @param i2cAddr I2C address to send data along, for optoboard communication, address of primary LpGBT (const uint8_t)
+    /// @param deviceVersion LpGBT version (either 0 or 1), affects how the data frame is prepared (const unsigned int)
+    /// @param data Data to send (const std::vector <uint8_t>&)
+    /// @return Returns the dataframe to send through the IC channel (std::vector<uint8_t>)
+    std::vector<uint8_t> prepareICDataFrame(const bool read, const uint16_t regAddr, const uint8_t i2cAddr, const unsigned int deviceVersion, const std::vector<uint8_t>& data);
+
+}
+
+#endif
