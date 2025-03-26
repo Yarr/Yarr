62c859cb394c77adb7d22120d02733141775c1f6
Luc Tomas Le Pottier <luclepot@berkeley.edu>
2025-03-26 09:05:59 -0700

    Verify edge case fails with un-fixed data processor

--- src/libItkpixv2/tests/test_data_processor_edgecases.cpp
+++ src/libItkpixv2/tests/test_data_processor_edgecases.cpp
@@ -122,7 +122,7 @@ TEST_CASE("Itkpixv2DataProcessor", "[itkpixv2][data_processor_edge_case]") {
     // High FFFFDEAD rate case
     std::vector<std::vector<uint32_t>> input;
     std::vector<uint32_t> deads;
-    for(int i = 0; i < 10000; i++)
+    for(int i = 0; i < 1000000; i++)
         deads.push_back(0xFFFFDEAD);
     input.push_back(deads);
     std::cout << "HIGH FFFFDEAD RATE CASE" << std::endl;
