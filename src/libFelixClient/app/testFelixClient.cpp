ffef06ad5e29755bc5b068eee0e119c766689053
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2025-03-31 18:38:35 -0700

    Call initRxChannels in testFelixClient.cpp

--- src/libFelixClient/app/testFelixClient.cpp
+++ src/libFelixClient/app/testFelixClient.cpp
@@ -166,6 +166,7 @@ int main(int argc, char **argv) {
   // Subscribe to elinks
   if (not elinks_rx.empty()) {
     try {
+      hwCtrl->initRxChannels(elinks_rx);
       hwCtrl->setRxEnable(elinks_rx);
     } catch (std::runtime_error& e) {
       logger->error("Fail to subscribe: {}", e.what());
