c610839cf6318500971fb6b025f2ab8c01fc1746
Bruce Gallop <bruce.gallop@cern.ch>
2025-04-03 13:29:03 +0100

    Add build type/flags to version info

--- src/libYarr/yarr.cpp
+++ src/libYarr/yarr.cpp
@@ -9,6 +9,8 @@ static const std::string git_hash = YARR_GIT_HASH;
 static const std::string git_date = YARR_GIT_DATE;
 static const std::string git_subject = YARR_GIT_SUBJECT;
 static const std::string build_time = BUILD_TIME;
+static const std::string build_type = CMAKE_BUILD_TYPE;
+static const std::string build_flags = CMAKE_CXX_FLAGS;
 
 json get() {
     json j;
@@ -18,6 +20,8 @@ json get() {
     j["git_date"] = git_date;
     j["git_subject"] = git_subject;
     j["build_time"] = build_time;
+    j["build_type"] = build_type;
+    j["build_flags"] = build_flags;
     return j;
 }
 }
