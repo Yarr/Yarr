87484a781d923b7743736506349a44c644bf55f7
Laura Clare Nosler <laura.clare.nosler@cern.ch>
2025-04-07 14:20:40 -0700

    adding function to check if primary

--- src/libFelixClient/include/OptoUtils.h
+++ src/libFelixClient/include/OptoUtils.h
@@ -1,6 +1,8 @@
 #ifndef OPTOUTILS_H
 #define OPTOUTILS_H
 
+#include <map>
+
 #include "Utils.h"
 #include "logging.h"
 
@@ -34,55 +36,54 @@ namespace OptoUtils {
         Registers
     */
     // Optoboard V0
-    static const std::map<std::string, uint32_t> LPGBT_REGMAP;
-    LPGBT_REGMAP["I2CM0ADDR_V0"] = 0x0F1;
-    LPGBT_REGMAP["I2CM0DATA0_V0"] = 0x0F2;
-    LPGBT_REGMAP["I2CM0DATA1_V0"] = 0x0F3;
-    LPGBT_REGMAP["I2CM0DATA2_V0"] = 0x0F4;
-    LPGBT_REGMAP["I2CM0CMD_V0"] = 0x0F6;
-    LPGBT_REGMAP["I2CM0STATUS_V0"] = 0x161;
-    LPGBT_REGMAP["I2CM0READ15_V0"] = 0x173;
+    static const std::map<std::string, uint32_t> LPGBT_REGMAP {
+    {"I2CM0ADDR_V0", 0x0F1},
+    {"I2CM0DATA0_V0", 0x0F2},
+    {"I2CM0DATA1_V0", 0x0F3},
+    {"I2CM0DATA2_V0", 0x0F4},
+    {"I2CM0CMD_V0", 0x0F6},
+    {"I2CM0STATUS_V0", 0x161},
+    {"I2CM0READ15_V0", 0x173},
     
-    LPGBT_REGMAP["I2CM1ADDR_V0"] = 0x0F8;
-    LPGBT_REGMAP["I2CM1DATA0_V0"] = 0x0F9;
-    LPGBT_REGMAP["I2CM1DATA1_V0"] = 0x0FA;
-    LPGBT_REGMAP["I2CM1DATA2_V0"] = 0x0FB;
-    LPGBT_REGMAP["I2CM1CMD_V0"] = 0x0FD;
-    LPGBT_REGMAP["I2CM1STATUS_V0"] = 0x176;   
-    LPGBT_REGMAP["I2CM1READ15_V0"] = 0x188;
+    {"I2CM1ADDR_V0", 0x0F8},
+    {"I2CM1DATA0_V0", 0x0F9},
+    {"I2CM1DATA1_V0", 0x0FA},
+    {"I2CM1DATA2_V0", 0x0FB},
+    {"I2CM1CMD_V0", 0x0FD},
+    {"I2CM1STATUS_V0", 0x176},   
+    {"I2CM1READ15_V0", 0x188},
 
-    LPGBT_REGMAP["I2CM2ADDR_V0"] = 0x0FF;
-    LPGBT_REGMAP["I2CM2DATA0_V0"] = 0x100;
-    LPGBT_REGMAP["I2CM2DATA1_V0"] = 0x101;
-    LPGBT_REGMAP["I2CM2DATA2_V0"] = 0x102;
-    LPGBT_REGMAP["I2CM2CMD_V0"] = 0x104;
-    LPGBT_REGMAP["I2CM2STATUS_V0"] = 0x18B;
-    LPGBT_REGMAP["I2CM2READ15_V0"] = 0x19D;
-
-    // Optoboard V1
-    LPGBT_REGMAP["I2CM0ADDR_V1"] = 0x101;    
-    LPGBT_REGMAP["I2CM0DATA0_V1"] = 0x102;
-    LPGBT_REGMAP["I2CM0DATA1_V1"] = 0x103;
-    LPGBT_REGMAP["I2CM0DATA2_V1"] = 0x104;
-    LPGBT_REGMAP["I2CM0CMD_V1"] = 0x106;
-    LPGBT_REGMAP["I2CM0STATUS_V1"] 0x171;
-    LPGBT_REGMAP["I2CM0READ15_V1"] 0x183;
+    {"I2CM2ADDR_V0", 0x0FF},
+    {"I2CM2DATA0_V0", 0x100},
+    {"I2CM2DATA1_V0", 0x101},
+    {"I2CM2DATA2_V0", 0x102},
+    {"I2CM2CMD_V0", 0x104},
+    {"I2CM2STATUS_V0", 0x18B},
+    {"I2CM2READ15_V0", 0x19D},
+    {"I2CM0ADDR_V1", 0x101},
+    {"I2CM0DATA0_V1", 0x102},
+    {"I2CM0DATA1_V1", 0x103},
+    {"I2CM0DATA2_V1", 0x104},
+    {"I2CM0CMD_V1", 0x106},
+    {"I2CM0STATUS_V1", 0x171},
+    {"I2CM0READ15_V1", 0x183},
     
-    LPGBT_REGMAP["I2CM1ADDR_V1"] = 0x108;
-    LPGBT_REGMAP["I2CM1DATA0_V1"] = 0x109;
-    LPGBT_REGMAP["I2CM1DATA1_V1"] = 0x10A;
-    LPGBT_REGMAP["I2CM1DATA2_V1"] = 0x10C;
-    LPGBT_REGMAP["I2CM1CMD_V1"] = 0x10D;
-    LPGBT_REGMAP["I2CM1STATUS_V1"] = 0x186;   
-    LPGBT_REGMAP["I2CM1READ15_V1"] = 0x198;
+    {"I2CM1ADDR_V1", 0x108},
+    {"I2CM1DATA0_V1", 0x109},
+    {"I2CM1DATA1_V1", 0x10A},
+    {"I2CM1DATA2_V1", 0x10C},
+    {"I2CM1CMD_V1", 0x10D},
+    {"I2CM1STATUS_V1", 0x186},
+    {"I2CM1READ15_V1", 0x198},
 
-    LPGBT_REGMAP["I2CM2ADDR_V1"] = 0x10F;
-    LPGBT_REGMAP["I2CM2DATA0_V1"] = 0x110;
-    LPGBT_REGMAP["I2CM2DATA1_V1"] = 0x111;
-    LPGBT_REGMAP["I2CM2DATA2_V1"] = 0x112;
-    LPGBT_REGMAP["I2CM2CMD_V1"] = 0x114;
-    LPGBT_REGMAP["I2CM2STATUS_V1"] = 0x19B;
-    LPGBT_REGMAP["I2CM2READ15_V1"] = 0x1AD;
+    {"I2CM2ADDR_V1", 0x10F},
+    {"I2CM2DATA0_V1", 0x110},
+    {"I2CM2DATA1_V1", 0x111},
+    {"I2CM2DATA2_V1", 0x112},
+    {"I2CM2CMD_V1", 0x114},
+    {"I2CM2STATUS_V1", 0x19B},
+    {"I2CM2READ15_V1", 0x1AD}
+    };
 
     /// @brief For a given device type and number (e.g. GBCR #2), return the address
     /// @param deviceType "GBCR", "LPGBT" accepted (string)
@@ -95,6 +96,7 @@ namespace OptoUtils {
 
     int getDeviceNum(int rx);
 
+    bool isPrimaryLpGBT(int rx);
 
     /*
     IC connection send/receive utilities
