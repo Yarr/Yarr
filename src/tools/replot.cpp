52da423a5b9b4b1140f04e4948d5577b877bb8e8
Bruce Gallop <bruce.gallop@stfc.ac.uk>
2025-05-17 14:05:56 +0100

    Allow replot for Histo3d and GraphErrors

--- src/tools/replot.cpp
+++ src/tools/replot.cpp
@@ -5,6 +5,8 @@
 
 #include "Histo2d.h"
 #include "Histo1d.h"
+#include "Histo3d.h"
+#include "GraphErrors.h"
 
 int main(int argc, char*argv[]) {
 	// Setup logger with some defaults
@@ -22,11 +24,17 @@ int main(int argc, char*argv[]) {
 	}
 	Histo1d h1("Temp1", 1, 0.0, 1.0);
 	Histo2d h2("Temp2", 1, 0.0, 1.0, 1, 0.0, 1.0);
+	Histo3d h3("Temp3", 1, 0.0, 1.0, 1, 0.0, 1.0, 1, 0.0, 1.0);
+	GraphErrors ge("TempErr");
 
 	if(h1.fromFile(std::string(argv[1]))){
 		h1.plot("replot", "./");
 	} else if (h2.fromFile(std::string(argv[1]))) {
 		h2.plot("replot", "./");
+	} else if(h3.fromFile(std::string(argv[1]))){
+		h3.plot("replot", "./");
+	} else if(ge.fromFile(std::string(argv[1]))){
+		ge.plot("replot", "./");
 	} else {
 		std::cout << "ABORTING: Could not read as either 1D or 2D histogram for replotting" << std::endl;
 	}
