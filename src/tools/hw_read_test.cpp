effa0031faba1060c0ca541a6ac80d0435a3e0f0
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2025-06-12 14:02:46 -0700

    Fix a bug to parse channel numbers so it can actually take a list of channel numbers

--- src/tools/hw_read_test.cpp
+++ src/tools/hw_read_test.cpp
@@ -56,7 +56,7 @@ Config parseOptions(int argc, char* argv[]) {
                 for (; optind < argc && *argv[optind] != '-'; optind += 1) {
                   try {
                     // Try parsing as number and throw if not
-                    config.read_channels.push_back(std::stoi(optarg));
+                    config.read_channels.push_back(std::stoi(argv[optind]));
                   } catch(std::exception &e) {
                     break;
                   }
