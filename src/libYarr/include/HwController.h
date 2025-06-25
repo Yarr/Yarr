15afc29d8731ee14fd71056ffca4361ffe180bd3
Bruce Gallop <bruce.gallop@cern.ch>
2025-06-25 13:30:38 +0100

    Add method to check for feature of HwController

--- src/libYarr/include/HwController.h
+++ src/libYarr/include/HwController.h
@@ -9,6 +9,8 @@
 // # Date: Feb 2017
 // ################################
 
+#include <string>
+
 #include "TxCore.h"
 #include "RxCore.h"
 
@@ -28,6 +30,8 @@ class HwController : virtual public TxCore, virtual public RxCore {
         virtual void runMode() {}
         virtual const json getStatus() { return json{}; };
 
+        virtual bool hasFeature(const std::string &feature) { return false; }
+  
         ~HwController() override = default;
 };
 
