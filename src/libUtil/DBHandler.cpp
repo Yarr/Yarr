09edb38a5658e9d580d01fd9dbaf7b3f4c95a653
Bruce Gallop <bruce.gallop@cern.ch>
2025-11-07 13:43:18 +0000

    Some more warnings

--- src/libUtil/DBHandler.cpp
+++ src/libUtil/DBHandler.cpp
@@ -72,7 +72,6 @@ void DBHandler::setDCSCfg(std::string i_dcs_path, std::string i_scanlog_path) {
 #if DBDEBUG
     std::cout << "DBHandler: Set DCS config: " << i_dcs_path << std::endl;
 #endif
-    char path[1000];
     std::string log_path = this->getAbsPath(i_scanlog_path);
     std::size_t pathPos = log_path.find_last_of('/');
     log_path = log_path.substr(0, pathPos);
@@ -143,7 +142,6 @@ void DBHandler::cleanUp(std::string i_option, std::string i_dir, bool i_back, bo
 #if DBDEBUG
     std::cout << "DBHandler: Clean Up." << std::endl;
 #endif
-    char path[1000];
     std::string result_dir;
     if (i_dir=="") {
         result_dir = m_output_dir;
@@ -188,6 +186,9 @@ void DBHandler::cleanUp(std::string i_option, std::string i_dir, bool i_back, bo
 		    result += buffer.data();
 	    }
 	    auto rc = pclose(pipe);
+	    if(rc) {
+	        dlog->warn("Ignoring non-zero return code from pclose {}", rc);
+	    }
 
 	    if( std::atoi( result.c_str() ) <= 2 ) {
 		break;
@@ -445,7 +446,7 @@ std::string DBHandler::checkDCSLog(std::string i_log_path, std::string i_dcs_pat
     for (unsigned i=0; i<log_lines.size(); i++) {
         log_ifs.getline(tmp, 1000);
         cnt = 0;
-        for (const auto s_tmp : split(tmp, separator)) {
+        for (const auto &s_tmp : split(tmp, separator)) {
             // check the first column
             if (i!=4&&columns==0&&s_tmp!=log_lines[i]) {
                 std::string message = "Set "+log_lines[i]+" in the "+std::to_string(i+1)+ "th line: "+i_log_path;
