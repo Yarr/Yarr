acba79f94ee8f6d829576ee0afc1522353a3af62
Bruce Gallop <bruce.gallop@cern.ch>
2026-02-11 16:01:43 +0000

    Warning related to json + diagnostics + gcc 14

--- src/libUtil/include/storage.hpp
+++ src/libUtil/include/storage.hpp
@@ -4,7 +4,16 @@
 // Include more info in exceptions
 #define JSON_DIAGNOSTICS 1
 
+#ifdef __GNUC__
+// Ignore false positives related to JSON_DIAGNOSTICS and gcc 14
+// (see https://github.com/nlohmann/json/issues/3808)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Warray-bounds"
 #include <json.hpp>
+#pragma GCC diagnostic pop
+#else
+#include <json.hpp>
+#endif
 
 // Mostly the defaults, but using 32 instead of 64 bit int/float
 using json=nlohmann::basic_json<std::map, std::vector, std::string, bool, std::int32_t, std::uint32_t, float>;
