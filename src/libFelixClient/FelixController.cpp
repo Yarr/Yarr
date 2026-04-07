e290ed22a4967527a9ee3c45e8e08dc0a3284b43
Koji Abel <kabel@berkeley.edu>
2026-04-07 16:47:38 -0700

    changed config parameter name to better reflect implementation

--- src/libFelixClient/FelixController.cpp
+++ src/libFelixClient/FelixController.cpp
@@ -92,8 +92,8 @@ void FelixController::loadConfig(const json &j) {
     fclog->info("Configuring FELIX card");
     auto cardCfg = j["Card"];
 
-    if(cardCfg.contains("configureEncodingDecodingOnLoad")){     
-        if(cardCfg["configureEncodingDecodingOnLoad"]){
+    if(cardCfg.contains("configureEpathOnLoad")){     
+        if(cardCfg["configureEpathOnLoad"]){
         //Must set the path encoding/decoding registers to 0x0 for per-link control as done with configureChannel 
         //This is opposed to the default register value which is 0x11111111
         fclog->info("Resetting encoding and decoding patterns");
@@ -154,7 +154,7 @@ void FelixController::loadConfig(const json &j) {
         }
       }
     }else{
-      fclog->info("configureEncodingDecodingOnLoad not specified, skipping...");
+      fclog->info("\"configureEpathOnLoad\" parameter not specified in \"Card\" configuration field, skipping...");
     }
 
     fclog->info("Setting provided FELIX registers");
