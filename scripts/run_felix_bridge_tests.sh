4c19dabff592c87648b11c1be64d0457e32cc2fd
Bruce Gallop <bruce.gallop@cern.ch>
2026-04-10 15:41:23 +0100

    Report bus files

--- scripts/run_felix_bridge_tests.sh
+++ scripts/run_felix_bridge_tests.sh
@@ -5,6 +5,10 @@ bin/felix_client_bridge --bus-dir /tmp/bus --bus-groupname FELIX -f configs/cont
 echo "TESTS: WAITING for FCB (on PID ${pid_bridge}) to start"
 sleep 3
 
+echo "TESTS: Check Felix Bus"
+find /tmp/bus -type f
+find /tmp/bus -name "*.ndjson" | xargs cat
+
 HW_CONTROLLER=configs/controller/felix_client_strips.json
 EMU_CONNECTIVITY=configs/connectivity/example_star_setup.json
 
@@ -12,6 +16,10 @@ EMU_CONNECTIVITY=configs/connectivity/example_star_setup.json
 echo "TESTS: Run generic felix client test"
 # Send a couple of L0A
 bin/testFelixClient ${HW_CONTROLLER} -r 0 -t 0 -w 4 -d 0x1e72d865 -n 2 -l configs/logging/default.json
+
+echo "TESTS: Check Felix Bus after tests"
+find /tmp/bus -type f
+find /tmp/bus -name "*.ndjson" | xargs cat
 
 echo "TESTS: Now shutdown FCB"
 
