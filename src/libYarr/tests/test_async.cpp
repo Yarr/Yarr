fb6b49c685ec811ed7efa9c4116f5dda57ef5afb
Bruce Gallop <bruce.gallop@cern.ch>
2026-06-26 10:17:15 +0100

    Filter on rx channel

--- src/libYarr/tests/test_async.cpp
+++ src/libYarr/tests/test_async.cpp
@@ -89,3 +89,41 @@ TEST_CASE("AsyncReadRegisterWithFilter", "[Async]") {
   }
   logger->debug("++ Finish async test");
 }
+
+TEST_CASE("AsyncReadRegisterMultipleChannels", "[Async]") {
+  // For instance reads from multiple registers discriminate by address
+  auto l = spdlog::get("StarChips");
+
+  MyTestHw hw;
+
+  logger->debug("++ Make async");
+  AsyncAccess::AsyncContext anon{hw, hw};
+
+  logger->debug("++ Do async read");
+  std::vector<AsyncAccess::AsyncReg> regs;
+
+  for(uint32_t idx=0; idx<10; idx++) {
+    regs.push_back
+      (
+       AsyncAccess::AsyncReg
+       (anon,
+        [&, idx](TxCore &) {
+          hw.data.push_back({std::make_unique<RawData>(idx, std::vector<uint32_t>{idx, idx*2})});
+        },
+        // Filter on rx channel
+        [idx](const RawData &d){ return d.getAdr() == idx; },
+        // Check filtered data is correct
+        [idx](const RawData &d){
+          CHECK(d.getBuf()[0] == idx);
+          CHECK(d.getBuf()[1] == idx*2);
+        }
+        ));
+  }
+
+  logger->debug("++ Async read in progress");
+
+  for(auto &r: regs) {
+    r.result.get();
+  }
+  logger->debug("++ Finish async test");
+}
