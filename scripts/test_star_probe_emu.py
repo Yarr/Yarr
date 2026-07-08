f3a868956d8a22ee2c294d645915ad8ffdfa2ff8
Bruce Gallop <bruce.gallop@cern.ch>
2026-07-08 16:24:31 +0100

    Pass flags for test_star
    Add quick test with reset flag

--- scripts/test_star_probe_emu.py
+++ scripts/test_star_probe_emu.py
@@ -5,8 +5,18 @@ import subprocess
 def run_test_star(
     emu_config="configs/controller/emuCfg_star_ppb.json",
     sequence=None,
+    do_resets=False,
+    report_mappings=False,
+    record_ids=False,
 ):
     cmdline = ["bin/test_star", emu_config]
+
+    if report_mappings:
+        # Report mapping between actions
+        cmdline.append("-v")
+
+    if do_resets:
+        cmdline.append("-R")
 
     rx_list = [0]
     tx_list = [0]
@@ -17,6 +27,10 @@ def run_test_star(
     if sequence is not None:
         cmdline.extend(["-s", sequence])
 
+    if record_ids:
+        # Save IDs to test_star_probe.out
+        cmdline.extend(["-l", "configs/logging/trace_star_id_to_file.json"])
+
     print(f"Running {cmdline}")
     info = subprocess.run(cmdline)
     print(f"Complete with {info.returncode}")
@@ -26,7 +40,9 @@ def run_test_star(
 
 
 def main():
+    # Run default (was the default quick CI check)
     run_test_star()
+    run_test_star(do_resets=True)
 
 
 if __name__ == "__main__":
