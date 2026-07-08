ee8272ec12ba6525c36fa9128986174cc319f4d9
Deion Elgin Fellers <dfellers@lbl.gov>
2026-07-08 20:42:33 +0200

    Fix type mismatch in ternary operator for config defaults

--- src/libFelixClient/FelixController.cpp
+++ src/libFelixClient/FelixController.cpp
@@ -32,9 +32,9 @@ void FelixController::loadConfig(const json &j) {
   fcConfig.property[FELIX_CLIENT_LOCAL_IP_OR_INTERFACE] = clientCfg["localIPorInterface"];
   fcConfig.property[FELIX_CLIENT_BUS_DIR] = clientCfg["busDir"];
   fcConfig.property[FELIX_CLIENT_BUS_GROUP_NAME] = clientCfg["busGroupName"];
-  fcConfig.property[FELIX_CLIENT_VERBOSE_BUS] = (clientCfg.contains("verboseBus") ? clientCfg["verboseBus"] : false) ? "True" : "False";
-  fcConfig.property[FELIX_CLIENT_USE_ASIO_EVLOOP] = (clientCfg.contains("useASIOevloop") ? clientCfg["useASIOevloop"] : false)  ? "True" : "False";
-  fcConfig.property[FELIX_CLIENT_USE_THREAD_UNSAFE_NETIO] = (clientCfg.contains("useThreadUnsafeNetio") ? clientCfg["useThreadUnsafeNetio"] : false)  ? "True" : "False"; 
+  fcConfig.property[FELIX_CLIENT_VERBOSE_BUS] = (clientCfg.contains("verboseBus") && clientCfg["verboseBus"].is_boolean() ? clientCfg["verboseBus"].get<bool>() : false) ? "True" : "False";
+  fcConfig.property[FELIX_CLIENT_USE_ASIO_EVLOOP] = (clientCfg.contains("useASIOevloop") && clientCfg["useASIOevloop"].is_boolean() ? clientCfg["useASIOevloop"].get<bool>() : false)  ? "True" : "False";
+  fcConfig.property[FELIX_CLIENT_USE_THREAD_UNSAFE_NETIO] = (clientCfg.contains("useThreadUnsafeNetio") && clientCfg["useThreadUnsafeNetio"].is_boolean() ? clientCfg["useThreadUnsafeNetio"].get<bool>() : false)  ? "True" : "False"; 
       try {
     auto txCfg = j["ToFLX"];
     FelixTxCore::loadConfig(txCfg);
